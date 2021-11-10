1a6d2b021d4203c0a4180f5c8768e576515e8d03
shsimeonova <simona.simeonova@hotmail.com>
2021-11-10 10:32:27 +0200

    Removed unused import

--- Open Judge System/Web/OJS.Web/Areas/Api/Controllers/ResultsController.cs
+++ Open Judge System/Web/OJS.Web/Areas/Api/Controllers/ResultsController.cs
@@ -4,7 +4,6 @@
     using System.Globalization;
     using System.Linq;
     using System.Web.Mvc;
-    using MissingFeatures;
     using OJS.Common.Models;
     using OJS.Data.Models;
     using OJS.Services.Data.Contests;
