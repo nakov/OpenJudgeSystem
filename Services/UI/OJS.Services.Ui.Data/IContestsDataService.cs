4c6576019707c1e0c8c7a26f8af07dd31d6f13d8
gogo4ds <gogo4ds@gmail.com>
2021-11-16 16:07:55 +0200

    Copied ContestsDataService.cs from old judge

--- Services/UI/OJS.Services.Ui.Data/IContestsDataService.cs
+++ Services/UI/OJS.Services.Ui.Data/IContestsDataService.cs
@@ -4,6 +4,7 @@ namespace OJS.Services.Ui.Data
     using OJS.Services.Common.Data.Infrastructure;
     using OJS.Services.Infrastructure.Mapping;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
 
     public interface IContestsDataService : IDataService<Contest>
@@ -14,6 +15,42 @@ namespace OJS.Services.Ui.Data
         Task<IEnumerable<TServiceModel>> GetAllPast<TServiceModel>()
             where TServiceModel : IMapFrom<Contest>;
 
+        Task<Contest> GetByIdWithProblems(int id);
+
+        IQueryable<Contest> GetByIdQuery(int id);
+
+        IQueryable<Contest> GetAllActive();
+
+        IQueryable<Contest> GetAllInactive();
+
+        IQueryable<Contest> GetAllUpcoming();
+
+        IQueryable<Contest> GetAllVisible();
+
+        IQueryable<Contest> GetAllVisibleByCategory(int categoryId);
+
+        IQueryable<Contest> GetAllVisibleBySubmissionType(int submissionTypeId);
+
+        IQueryable<Contest> GetAllByLecturer(string lecturerId);
+
+        IQueryable<Contest> GetAllVisibleByCategoryAndLecturer(int categoryId, string lecturerId);
+
+        IQueryable<Contest> GetAllWithDeleted();
+
+        Task<int> GetMaxPointsById(int id);
+
+        Task<int> GetMaxPointsForExportById(int id);
+
+        Task<string> GetNameById(int id);
+
+        Task<bool> IsActiveById(int id);
+
+        Task<bool> IsOnlineById(int id);
+
         Task<bool> IsUserLecturerInByContestAndUser(int id, string userId);
+
+        Task<bool> IsUserParticipantInByContestAndUser(int id, string userId);
+
+        Task<bool> IsUserInExamGroupByContestAndUser(int id, string userId);
     }
 }
--- Services/UI/OJS.Services.Ui.Data/Implementations/ContestsDataService.cs
+++ Services/UI/OJS.Services.Ui.Data/Implementations/ContestsDataService.cs
@@ -1,13 +1,17 @@
 namespace OJS.Services.Ui.Data.Implementations
 {
     using Microsoft.EntityFrameworkCore;
+    using OJS.Common.Enumerations;
     using OJS.Data;
     using OJS.Data.Models.Contests;
+    using OJS.Data.Models.Problems;
     using OJS.Services.Common;
     using OJS.Services.Common.Data.Infrastructure.Implementations;
     using OJS.Services.Infrastructure.Mapping;
+    using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Linq.Expressions;
     using System.Threading.Tasks;
 
     public class ContestsDataService : DataService<Contest>, IContestsDataService
@@ -35,11 +39,109 @@ namespace OJS.Services.Ui.Data.Implementations
                 .MapCollection<TServiceModel>()
                 .ToListAsync();
 
+        public Task<Contest> GetByIdWithProblems(int id)
+            => this.DbSet
+                .Include(c => c.ProblemGroups.Select(pg => pg.Problems))
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+        public IQueryable<Contest> GetByIdQuery(int id)
+            => this.DbSet
+                .Where(c => c.Id == id);
+
+        public IQueryable<Contest> GetAllActive()
+            => this.GetAllVisible()
+                .Where(c =>
+                    c.StartTime <= DateTime.Now &&
+                    (c.EndTime >= DateTime.Now ||
+                        (c.Type == ContestType.OnlinePracticalExam && c.Participants.Any(p =>
+                            p.IsOfficial &&
+                            p.ParticipationEndTime >= DateTime.Now))));
+
+        public IQueryable<Contest> GetAllInactive()
+            => this.DbSet
+                .Where(c =>
+                    c.StartTime > DateTime.Now ||
+                    (c.EndTime < DateTime.Now && c.Type != ContestType.OnlinePracticalExam) ||
+                    !c.Participants.Any(p => p.ParticipationEndTime < DateTime.Now));
+
+        public IQueryable<Contest> GetAllUpcoming()
+            => this.GetAllVisible()
+                .Where(c => c.StartTime > DateTime.Now);
+
+        public IQueryable<Contest> GetAllVisible()
+            => this.DbSet
+                .Where(c => c.IsVisible);
+
+        public IQueryable<Contest> GetAllVisibleByCategory(int categoryId)
+            => this.GetAllVisible()
+                .Where(c => c.CategoryId == categoryId);
+
+        public IQueryable<Contest> GetAllVisibleBySubmissionType(int submissionTypeId)
+            => this.GetAllVisible()
+                .Where(c => c.ProblemGroups
+                    .SelectMany(pg => pg.Problems)
+                    .Any(p => p.SubmissionTypesInProblems.Any(s => s.SubmissionTypeId == submissionTypeId)));
+
+        public IQueryable<Contest> GetAllByLecturer(string lecturerId)
+            => this.DbSet
+                .Where(c =>
+                    c.LecturersInContests.Any(l => l.LecturerId == lecturerId) ||
+                    c.Category.LecturersInContestCategories.Any(l => l.LecturerId == lecturerId));
+
+        public IQueryable<Contest> GetAllVisibleByCategoryAndLecturer(int categoryId, string lecturerId)
+            => this.GetAllByLecturer(lecturerId)
+                .Where(c => c.CategoryId == categoryId);
+
+        public IQueryable<Contest> GetAllWithDeleted() => this.DbSet.IgnoreQueryFilters();
+
+        public Task<int> GetMaxPointsById(int id)
+            => this.GetMaxPointsByIdAndProblemGroupsFilter(id, pg => true);
+
+        public Task<int> GetMaxPointsForExportById(int id)
+            => this.GetMaxPointsByIdAndProblemGroupsFilter(id, pg => pg.Type != ProblemGroupType.ExcludedFromHomework);
+
+        public Task<string> GetNameById(int id)
+            => this.GetByIdQuery(id)
+                .Select(c => c.Name)
+                .FirstOrDefaultAsync();
+
+        public async Task<bool> IsActiveById(int id)
+        {
+            var contest = await this.OneById(id);
+            return contest != null && contest.IsActive;
+        }
+
+        public async Task<bool> IsOnlineById(int id)
+            => await this.GetByIdQuery(id)
+                .Select(c => c.Type)
+                .FirstOrDefaultAsync() == ContestType.OnlinePracticalExam;
+
         public Task<bool> IsUserLecturerInByContestAndUser(int id, string userId)
             => this.GetByIdQuery(id)
                 .AnyAsync(c =>
                     c.LecturersInContests.Any(l => l.LecturerId == userId) ||
                     c.Category.LecturersInContestCategories.Any(l => l.LecturerId == userId));
+
+        public Task<bool> IsUserParticipantInByContestAndUser(int id, string userId)
+            => this.DbSet
+                .AnyAsync(c =>
+                    c.Id == id &&
+                    c.Participants.Any(p => p.UserId == userId));
+
+        public Task<bool> IsUserInExamGroupByContestAndUser(int id, string userId)
+            => this.DbSet
+                .AnyAsync(c =>
+                    c.Id == id &&
+                    c.ExamGroups.Any(eg => eg.UsersInExamGroups.Any(u => u.UserId == userId)));
+
+        private async Task<int> GetMaxPointsByIdAndProblemGroupsFilter(int id, Expression<Func<ProblemGroup, bool>> filter)
+            => await this.GetByIdQuery(id)
+                .Select(c => c.ProblemGroups
+                    .AsQueryable()
+                    .Where(pg => pg.Problems.Any(p => !p.IsDeleted))
+                    .Where(filter)
+                    .Sum(pg => (int?)pg.Problems.FirstOrDefault().MaximumPoints))
+                .FirstOrDefaultAsync() ?? default(int);
 
         private IQueryable<Contest> GetAllVisibleQuery()
             => this.DbSet
