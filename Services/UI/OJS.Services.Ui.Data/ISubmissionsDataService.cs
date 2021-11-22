8f7086e90f2adf0c324fdc38e95584a13f1d6b33
Georgi Georgiev <gogo4ds@gmail.com>
2021-11-22 17:00:18 +0200

    Copied SubmissionsDataService

--- Services/UI/OJS.Services.Ui.Data/ISubmissionsDataService.cs
+++ Services/UI/OJS.Services.Ui.Data/ISubmissionsDataService.cs
@@ -2,8 +2,35 @@ namespace OJS.Services.Ui.Data
 {
     using OJS.Data.Models.Submissions;
     using OJS.Services.Common.Data;
+    using System;
+    using System.Linq;
 
     public interface ISubmissionsDataService : IDataService<Submission>
     {
+        Submission GetBestForParticipantByProblem(int participantId, int problemId);
+
+        IQueryable<Submission> GetByIdQuery(int id);
+
+        IQueryable<Submission> GetAllByProblem(int problemId);
+
+        IQueryable<Submission> GetAllByProblemAndParticipant(int problemId, int participantId);
+
+        IQueryable<Submission> GetAllFromContestsByLecturer(string lecturerId);
+
+        IQueryable<Submission> GetAllCreatedBeforeDateAndNonBestCreatedBeforeDate(
+            DateTime createdBeforeDate,
+            DateTime nonBestCreatedBeforeDate);
+
+        IQueryable<Submission> GetAllHavingPointsExceedingLimit();
+
+        IQueryable<int> GetIdsByProblem(int problemId);
+
+        bool IsOfficialById(int id);
+
+        void SetAllToUnprocessedByProblem(int problemId);
+
+        void DeleteByProblem(int problemId);
+
+        void RemoveTestRunsCacheByProblem(int problemId);
     }
 }
