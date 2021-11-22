c5c536e79cb33358dedad32a9095ba6048547185
Georgi Georgiev <gogo4ds@gmail.com>
2021-11-22 11:17:22 +0200

    Added ProblemGroupsDataService

---
+++ Services/UI/OJS.Services.Ui.Data/Implementations/ProblemGroupsDataService.cs
@@ -0,0 +1,42 @@
+namespace OJS.Services.Ui.Data.Implementations
+{
+    using Microsoft.EntityFrameworkCore;
+    using OJS.Data.Models.Problems;
+    using OJS.Services.Common.Data.Implementations;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class ProblemGroupsDataService : DataService<ProblemGroup>, IProblemGroupsDataService
+    {
+        public ProblemGroupsDataService(DbContext db) : base(db)
+        {
+        }
+
+        public Task<ProblemGroup> GetByProblem(int problemId) =>
+            this.GetAll()
+                .FirstOrDefaultAsync(pg => pg.Problems
+                    .Any(p => p.Id == problemId));
+
+        public IQueryable<ProblemGroup> GetByIdQuery(int id) =>
+            this.GetAll()
+                .Where(pg => pg.Id == id);
+
+        public IQueryable<ProblemGroup> GetAll() => this.DbSet;
+
+        public IQueryable<ProblemGroup> GetAllWithDeleted() =>
+            this.DbSet.IgnoreQueryFilters();
+
+        public IQueryable<ProblemGroup> GetAllByContest(int contestId) =>
+            this.GetAll()
+                .Where(pg => pg.ContestId == contestId);
+
+        public IQueryable<Problem> GetProblemsById(int id) =>
+            this.GetByIdQuery(id)
+                .SelectMany(eg => eg.Problems)
+                .Where(p => !p.IsDeleted);
+
+        public Task<bool> IsFromContestByIdAndContest(int id, int contestId) =>
+            this.GetByIdQuery(id)
+                .AnyAsync(pg => pg.ContestId == contestId);
+    }
+}
