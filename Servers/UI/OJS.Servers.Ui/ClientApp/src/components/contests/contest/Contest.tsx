54d998f1742dd3961b50135926bd852ab2fa7acd
SIMONASIMEONOVA\User <simona.simeonova@softuni.bg>
2022-03-01 12:12:04 +0200

    Added contest page code editor and moved material ui tabs in component

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/contests/contest/Contest.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/contests/contest/Contest.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import MonacoEditor from 'react-monaco-editor';
 import { useContests } from '../../../hooks/contests/use-contests';
 import Heading from '../../guidelines/headings/Heading';
 import List from '../../guidelines/lists/List';
 import { IProblemType } from '../../../hooks/submissions/types';
+import Tabs from '../../guidelines/tabs/Tabs';
 import styles from './Contest.module.scss';
 
 const Contest = () => {
@@ -26,6 +28,12 @@ const Contest = () => {
             )
     );
 
+    const onNewCode = (newValue: string, e: any) => {
+        console.log('onChange', newValue, e);
+    };
+
+    const code = 'Write your code here';
+
     return (
         <div className={styles.contestWrapper}>
             <div className={styles.tasksSideNavigation}>
@@ -34,6 +42,35 @@ const Contest = () => {
             </div>
             <div className={styles.contestMainWrapper}>
                 <Heading type="primary">{currentContest?.name}</Heading>
+                <div className={styles.contestInnerLayout}>
+                    <div>
+                        <MonacoEditor
+                          width="600"
+                          height="400"
+                          language="javascript"
+                          theme="vs-dark"
+                          value={code}
+                          options={{
+                              selectOnLineNumbers: true,
+                              minimap: { enabled: false },
+                              automaticLayout: true,
+                              hideCursorInOverviewRuler: true,
+                              lineHeight: 20,
+                              scrollbar: {
+                                  horizontal: 'hidden',
+                                  vertical: 'hidden',
+                              },
+                          }}
+                          onChange={onNewCode}
+                        />
+                    </div>
+                    <div>
+                        <Tabs
+                          tabLabels={[ 'Problem', 'Submissions' ]}
+                          tabChildren={[ <p>Problem Description</p>, <p>Submissions</p> ]}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     );
