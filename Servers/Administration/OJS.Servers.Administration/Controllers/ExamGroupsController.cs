70370859eaa20bddab5fcc6a4a576e06e1dd8b2c
gogo4ds <gogo4ds@gmail.com>
2022-03-21 16:05:33 +0200

    Removed db calls from ExamGroupValidatorsFactory.cs https://github.com/SoftUni-Internal/exam-systems-issues/issues/122

--- Servers/Administration/OJS.Servers.Administration/Controllers/ExamGroupsController.cs
+++ Servers/Administration/OJS.Servers.Administration/Controllers/ExamGroupsController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using OJS.Data.Models;
 using OJS.Data.Models.Contests;
 using OJS.Services.Administration.Business.Validation.Factories;
+using OJS.Services.Administration.Business.Validation.Helpers;
+using OJS.Services.Infrastructure.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -13,11 +15,16 @@ using Resource = OJS.Common.Resources.ExamGroupsController;
 
 public class ExamGroupsController : BaseAutoCrudAdminController<ExamGroup>
 {
-    private readonly IExamGroupValidatorsFactory examGroupValidatorsFactory;
+    private readonly IContestsValidationHelper contestsValidationHelper;
+    private readonly IValidatorsFactory<ExamGroup> examGroupValidatorsFactory;
 
     public ExamGroupsController(
-        IExamGroupValidatorsFactory examGroupValidatorsFactory)
-        => this.examGroupValidatorsFactory = examGroupValidatorsFactory;
+        IContestsValidationHelper contestsValidationHelper,
+        IValidatorsFactory<ExamGroup> examGroupValidatorsFactory)
+    {
+        this.contestsValidationHelper = contestsValidationHelper;
+        this.examGroupValidatorsFactory = examGroupValidatorsFactory;
+    }
 
     public IActionResult Users([FromQuery] IDictionary<string, string> complexId)
     {
@@ -43,4 +50,23 @@ public class ExamGroupsController : BaseAutoCrudAdminController<ExamGroup>
         {
             new() { Action = nameof(this.Users) },
         };
+
+    protected override Task BeforeGeneratingForm(
+        ExamGroup entity,
+        EntityAction action,
+        IDictionary<string, string> entityDict)
+        => this.ValidateContestPermissions(entity);
+
+    protected override Task BeforeEntitySaveAsync(ExamGroup entity, AdminActionContext actionContext)
+        => this.ValidateContestPermissions(entity);
+
+    private async Task ValidateContestPermissions(ExamGroup entity)
+    {
+        if (entity.ContestId.HasValue)
+        {
+            await this.contestsValidationHelper
+                .ValidatePermissionsOfCurrentUser(entity.ContestId)
+                .VerifyResult();
+        }
+    }
 }
