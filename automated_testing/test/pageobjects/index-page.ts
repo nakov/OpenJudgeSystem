9fa22b9da7137e262f642bc7079a2796e71f290d
Mila Madzharova <madjarova.ml@gmail.com>
2022-03-30 15:21:00 +0200

    tests ++ https://github.com/SoftUni-Internal/exam-systems-issues/issues/124

--- automated_testing/test/pageobjects/index-page.ts
+++ automated_testing/test/pageobjects/index-page.ts
@@ -13,12 +13,40 @@ class IndexPage extends Page {
 
     private pageHeadingImageSelector = '#page-header-h2 a img';
 
-    private pageLogInButtonSelector = '#nav-login-link';
+    private pageLogInButtonSelector = '#anonymous-login-link';
 
-    private pageRegisterButtonSelector = '#nav-register-link';
+    private pageRegisterButtonSelector = '#anonymous-register-link';
+
+    private pageNavContestsLink = '#nav-contests-link';
+
+    private pageNavSubmissionssLink = '#nav-submissions-link';
+
+    private pageSeeContestsButton = '#button-see-contests';
+
+    // private pageCompeteCardButton = '#button-card-compete';
+
+    // private pagePracticeCardButton = '#button-card-practice';
+
+    private pageYoutubeVideo = '#youtube-video';
 
     public get headerHeading() {
         return $(this.pageHeaderHeadingSelector);
+    }
+
+    public get navSubmissionssLink() {
+        return $(this.pageNavSubmissionssLink);
+    }
+
+    public get navContestsLink() {
+        return $(this.pageNavContestsLink);
+    }
+
+    public get youtubeVideo() {
+        return $(this.pageYoutubeVideo);
+    }
+
+    public get seeContestsButton() {
+        return $(this.pageSeeContestsButton);
     }
 
     public get footer() {
--- automated_testing/test/tests/index-test.ts
+++ automated_testing/test/tests/index-test.ts
@@ -1,36 +1,65 @@
 import IndexPage from '../pageobjects/index-page';
 
 describe('Testing index', () => {
-    it('Expect logInButton to exist', async () => {
+    xit('Expect logInButton to exist', async () => {
         await IndexPage.open();
         const btn = await IndexPage.logInButton;
         await expect(btn).toExist();
         await expect(btn).toHaveText('LOGIN');
+        await expect(btn).toHaveHrefContaining('/login');
     });
 
-    it('Expect registerButton to exist', async () => {
+    xit('Expect registerButton to exist', async () => {
         await IndexPage.open();
         const btn = await IndexPage.registerButton;
         await expect(btn).toExist();
         await expect(btn).toHaveText('REGISTER');
+        await expect(btn).toHaveHrefContaining('/register');
     });
 
-    it('Expect navBar to have headerLogoLink with href', async () => {
+    xit('Expect navBar to have headerLogoLink with href', async () => {
         await IndexPage.open();
         const a = await IndexPage.headerLogoLink;
         await expect(a).toHaveHrefContaining('/');
     });
 
-    it('Expect to have imgTag with alt', async () => {
+    xit('Expect to have imgTag with alt', async () => {
         await IndexPage.open();
         const img = await IndexPage.headingImage;
         await expect(img).toExist();
         await expect(img).toHaveAttr('alt', 'softuni logo');
     });
 
-    it('Expect footer to exist', async () => {
+    xit('Expect footer to exist', async () => {
         await IndexPage.open();
         const footer = await IndexPage.footer;
         await expect(footer).toExist();
+    });
+
+    xit('Expect contests link in navigation to exist and have the correct href', async () => {
+        await IndexPage.open();
+        const contestsLink = await IndexPage.navContestsLink;
+        await expect(contestsLink).toExist();
+        await expect(contestsLink).toHaveHrefContaining('/contests/all');
+    });
+
+    xit('Expect submissions link in navigation to exist and have the correct href', async () => {
+        await IndexPage.open();
+        const submissionsLink = await IndexPage.navSubmissionssLink;
+        await expect(submissionsLink).toExist();
+        await expect(submissionsLink).toHaveHrefContaining('/submissions');
+    });
+
+    xit('Expect "See contests" button in navigation to exist and have the correct href', async () => {
+        await IndexPage.open();
+        const btn = await IndexPage.seeContestsButton;
+        await expect(btn).toExist();
+    // await expect(btn).toHaveHrefContaining('.....'); once the page is ready we will add it
+    });
+
+    it('Expect YouTub video to exist', async () => {
+        await IndexPage.open();
+        const btn = await IndexPage.youtubeVideo;
+        await expect(btn).toExist();
     });
 });
