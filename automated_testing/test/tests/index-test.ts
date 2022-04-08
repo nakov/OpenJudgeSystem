3693f67ce8a6871dce74b0ebe6966d97f9469107
Mila Madzharova <madjarova.ml@gmail.com>
2022-04-08 17:56:17 +0200

    added test in progress https://github.com/SoftUni-Internal/OpenJudgeSystem/pull/440#

--- automated_testing/test/tests/index-test.ts
+++ automated_testing/test/tests/index-test.ts
@@ -1,7 +1,21 @@
 import IndexPage from '../pageobjects/index-page';
 
 describe('Testing index', () => {
-    it('Expect logInButton to exist', async () => {
+    const activeContestCards = async () => {
+        await IndexPage.open();
+        const activeCards = await IndexPage.allContestCards[0];
+        return activeCards;
+    };
+
+    const card = {
+        header: IndexPage.contestCardHeader,
+        practiceTimeText: IndexPage.contestCardTimeText,
+        timer: IndexPage.contestCardTimer,
+        competeBtn: IndexPage.competeCardButton,
+        practiceBtn: IndexPage.practiceCardButton,
+    };
+
+    xit('Expect logInButton to exist', async () => {
         await IndexPage.open();
         const btn = await IndexPage.logInButton;
         await expect(btn).toExist();
@@ -9,7 +23,7 @@ describe('Testing index', () => {
         await expect(btn).toHaveHrefContaining('/login');
     });
 
-    it('Expect registerButton to exist', async () => {
+    xit('Expect registerButton to exist', async () => {
         await IndexPage.open();
         const btn = await IndexPage.registerButton;
         await expect(btn).toExist();
@@ -17,57 +31,139 @@ describe('Testing index', () => {
         await expect(btn).toHaveHrefContaining('/register');
     });
 
-    it('Expect navBar to have headerLogoLink with href', async () => {
+    xit('Expect navBar to have headerLogoLink with href', async () => {
         await IndexPage.open();
         const a = await IndexPage.headerLogoLink;
         await expect(a).toHaveHrefContaining('/');
     });
 
-    it('Expect to have imgTag with alt', async () => {
+    xit('Expect to have imgTag with alt', async () => {
         await IndexPage.open();
         const img = await IndexPage.headingImage;
         await expect(img).toExist();
         await expect(img).toHaveAttr('alt', 'softuni logo');
     });
 
-    it('Expect footer to exist', async () => {
+    xit('Expect footer to exist', async () => {
         await IndexPage.open();
         const footer = await IndexPage.footer;
         await expect(footer).toExist();
     });
 
-    it('Expect contests link in navigation to exist and have the correct href', async () => {
+    xit('Expect contests link in navigation to exist and have the correct href', async () => {
         await IndexPage.open();
         const contestsLink = await IndexPage.navContestsLink;
         await expect(contestsLink).toExist();
         await expect(contestsLink).toHaveHrefContaining('/contests/all');
     });
 
-    it('Expect submissions link in navigation to exist and have the correct href', async () => {
+    xit('Expect submissions link in navigation to exist and have the correct href', async () => {
         await IndexPage.open();
         const submissionsLink = await IndexPage.navSubmissionssLink;
         await expect(submissionsLink).toExist();
         await expect(submissionsLink).toHaveHrefContaining('/submissions');
     });
 
-    it('Expect "See contests" button in navigation to exist and have the correct href', async () => {
+    xit('Expect "See contests" button in navigation to exist and have the correct href', async () => {
         await IndexPage.open();
         const btn = await IndexPage.seeContestsButton;
         await expect(btn).toExist();
     //  await expect(btn).toHaveHrefContaining('.....'); once the page is ready we will add it
     });
 
-    it('Expect YouTub video to exist', async () => {
+    xit('Expect YouTub video to exist', async () => {
         await IndexPage.open();
         const video = await IndexPage.youtubeVideo;
         await expect(video).toExist();
     });
 
-    it('Expect Youtube video to have src', async () => {
+    xit('Expect Youtube video to have src', async () => {
         await IndexPage.open();
         const video = await IndexPage.youtubeVideo;
         await expect(video).toHaveAttr('src');
         const src = await video.getAttribute('src');
         await expect(src).not.toBeNull();
+    });
+
+    xit('Expect activeContest card to have disabled "Practice" button ', async () => {
+        await IndexPage.open();
+        const btn = await IndexPage.practiceCardButton;
+
+        await expect(btn).toExist();
+        await expect(btn).not.toBeClickable();
+    });
+
+    xit('Expect activeContest card to have active "Compete" button ', async () => {
+        await IndexPage.open();
+        const btn = await IndexPage.competeCardButton;
+
+        await expect(btn).toExist();
+        await expect(btn).toBeClickable();
+    });
+
+    it('Expect contest card to have displayed header', async () => {
+        await IndexPage.open();
+        const el = await IndexPage.contestCardHeader;
+
+        await expect(el).toExist();
+        await expect(el).toBeDisplayed;
+    });
+
+    it('Expect contest card to have displayed course category', async () => {
+        await IndexPage.open();
+        const el = await IndexPage.contestCardCategory;
+
+        await expect(el).toExist();
+        await expect(el).toBeDisplayed;
+    });
+
+    it('Expect activeContest card to have displayed message for NO practice time ', async () => {
+        await IndexPage.open();
+        const text = await IndexPage.contestCardTimeText;
+
+        await expect(text).toExist();
+        await expect(text).toBeDisplayed;
+        await expect(text).toEqual('No practice end time.');
+    });
+
+    it('Expect practice timer to be displayed in activeContest card ', async () => {
+        await IndexPage.open();
+        const text = await IndexPage.contestCardTimer;
+
+        await expect(text).not.toExist();
+        await expect(text).not.toBeDisplayed();
+    });
+
+    it('Expect pastContest card to have displayed message for practice time ', async () => {
+        await IndexPage.open();
+        const text = await IndexPage.contestCardTimeText;
+
+        await expect(text).toExist();
+        await expect(text).toBeDisplayed();
+        await expect(text).toEqual('Remaining time:');
+    });
+
+    it('Expect pastContest card to have displayed timer ', async () => {
+        await IndexPage.open();
+        const text = await IndexPage.contestCardTimer;
+
+        await expect(text).toExist();
+        await expect(text).toBeDisplayed();
+    });
+
+    xit('Expect pastContest to have active "Practice" button ', async () => {
+        await IndexPage.open();
+        const btn = await IndexPage.practiceCardButton;
+
+        await expect(btn).toExist();
+        await expect(btn).toBeClickable();
+    });
+
+    xit('Expect activeContest card to have disabled "Compete" button ', async () => {
+        await IndexPage.open();
+        const btn = await IndexPage.competeCardButton;
+
+        await expect(btn).toExist();
+        await expect(btn).not.toBeClickable();
     });
 });
