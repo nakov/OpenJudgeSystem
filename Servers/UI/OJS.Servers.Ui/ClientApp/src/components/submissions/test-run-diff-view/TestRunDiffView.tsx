7346c7b5b95660bd04c78741d7216b3402f13d7e
Simona Simeonova <simona.simeonova@softuni.bg>
2022-06-13 16:43:03 +0300

    Added missing file

---
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/submissions/test-run-diff-view/TestRunDiffView.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { ITestRunDetailsType } from '../../../hooks/submissions/types';
+import Diff from '../../Diff';
+
+interface ITestRunDiffViewProps {
+    testRun: ITestRunDetailsType;
+}
+
+const TestRunDiffView = ({ testRun }: ITestRunDiffViewProps) => (
+    <Diff expectedStr={testRun.expectedOutputFragment} actualStr={testRun.userOutputFragment} />
+);
+
+export default TestRunDiffView;
