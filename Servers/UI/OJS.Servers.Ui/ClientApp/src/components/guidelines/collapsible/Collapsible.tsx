dae657ac73ec3afa870c88b17eeb27b18bf35e6c
Simona Simeonova <simona.simeonova@softuni.bg>
2022-06-14 11:47:27 +0300

    Removed Collapsible container classname

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/guidelines/collapsible/Collapsible.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/guidelines/collapsible/Collapsible.tsx
@@ -6,23 +6,20 @@ import styles from './Collapsible.module.scss';
 
 interface ICollapsibleComponentProps extends IHaveChildrenProps {
     collapsed: boolean;
-    containerClassName?: string;
 }
 
 const Collapsible = ({
     collapsed,
-    containerClassName = '',
     children,
 }: ICollapsibleComponentProps) => {
     const [ internalCollapsed, setInternalCollapsed ] = useState(collapsed);
 
     const internalContainerClassName = useMemo(() => concatClassNames(
-        containerClassName,
         styles.collapsibleContainer,
         internalCollapsed
             ? styles.visible
             : styles.hidden,
-    ), [ containerClassName, internalCollapsed ]);
+    ), [ internalCollapsed ]);
 
     useEffect(() => {
         setInternalCollapsed(collapsed);
