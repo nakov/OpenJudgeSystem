39c0c492f651e8f42eb30866b6bab211affba40d
SIMONASIMEONOVA\User <simona.simeonova@softuni.bg>
2022-06-24 14:17:53 +0300

    Added statistics controller

---
+++ Servers/UI/OJS.Servers.Ui/Controllers/Api/StatisticsController.cs
@@ -0,0 +1,19 @@
+﻿namespace OJS.Servers.Ui.Controllers.Api;
+
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+public class StatisticsController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetForIndex()
+        => this.Ok(await Task.FromResult(new
+        {
+            usersCount = 15_000,
+            submissionsCount = 2_000_000,
+            submissionsPerMinute = 160,
+            contestsCount = 2000,
+            problemsCount = 10_000,
+            strategiesCount = 37,
+        }));
+}
