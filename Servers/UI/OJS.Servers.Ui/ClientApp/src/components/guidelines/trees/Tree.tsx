5dc6a675f4cf0b5ebc14f68f0769380110309450
Petar Neshkov <petar.neshkov@softuni.bg>
2022-09-20 11:06:28 +0300

    Implemented tooltip and condensed categories https://github.com/SoftUni-Internal/exam-systems-issues/issues/182

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/guidelines/trees/Tree.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/guidelines/trees/Tree.tsx
@@ -3,6 +3,9 @@ import { TreeItem, TreeView } from '@material-ui/lab';
 import { MdChevronRight, MdExpandMore } from 'react-icons/md';
 import { isArray, isEmpty } from 'lodash';
 
+import concatClassNames from '../../../utils/class-names';
+import styles from './Tree.module.scss';
+
 interface ITreeItemType {
     id: string,
     name: string,
@@ -22,7 +25,7 @@ const Tree = ({
     onTreeLabelClick,
     defaultSelected,
     defaultExpanded = [],
-} : ITreeProps) => {
+}: ITreeProps) => {
     const [ expanded, setExpanded ] = useState([] as string[]);
     const [ selected, setSelected ] = useState('');
 
@@ -69,30 +72,50 @@ const Tree = ({
         [ defaultExpanded, expanded ],
     );
 
+    const categoriesTree = 'tree-Header';
+    const categoriesTreeClassName = concatClassNames(categoriesTree, styles.categoriesTree);
+    const treeElement = 'tree-Element';
+    const treeElementClassName = concatClassNames(treeElement, styles.treeElement);
+    const treeTooltip = 'tooltip';
+    const treeTooltipClassName = concatClassNames(treeTooltip, styles.tooltip);
+    const treeTooltipElement = 'tooltip-Element';
+    const treeTooltipElementClassName = concatClassNames(treeTooltipElement, styles.tooltipElement);
+
     const renderTree = useCallback((node: ITreeItemType) => (
-        <TreeItem
-          key={node.id}
-          nodeId={node.id.toString()}
-          label={node.name}
-          onClick={() => handleTreeItemClick(node)}
-          onLabelClick={() => handleLabelClick(node)}
-        >
-            {isArray(node.children)
-                ? node.children.map((child) => renderTree(child))
-                : null}
-        </TreeItem>
-    ), [ handleLabelClick, handleTreeItemClick ]);
+        <div className={categoriesTreeClassName}>
+            <div className={treeTooltipClassName}>
+                <div className={treeTooltipElementClassName}
+                     key={node.id}>
+                    {node.name}
+                </div>
+            </div>
+            <TreeItem
+                key={node.id}
+                className={treeElementClassName}
+                nodeId={node.id.toString()}
+                label={node.name}
+                onClick={() => handleTreeItemClick(node)}
+                onLabelClick={() => handleLabelClick(node)}
+            >
+                {isArray(node.children)
+                    ? node.children.map((child) => renderTree(child))
+                    : null}
+            </TreeItem>
+        </div>
+    ), [ categoriesTreeClassName, treeTooltipClassName, treeTooltipElementClassName,
+        treeElementClassName, handleLabelClick, handleTreeItemClick ]);
 
     const renderTreeView = (treeItems: ITreeItemType[]) => treeItems.map((c) => renderTree(c));
 
     return (
         <TreeView
-          aria-label="rich object"
-          defaultCollapseIcon={<MdExpandMore />}
-          defaultExpandIcon={<MdChevronRight />}
-          selected={selected}
-          expanded={expanded}
+            aria-label="rich object"
+            defaultCollapseIcon={<MdExpandMore/>}
+            defaultExpandIcon={<MdChevronRight/>}
+            selected={selected}
+            expanded={expanded}
         >
+            <div className={treeTooltipClassName}> {renderTreeView(items)}</div>
             {renderTreeView(items)}
         </TreeView>
     );
