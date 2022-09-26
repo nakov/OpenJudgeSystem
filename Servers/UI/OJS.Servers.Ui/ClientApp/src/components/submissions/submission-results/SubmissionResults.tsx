ec4c147c13282266ef60da256e06ae3bc17b4cb2
msmner <ivaylo.nikolov88@gmail.com>
2022-09-26 18:19:37 +0300

    changed sorting function name and used ternary operator

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/submissions/submission-results/SubmissionResults.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/submissions/submission-results/SubmissionResults.tsx
@@ -8,30 +8,27 @@ interface ISubmissionResultsProps {
     testRuns: ITestRunDetailsType[];
 }
 
-const SubmissionResults = ({ testRuns } : ISubmissionResultsProps) => {
+const SubmissionResults = ({ testRuns }: ISubmissionResultsProps) => {
     const renderTestRunsDetails = useCallback((run: ITestRunDetailsType) => (
-        <TestRunDetails testRun={run} />
+        <TestRunDetails testRun={run}/>
     ), []);
 
-    const compareByOrderByAsc = (tr1: ITestRunDetailsType, tr2: ITestRunDetailsType) => {
-        if(tr1.isTrialTest === tr2.isTrialTest)
-        {return tr1.orderBy-tr2.orderBy;}
-
-        if(tr1.isTrialTest)
-        {return -1;}
-
-        return 1;
-    };
+    const compareByTestTypeAndOrderByAsc = (tr1: ITestRunDetailsType, tr2: ITestRunDetailsType) =>
+        tr1.isTrialTest === tr2.isTrialTest
+            ? tr1.orderBy - tr2.orderBy
+            : tr1.isTrialTest
+                ? -1
+                : 1;
 
     return (
         <List
-          values={testRuns.sort(compareByOrderByAsc)}
-          className="submissionTestRunsList"
-          itemClassName="testRunDetails"
-          orientation={Orientation.vertical}
-          itemFunc={renderTestRunsDetails}
-          type={ListType.normal}
-          fullWidth
+            values={testRuns.sort(compareByTestTypeAndOrderByAsc)}
+            className="submissionTestRunsList"
+            itemClassName="testRunDetails"
+            orientation={Orientation.vertical}
+            itemFunc={renderTestRunsDetails}
+            type={ListType.normal}
+            fullWidth
         />
     );
 };
