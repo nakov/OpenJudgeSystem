346cb1e10df3b33df222ef9f72ef1bab491d1f86
Mihail Petrov <43758817+mishogv@users.noreply.github.com>
2022-10-10 14:45:14 +0300

    Added script to support ExpandableMultiChoiceCheckBoxFormControlViewModel https://github.com/SoftUni-Internal/exam-systems-issues/issues/279

--- Servers/Administration/OJS.Servers.Administration/wwwroot/js/mvc-grid-form.js
+++ Servers/Administration/OJS.Servers.Administration/wwwroot/js/mvc-grid-form.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $(":checkbox").on('click', function(event) {
+$(document).ready(function () {
+    $(":checkbox").on('click', function (event) {
         if (event.target.value === "True") {
             event.target.value = "False"
         } else {
@@ -7,24 +7,45 @@ $(document).ready(function() {
         }
     });
 
+    $(".expandable").on('click', function (event) {
+        const expandElementId = $(event.target).attr("expand");
+
+        $(`#${expandElementId}`).attr('class').includes('d-none')
+            ? $(`#${expandElementId}`).removeClass('d-none')
+            : $(`#${expandElementId}`).addClass('d-none');
+    });
+
     const forms = $('form');
 
     forms.find('select').select2();
 
-    forms.submit(function(ev) {
+    forms.submit(function (ev) {
         const form = $(ev.target);
 
         form.find('select').prop('disabled', false);
 
         form.find('fieldset').each(function (ev, fs) {
             const selectedCheckboxes = $(fs).find('input[type=checkbox]').toArray();
+
+            const getExpandElement = (checkbox) => {
+                const expandElement = $('#' + $(checkbox).attr("expand") + ' :input')[0];
+
+                return expandElement && expandElement.name && expandElement.value
+                    ? {name: expandElement.name, value: expandElement.value}
+                    : null;
+            };
+
             const result = selectedCheckboxes.map(x => ({
                 name: $(x).data("name"),
                 value: $(x).data("value"),
-                isChecked: $(x).is(":checked")}));
+                isChecked: $(x).is(":checked"),
+                expand: getExpandElement(x)
+            }));
+
             const input = $(`<input name="${fs.name}">`)
-                .attr('type','hidden')
+                .attr('type', 'hidden')
                 .val(JSON.stringify(result));
+
             form.append(input);
         });
 
