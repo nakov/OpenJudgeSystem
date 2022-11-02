a793a0e48403e7058a6fe01a146832b04c172d31
Petar Neshkov <petar.neshkov@softuni.bg>
2022-11-02 09:25:04 +0200

    Improved styling in SubmissionsList.tsx

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/submissions/submissions-list/SubmissionsList.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/submissions/submissions-list/SubmissionsList.tsx
@@ -75,9 +75,9 @@ const SubmissionsList = ({
             <div className={itemClassName}>
                 <div className={styles.infoContainer}>
                     <SubmissionResultPointsLabel
-                      points={points}
-                      maximumPoints={maximumPoints}
-                      isProcessed={isProcessed}
+                        points={points}
+                        maximumPoints={maximumPoints}
+                        isProcessed={isProcessed}
                     />
                     <p className={styles.submissionCreatedOnParagraph}>{formatDate(createdOn)}</p>
                 </div>
@@ -85,14 +85,14 @@ const SubmissionsList = ({
                     <Text>
                         {submissionType}
                     </Text>
-                    <Label type={LabelType.plain} text={typeLabelText} className={submissionsLabelTypeClassName} />
+                    <Label type={LabelType.plain} text={typeLabelText} className={submissionsLabelTypeClassName}/>
                     <LinkButton
-                      size={ButtonSize.small}
-                      to={`/submissions/${id}/details`}
-                      className={submissionBtnClass}
-                      type={LinkButtonType.secondary}
-                      text="Details"
-                      state={buttonState}
+                        size={ButtonSize.small}
+                        to={`/submissions/${id}/details`}
+                        className={submissionBtnClass}
+                        type={LinkButtonType.secondary}
+                        text="Details"
+                        state={buttonState}
                     />
                 </div>
             </div>
@@ -102,14 +102,14 @@ const SubmissionsList = ({
     return (
         <div className={containerClassName}>
             <List
-              values={items}
-              className={submissionsListClassName}
-              itemClassName={submissionListItemClassName}
-              itemFunc={renderSubmissionListItem}
-              type={ListType.normal}
-              orientation={Orientation.vertical}
-              fullWidth
-              scrollable={false}
+                values={items}
+                className={submissionsListClassName}
+                itemClassName={submissionListItemClassName}
+                itemFunc={renderSubmissionListItem}
+                type={ListType.normal}
+                orientation={Orientation.vertical}
+                fullWidth
+                scrollable={false}
             />
         </div>
     );
