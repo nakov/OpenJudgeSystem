851e4e5015e66bde5d5a2e9ff8f44b971b87befa
Mila Madzharova <madjarova.ml@gmail.com>
2022-11-02 14:32:04 +0200

    fix

--- automated_testing/restore/restore_db/queries/013__[HangFire].[Server].sql
+++ automated_testing/restore/restore_db/queries/013__[HangFire].[Server].sql
@@ -7,8 +7,8 @@ IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [object_id] = OBJECT_ID(
 GO
 
 INSERT INTO [HangFire].[Server] ([Id], [Data], [LastHeartbeat])
-VALUES (N'81f05525987b:8:d6c9a083-d787-4a71-8e5a-3a9c3d37e837', N'{"WorkerCount":20,"Queues":["default"],"StartedAt":"2022-09-28T14:40:46.6712883Z"}', CAST(N'2022-09-28T14:40:46+00:00' AS DateTime2)),
-(N'ebe504589a49:7:08e5cd44-e02d-41c2-a7e7-d1af64be655f', N'{"WorkerCount":20,"Queues":["default"],"StartedAt":"2022-09-28T14:40:46.4761015Z"}', CAST(N'2022-09-28T14:40:46+00:00' AS DateTime2))
+VALUES (N'7ff4fea6629f:8:eacb9fa0-a8a5-4c78-98df-9381fecdf242', N'{"WorkerCount":20,"Queues":["default"],"StartedAt":"2022-11-02T11:02:57.2859918Z"}', CAST(N'2022-11-02T11:03:27+00:00' AS DateTime2)),
+(N'88bb6d84479e:7:6b2d5fdd-1f7a-4262-ad76-659b5cb5c4c3', N'{"WorkerCount":20,"Queues":["default"],"StartedAt":"2022-11-02T11:02:57.9566177Z"}', CAST(N'2022-11-02T11:03:28+00:00' AS DateTime2))
 GO
 
 IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [object_id] = OBJECT_ID(N'[HangFire].[Server]'))
