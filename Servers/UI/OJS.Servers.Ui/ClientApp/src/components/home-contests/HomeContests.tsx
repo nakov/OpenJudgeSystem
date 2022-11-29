c746e0ec04e0c039ab0f691fd688725ae19b947f
msmner <ivaylo.nikolov88@gmail.com>
2022-11-29 19:21:15 +0200

    change default contests to take to 3 from 4

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/home-contests/HomeContests.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/home-contests/HomeContests.tsx
@@ -1,13 +1,12 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 import { ContestStatus } from '../../common/contest-types';
-import { IIndexContestsType, useHomeContests } from '../../hooks/use-home-contests';
+import { useHomeContests } from '../../hooks/use-home-contests';
 import { setLayout } from '../../pages/shared/set-layout';
 
 import ContestsList from './ContestsList';
 
 const HomeContests = () => {
-    const maxContestsToDisplay = 3;
     const {
         state: {
             activeContests,
@@ -15,11 +14,6 @@ const HomeContests = () => {
         },
         actions: { getForHome },
     } = useHomeContests();
-
-    const contestsToDisplay = useCallback(
-        (contests: IIndexContestsType[]) => contests.slice(0, maxContestsToDisplay),
-        [],
-    );
 
     useEffect(() => {
         (async () => {
@@ -31,12 +25,12 @@ const HomeContests = () => {
         <>
             <ContestsList
               title="Active"
-              contests={contestsToDisplay(activeContests)}
+              contests={activeContests}
               contestState={ContestStatus.Active}
             />
             <ContestsList
               title="Past"
-              contests={contestsToDisplay(pastContests)}
+              contests={pastContests}
               contestState={ContestStatus.Past}
             />
         </>
