e3af24ec01fa82243f0fe09bda62a35309fbc6d5
Mihail Petrov <43758817+mishogv@users.noreply.github.com>
2023-01-03 14:34:28 +0200

    Remove migration for clean up from DB https://github.com/SoftUni-Internal/exam-systems-issues/issues/342

--- Data/OJS.Data.Models/Problems/Problem.cs
+++ Data/OJS.Data.Models/Problems/Problem.cs
@@ -44,6 +44,11 @@ namespace OJS.Data.Models.Problems
         public double OrderBy { get; set; }
 
         /// <summary>
+        /// Gets or sets a predefined skeleton for the task
+        /// </summary>
+        public byte[]? SolutionSkeleton { get; set; }
+
+        /// <summary>
         /// Gets or sets Problem specific dependencies that will be compiled and executed with the user code
         /// such as Solution skeletons, mocks or data and text files.
         /// </summary>
