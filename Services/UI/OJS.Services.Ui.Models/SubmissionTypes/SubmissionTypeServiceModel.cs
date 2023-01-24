3dc923d04c4c108a4f6488e88fecccb315b3ad43
msmner <ivaylo.nikolov88@gmail.com>
2023-01-24 17:29:19 +0200

    fix indentation

--- Services/UI/OJS.Services.Ui.Models/SubmissionTypes/SubmissionTypeServiceModel.cs
+++ Services/UI/OJS.Services.Ui.Models/SubmissionTypes/SubmissionTypeServiceModel.cs
@@ -12,15 +12,15 @@
 
         public int Id { get; set; }
 
-    public string Name { get; set; } = null!;
+        public string Name { get; set; } = null!;
 
-    public bool IsSelectedByDefault { get; set; }
+        public bool IsSelectedByDefault { get; set; }
 
-    public bool AllowBinaryFilesUpload { get; set; }
+        public bool AllowBinaryFilesUpload { get; set; }
 
-    public IEnumerable<string> AllowedFileExtensions { get; set; } = new List<string>();
+        public IEnumerable<string> AllowedFileExtensions { get; set; } = new List<string>();
 
-    public void RegisterMappings(IProfileExpression configuration) =>
+        public void RegisterMappings(IProfileExpression configuration) =>
             configuration.CreateMap<SubmissionType, SubmissionTypeServiceModel>()
                 .ForMember(
                     d => d.AllowedFileExtensions,
