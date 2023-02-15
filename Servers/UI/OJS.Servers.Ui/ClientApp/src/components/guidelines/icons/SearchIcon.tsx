b781f23b08968ec60130c6bf4fb60891c38aff74
PetarNeshkov <petio567@abv.bg>
2023-02-15 18:36:13 +0200

    Changed the styling of the input search

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/guidelines/icons/SearchIcon.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/guidelines/icons/SearchIcon.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { BsSearch } from 'react-icons/bs';
 
+import concatClassNames from '../../../utils/class-names';
+
 import IconSize from './common/icon-sizes';
 import Icon, { IIconProps } from './Icon';
+
+import styles from './SearchIcon.module.scss';
 
 type IExpandMoreIconProps = IIconProps
 
 const SearchIcon = ({
     className = '',
-    size = IconSize.Medium,
+    size = IconSize.Large,
     helperText = '',
 }: IExpandMoreIconProps) => (
     <Icon
-      className={className}
+      className={concatClassNames(styles.searchIcon, className)}
       size={size}
       helperText={helperText}
       Component={BsSearch}
