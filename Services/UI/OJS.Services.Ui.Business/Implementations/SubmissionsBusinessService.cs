f5cec4e2cbc20c16a18ff6631016fda107ec72af
msmner <ivaylo.nikolov88@gmail.com>
2023-04-26 10:26:13 +0300

    merge commit

--- Services/UI/OJS.Services.Ui.Business/Implementations/SubmissionsBusinessService.cs
+++ Services/UI/OJS.Services.Ui.Business/Implementations/SubmissionsBusinessService.cs
@@ -21,7 +21,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using OJS.Services.Ui.Business.Validations.Implementations.Contests;
 
 using static Constants.PublicSubmissions;
 
