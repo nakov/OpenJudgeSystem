cb1b2479e028ba76ad1400b84fa9ba7e70812552
Viktor Kazakov <viktor.kazakov@softuni.bg>
2023-05-09 19:32:53 +0300

    Added EnsureValidAuthorSubmisions property to Contest model Added GetSumbissionTypesForProblemsWithCurrentAuthorSolution method to ContestDataService Added EnsureValidAuthorSubmisions property to Contest/ContestAdministrationViewModel and _ContestEditor https://github.com/SoftUni-Internal/exam-systems-issues/issues/588

---
+++ Open Judge System/Services/OJS.Services/Data/Submissions/Models/SubmissionTypeForProblem.cs
@@ -0,0 +1,19 @@
+﻿using OJS.Data.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace OJS.Services.Data.Submissions.Models
+{
+    public class SubmissionTypeForProblem
+    {
+        public int ProblemId { get; set; }
+
+        public string ProblemName { get; set; }
+
+        public int SubmissionTypeId { get; set; }
+
+        public string SubmissionTypeName { get; set; }
+
+        public bool HasAuthorSubmission { get; set; }
+    }
+}
