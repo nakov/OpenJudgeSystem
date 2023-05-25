11aa3ea9460ba5d60cf65151dfd724f5445f142c
PetarNeshkov <petio567@abv.bg>
2023-05-25 19:05:26 +0300

    Added validation for Admin services

--- Servers/Administration/OJS.Servers.Administration.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Servers/Administration/OJS.Servers.Administration.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -3,12 +3,14 @@ namespace OJS.Servers.Administration.Infrastructure.Extensions
     using AutoCrudAdmin.Extensions;
     using Microsoft.Extensions.DependencyInjection;
     using OJS.Common.Enumerations;
+    using OJS.Common.Utils;
     using OJS.Data;
     using OJS.Data.Models.Users;
     using OJS.Servers.Infrastructure.Extensions;
     using SoftUni.Data.Infrastructure.Enumerations;
     using SoftUni.Judge.Common.Extensions;
     using System.Linq;
+    using static OJS.Common.GlobalConstants;
 
     public static class ServiceCollectionExtensions
     {
@@ -18,10 +20,28 @@ namespace OJS.Servers.Administration.Infrastructure.Extensions
             => services
                 .AddWebServer<TProgram>()
                 .AddHangfireServer(AppName)
+                .ValidateLaunchSettings()
                 .AddIdentityDatabase<AdminDbContext, UserProfile, Role, UserInRole>(Enumerable.Empty<GlobalQueryFilterType>())
                 .AddScoped<OjsDbContext, AdminDbContext>()
                 .AddSoftUniJudgeCommonServices()
                 .UseAutoCrudAdmin()
                 .AddControllersWithViews();
+
+        private static IServiceCollection ValidateLaunchSettings(this IServiceCollection services)
+        {
+            var requiredConfigValues = new[]
+            {
+                EnvironmentVariables.DistributorBaseUrlKey,
+                EnvironmentVariables.ApplicationUrl,
+                EnvironmentVariables.PathToCommonKeyRingFolderKey,
+                EnvironmentVariables.RedisConnectionString,
+                EnvironmentVariables.SharedAuthCookieDomain,
+                EnvironmentVariables.LocalTimeZone,
+            };
+
+            EnvironmentUtils.ValidateEnvironmentVariableExists(requiredConfigValues);
+
+            return services;
+        }
     }
 }
