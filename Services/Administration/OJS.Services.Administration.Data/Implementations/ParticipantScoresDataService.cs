9ed0c158dd9e18ab2ea8fabd64216b22ae7aaed0
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2023-06-15 12:29:25 +0300

    Fixed a bug in which user can enter contest after timer runs out (#663)
    * Fixed a bug in which user can enter contest after the timer runs out.
    
    * Fixed a bug where contest is displayed as expired when it shouldn't be
    
    * Fixed a bug in which timer and active participants are inconsistent after reload of the page on local run.
    
    * Fixed a bug in which ContestParticipationServiceModel.cs throws an error.
    
    * Check for null participationEndTime
    
    * Added contest endTime validation for OnsitePracticalExam
    
    * Removed Interceptor and added UTC validations
    
    * Refactored contest validation to use dateService in ContestValidationService
    
    * Changed ParticipantBusinessService to use IDateService
    
    * Converted values to be UTC in database and Local on Administration.
    
    * PR changes related to naming conventions and abstract
    
    * Naming convention.
    
    * Converted to DateTimeKind.Unspecified
    
    * Added validation for Admin services
    
    * Fixed environment variables abstractions
    
    * Added new version of AutoCrudAdmin which updates the edit and delete binding forms
    
    * Update autocrudadmin version
    
    * Removed Youtube validation in UI
    
    * Brought back environment validations in UI
    
    * Refactored LaunchSettingsValidation to be common
    
    * Added docker env
    
    * Fixed a bug in which DateTimes in certain properties does not work as appropriate
    
    * Removed unused validation
    
    * Implemented UtcConvertableAttribute
    
    * Refactored logic to AdminDbContext and created LocalDateConverter
    
    * Added documentation for Converters and the attribute
    
    * Removed duplicate configuration
    
    ---------
    
    Co-authored-by: Petar Neshkov <petar.neshkov@softuni.bg>

--- Services/Administration/OJS.Services.Administration.Data/Implementations/ParticipantScoresDataService.cs
+++ Services/Administration/OJS.Services.Administration.Data/Implementations/ParticipantScoresDataService.cs
@@ -14,7 +14,7 @@ namespace OJS.Services.Administration.Data.Implementations
         private readonly IParticipantsDataService participantsData;
 
         public ParticipantScoresDataService(
-            OjsDbContext db,
+            AdminDbContext db,
             IParticipantsDataService participantsData)
             : base(db)
             => this.participantsData = participantsData;
