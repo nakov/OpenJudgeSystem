9debe0b4c3add0da0d15e8e9c4fe85ed0efde71c
PetarNeshkov <71330136+PetarNeshkov@users.noreply.github.com>
2023-06-27 10:44:57 +0300

    Fixed Autocrud edit date format (#693)
    * Applied Edit Form Changes
    
    * added new package
    
    * Refactored global format to be in 'dd/MM/yyyy' variant
    
    ---------
    
    Co-authored-by: Petar Neshkov <petar.neshkov@softuni.bg>

--- Servers/Administration/OJS.Servers.Administration.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Servers/Administration/OJS.Servers.Administration.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -3,7 +3,6 @@ namespace OJS.Servers.Administration.Infrastructure.Extensions
     using AutoCrudAdmin.Extensions;
     using Microsoft.Extensions.DependencyInjection;
     using OJS.Common.Enumerations;
-    using OJS.Common.Utils;
     using OJS.Data;
     using OJS.Data.Models.Users;
     using OJS.Servers.Infrastructure.Extensions;
@@ -25,6 +24,7 @@ namespace OJS.Servers.Administration.Infrastructure.Extensions
             services
                 .AddWebServer<TProgram>()
                 .AddHangfireServer(AppName)
+                .ConfigureGlobalDateFormat()
                 .ValidateLaunchSettings(RequiredConfigValues)
                 .AddIdentityDatabase<AdminDbContext, UserProfile, Role, UserInRole>(Enumerable.Empty<GlobalQueryFilterType>())
                 .AddScoped<OjsDbContext, AdminDbContext>()
