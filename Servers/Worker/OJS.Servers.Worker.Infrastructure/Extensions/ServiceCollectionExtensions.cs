a2556722ccd487d8c5bb6ab364650263f4bb4016
Georgi Georgiev <gogo4ds@gmail.com>
2023-07-11 14:06:05 +0300

    Fixed logger

--- Servers/Worker/OJS.Servers.Worker.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Servers/Worker/OJS.Servers.Worker.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,17 +1,14 @@
 ﻿namespace OJS.Servers.Worker.Infrastructure.Extensions;
 
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OJS.Common.Utils;
 using OJS.Servers.Infrastructure.Extensions;
 using SoftUni.AutoMapper.Infrastructure.Extensions;
 using OJS.Services.Worker.Models.Configuration;
-using SoftUni.Services.Infrastructure.Extensions;
 using OJS.Workers.Common;
 using OJS.Workers.SubmissionProcessors;
-using OJS.Servers.Infrastructure.Middleware;
 
 public static class ServiceCollectionExtensions
 {
@@ -29,14 +26,8 @@ public static class ServiceCollectionExtensions
             .AddConfiguration(configuration)
             .AddControllers();
 
-    public static WebApplication ConfigureWebApplication(
-        this WebApplication app,
-        string apiVersion)
+    public static WebApplication ConfigureWebApplication(this WebApplication app)
     {
-        // if (app.Environment.IsDevelopment())
-        // {
-        //     app.UseSwaggerDocs(apiVersion.ToApiName());
-        // }
         app.UseCustomExceptionHandling();
         app.UseAutoMapper();
         app.MapControllers();
@@ -48,33 +39,11 @@ public static class ServiceCollectionExtensions
         this IServiceCollection services)
         => services
             .AddTransient<ISubmissionExecutor>(sp => new SubmissionExecutor(
-                EnvironmentUtils.GetByKey(SubmissionsProcessorIdentifierNumberEnvVariableName) !));
+                EnvironmentUtils.GetRequiredByKey(SubmissionsProcessorIdentifierNumberEnvVariableName)));
 
     public static IServiceCollection AddConfiguration(
         this IServiceCollection services,
         IConfiguration configuration)
         => services
             .Configure<SubmissionExecutionConfig>(configuration.GetSection(nameof(SubmissionExecutionConfig)));
-
-    private static WebApplication UseCustomExceptionHandling(this WebApplication app)
-    {
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler(errorApp => errorApp.Run(new DevelopmentExceptionMiddleware().Get));
-        }
-        else
-        {
-            app.UseExceptionHandler(errorApp => errorApp.Run(new Rfc7807ExceptionMiddleware().Get));
-        }
-
-        return app;
-    }
-
-    private static IServiceCollection AddWebServerServices<TStartUp>(this IServiceCollection services)
-    {
-        services
-            .AddConventionServices<TStartUp>();
-
-        return services;
-    }
 }
--- Servers/Worker/OJS.Servers.Worker/Program.cs
+++ Servers/Worker/OJS.Servers.Worker/Program.cs
@@ -5,49 +5,11 @@
 
     internal class Program
     {
-        private const string ApiVersion = "v1";
-
         public static void Main(string[] args)
             => WebApplication.CreateBuilder(args)
                 .ConfigureBuilder<Program>()
                 .Build()
-                .ConfigureWebApplication(ApiVersion)
+                .ConfigureWebApplication()
                 .Run();
-
-//         private static readonly ApplicationName[] ApplicationUrls =
-//         {
-//             ApplicationName.Auth,
-//         };
-//
-//         public static async Task Main(string[] args)
-//         {
-// #if DEBUG
-//             // Enable the line below when you want to test
-//             // the application inside a docker container
-//             // SetDebugEnvironment();
-// #endif
-//             var host = WebHost
-//                 .CreateDefaultBuilder(args)
-//                 .BuildWebHostConfiguration<JudgeStartup>(
-//                     args,
-//                     applicationUrls: ApplicationUrls)
-//                 .Build();
-//
-//             await host.InitAsync();
-//
-//             await host.RunAsync();
-//         }
-//
-//         private static void SetDebugEnvironment()
-//         {
-//             if (OSPlatformHelpers.IsUnix())
-//             {
-//                 // Debug mode in Docker overrides environment to "Development"
-//                 // and we have to bring it back to "Docker" manually.
-//                 Environment.SetEnvironmentVariable(
-//                     EnvironmentVariables.EnvironmentKey,
-//                     EnvironmentVariables.DockerValue);
-//             }
-//         }
     }
 }
