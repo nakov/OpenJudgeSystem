eb80f4605dd47b775b16e1d7ef5ad61e35b7f6d8
msmner <ivaylo.nikolov88@gmail.com>
2023-07-12 15:55:29 +0300

    fix stylecop errors

--- Data/OJS.Data/Migrations/20221010092328_Add-solution-skeleton-per-submission-type.cs
+++ Data/OJS.Data/Migrations/20221010092328_Add-solution-skeleton-per-submission-type.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
-
-#nullable disable
+﻿#nullable disable
 
 namespace OJS.Data.Migrations
 {
+    using System;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
     public partial class Addsolutionskeletonpersubmissiontype : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
