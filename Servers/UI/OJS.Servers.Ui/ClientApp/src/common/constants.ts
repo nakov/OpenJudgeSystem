0050f02a620d8b974274339fb1ad06a0db19c10e
User\User_ <yvmarkovska@gmail.com>
2023-07-12 17:48:14 +0300

    Adjustments to Contests' sorting and filtering logic - Default Filter Status = All, sorting for categories follows old judge logic (by orderby and enddate) and fixing the sorting for the main page - shows contests with the soonest ending time https://github.com/SoftUni-Internal/exam-systems-issues/issues/745

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/common/constants.ts
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/common/constants.ts
@@ -2,7 +2,7 @@ import { ContestStatus, FilterType, SortType } from './contest-types';
 
 const DEFAULT_PROBLEM_RESULTS_TAKE_CONTESTS_PAGE = 4;
 const { Status: DEFAULT_FILTER_TYPE } = FilterType;
-const { Active: DEFAULT_STATUS_FILTER_TYPE } = ContestStatus;
+const { All: DEFAULT_STATUS_FILTER_TYPE } = ContestStatus;
 const { Sort: DEFAULT_SORT_FILTER_TYPE } = FilterType;
 const { OrderBy: DEFAULT_SORT_TYPE } = SortType;
 
