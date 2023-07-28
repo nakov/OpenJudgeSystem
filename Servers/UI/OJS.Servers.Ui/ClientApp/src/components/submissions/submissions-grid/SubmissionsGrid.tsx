bc48d15ee4346baafbb68a1d57ee1e1551d76b61
Petar Neshkov <petar.neshkov@softuni.bg>
2023-07-28 10:36:13 +0300

    Implemented css styling

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/submissions/submissions-grid/SubmissionsGrid.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/submissions/submissions-grid/SubmissionsGrid.tsx
@@ -1,9 +1,11 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
 import { IPublicSubmissionResponseModel, usePublicSubmissions } from '../../../hooks/submissions/use-public-submissions';
 import { useAuth } from '../../../hooks/use-auth';
 import { usePages } from '../../../hooks/use-pages';
+import generateId from '../../../utils/id-generator';
 import { format } from '../../../utils/number-utils';
+import Button, { ButtonType } from '../../guidelines/buttons/Button';
 import Heading, { HeadingType } from '../../guidelines/headings/Heading';
 import List from '../../guidelines/lists/List';
 import PaginationControls from '../../guidelines/pagination/PaginationControls';
@@ -12,6 +14,8 @@ import SubmissionGridRow from '../submission-grid-row/SubmissionGridRow';
 import styles from './SubmissionsGrid.module.scss';
 
 const SubmissionsGrid = () => {
+    const [ selectedActive, setSelectedActive ] = useState<boolean>(true);
+
     const {
         state: {
             publicSubmissions,
@@ -37,23 +41,80 @@ const SubmissionsGrid = () => {
         [],
     );
 
+    const btnId = useMemo(
+        () => {
+            const privilegedIdBtn = generateId();
+            return `btn-submit-${privilegedIdBtn}`;
+        },
+        [],
+    );
+
+    const handleShowSubmissionsInQueue = useCallback(
+        () => {
+            if (!selectedActive) {
+                setSelectedActive(true);
+            }
+        },
+        [ selectedActive ],
+    );
+
+    const handlePublicSubmissions = useCallback(
+        () => {
+            if (selectedActive) {
+                setSelectedActive(false);
+            }
+        },
+        [ selectedActive ],
+    );
+
     const { pagesCount } = pagesInfo;
     const renderPrivilegedComponent = useCallback(
         () => {
             const { isInRole } = user;
             if (isInRole) {
                 return (
-                    <PaginationControls
-                      count={pagesCount}
-                      page={currentPage}
-                      onChange={handlePageChange}
-                    />
+                    <>
+                        <Heading type={HeadingType.secondary}>
+                            Submissions in queue:
+                            0
+                            {' '}
+                            (
+                            <Button
+                              id={btnId}
+                              onClick={handleShowSubmissionsInQueue}
+                              type={ButtonType.submit}
+                              internalClassName={`${styles.privilegedButtonClassName} 
+                              ${selectedActive
+                                  ? `${styles.active}`
+                                  : ''}
+                             `}
+                              text="Show submissions in queue"
+                            />
+                            /
+                            <Button
+                              id={btnId}
+                              onClick={handlePublicSubmissions}
+                              internalClassName={`${styles.privilegedButtonClassName} 
+                              ${!selectedActive
+                                  ? `${styles.active}`
+                                  : ''}
+                             `}
+                              text="Show all solutions"
+                            />
+                            )
+                        </Heading>
+                        <PaginationControls
+                          count={pagesCount}
+                          page={currentPage}
+                          onChange={handlePageChange}
+                        />
+                    </>
                 );
             }
 
             return null;
         },
-        [ currentPage, handlePageChange, pagesCount, user ],
+        [ btnId, currentPage, handlePageChange, handlePublicSubmissions, handleShowSubmissionsInQueue, pagesCount, selectedActive, user ],
     );
 
     return (
