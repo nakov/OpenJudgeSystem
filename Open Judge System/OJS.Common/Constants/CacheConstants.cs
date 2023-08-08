557dbda4fec7c173faa97521d0cfa6670aa184fe
MTsatsarov <77450034+MTsatsarov@users.noreply.github.com>
2023-08-08 12:12:17 +0300

    Fix constants

--- Open Judge System/OJS.Common/Constants/CacheConstants.cs
+++ Open Judge System/OJS.Common/Constants/CacheConstants.cs
@@ -7,23 +7,23 @@
 
         public const string MainContestCategoriesDropDown = "MainContestCategoriesDropDown";
         public const string ContestCategoriesTree = "ContestCategoriesTree";
-        public const string ContestSubCategoriesFormat = "ContestSubCategories:id:{0}";
-        public const string ContestParentCategoriesFormat = "ContestParentCategories:id:{0}";
-        public const string ContestCategoryNameFormat = "ContestCategoryName:id:{0}";
+        public const string ContestSubCategoriesFormat = "ContestSubCategories:{0}";
+        public const string ContestParentCategoriesFormat = "ContestParentCategories:{0}";
+        public const string ContestCategoryNameFormat = "ContestCategoryName:{0}";
 
-        public const string SubmissionsCountByMonthsForPastElevenMonthsKey = "Submissions:count:by:month:for:past:eleven:months";
-        public const string SubmissionsCountForLastMonthKey = "Submissions:count:by:month:for:last:month";
+        public const string SubmissionsCountByMonthsForPastElevenMonthsKey = "SubmissionsCountByMonthForPastElevenMonths";
+        public const string SubmissionsCountForLastMonthKey = "SubmissionsCountByMonthForLastMonth";
 
         public const string ActiveContests = "ActiveContests";
         public const string PastContests = "PastContests";
 
         public const int DefaultPastContestsToTake = 15;
 
-        public static readonly string ContestResultsFormat = "Contest:id:{0}:official:{1}:full:{2}:export{3}";
+        public static readonly string ContestResultsFormat = "ContestId:{0}official:{1}full:{2}export{3}";
 
         public static readonly string ContestView = "Contest:{0}";
 
-        public static readonly string ResultsByProblem = "Results:By:Problem:id:{0}:official:{1}";
+        public static readonly string ResultsByProblem = "ResultsByProblem:{0}:official:{1}";
 
         public static readonly string KeysPrefix = "OJS:";
     }
