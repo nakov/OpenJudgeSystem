b9198c1cff0d7877ba50ab7499947624bbebc986
Bojidar Tonev <bojidar.tonev.9@gmail.com>
2023-08-11 15:41:54 +0300

    additional redesign submission details implementations

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/submissions/details/submission-result-details/SubmissionResultsDetails.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/submissions/details/submission-result-details/SubmissionResultsDetails.tsx
@@ -1,17 +1,20 @@
 ﻿import React from 'react';
 import Icon from '../../../guidelines/icons/Icon';
 
-import { BiCaretDown } from "react-icons/bi";
+import { BiCaretDown, BiCaretUp } from "react-icons/bi";
 import IconSize from "../../../guidelines/icons/common/icon-sizes";
+import TestRunDiffView from "../../test-run-diff-view/TestRunDiffView";
+import Collapsible from "../../../guidelines/collapsible/Collapsible";
 
 import styles from '../SubmissionDetails.module.scss';
 interface ISubmissionResultsDetails {
     testRuns?: any[]
 }
+
 const SubmissionResultsDetails = ({ testRuns }: ISubmissionResultsDetails) => {
-    console.log('test runs => ', testRuns);
+    const [ testRunDetailsCollapsed, setTestRunDetailsCollapsed ] = React.useState<object>({});
     const renderTestHeading = (testRun: any) => {
-        const isWrongAnswer = testRun.resultType === 'WrongAnswer';
+        const isWrongAnswer = testRun.resultType === 'WrongAnswer' || 'RunTimeError';
         let testRunText = `Test #${testRun.orderBy}`;
         if (testRun.isTrialTest) {
             testRunText = `Zero ${testRunText}`;
@@ -24,33 +27,50 @@ const SubmissionResultsDetails = ({ testRuns }: ISubmissionResultsDetails) => {
         </h2>);
     };
     
-    const renderShowInputButton = () => {
-        return(<div className={styles.showInputButton}>
-            <span>SHOW INPUT</span>
-            <Icon Component={BiCaretDown} size={IconSize.Large} className={styles.iconActiveColor} />
+    const renderShowButton = React.useCallback((id: string, isExpanded: boolean) => {
+        return(<div className={styles.showInputButton} onClick={() => setTestRunDetailsCollapsed({
+            ...testRunDetailsCollapsed,
+            [id]: {
+                    'isExpanded': !testRunDetailsCollapsed[id]?.isExpanded,
+                    'detailsExpanded': testRunDetailsCollapsed[id]?.detailsExpanded
+                }
+        })}>
+            <span>{isExpanded ? 'HIDE' : 'SHOW'} INPUT</span>
+            <Icon Component={isExpanded ? BiCaretUp : BiCaretDown} size={IconSize.Large} className={styles.iconActiveColor} />
         </div>)
-    };
+    }, [testRunDetailsCollapsed]);
     
-    const renderTrialTestData = () => {
+    const renderTrialTestData = React.useCallback((test: any) => {
+        const { isExpanded, detailsExpanded } = testRunDetailsCollapsed[test.id] || {};
+        
         return(<>
             <div className={styles.zeroTestDescription}>The zero tests are not included in the final result.</div>
-            {renderShowInputButton()}
-            <div className={styles.zeroTestDataWrapper}>
-                <div className={styles.outputDiff}>
-                    <span>Expected output:</span>
-                </div>
-                <div className={styles.outputDiff}>
-                    <span>Received output:</span>
-                </div>
-            </div>
+            {test.showInput && renderShowButton(test.id, isExpanded)}
+            {test.showInput && (<div className={`${styles.collapsibleContainerWrapper} ${isExpanded ? 'expanded' : 'collapsed'}`}>
+                <Collapsible collapsed={isExpanded} className={styles.collapsibleElement}>
+                    <TestRunDiffView testRun={test} />
+                </Collapsible>
+                { test.executionComment && (<div 
+                    className={styles.showDetailsButton} 
+                    onClick={() => setTestRunDetailsCollapsed({
+                        ...testRunDetailsCollapsed,
+                        [test.id]: {
+                            'isExpanded': testRunDetailsCollapsed[test.id]?.isExpanded,
+                            'detailsExpanded': !testRunDetailsCollapsed[test.id]?.detailsExpanded
+                        }
+                })}>Show Details</div>) }
+            </div>)}
+            
+            { detailsExpanded && <span className={styles.executionComment}>{test.executionComment}</span> }
         </>);
-    }
+    }, [testRunDetailsCollapsed]);
     
     return(<div className={styles.submissionResultDetailsWrapper}>
         {testRuns?.map((test: any) => {
             return(<div key={`test-run-details-${test.id}`} className={styles.submissionResultDetails}>
                 {renderTestHeading(test)}
-                {test.isTrialTest && renderTrialTestData()}
+                {test.isTrialTest && renderTrialTestData(test)}
+                {test.showInput && test.executionComment && <span className={styles.executionComment}>{test.executionComment}</span>}
             </div>)
         })}
     </div>)
