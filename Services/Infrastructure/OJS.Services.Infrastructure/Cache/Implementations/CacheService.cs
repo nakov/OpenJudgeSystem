6e7e72746780a17be4734477884c096e2eb722b9
Mihail Tsatsarov <77450034+MTsatsarov@users.noreply.github.com>
2023-08-21 15:23:04 +0300

    Redis exception handling added. Email service implemented (#780)
    Closes
    https://github.com/SoftUni-Internal/exam-systems-issues/issues/796
    Closes
    https://github.com/SoftUni-Internal/exam-systems-issues/issues/795
    
    Implemented email service for sending emails.
    Added exception handling when redis is down.
    When ConnectionException occurs, it will be saved for 10 minutes in the
    memory cache and email will be sent.
    While the exceptions is in the memory cache, if it occurs again, no
    emails will be sent in order not flood the dev email.

--- Services/Infrastructure/OJS.Services.Infrastructure/Cache/Implementations/CacheService.cs
+++ Services/Infrastructure/OJS.Services.Infrastructure/Cache/Implementations/CacheService.cs
@@ -1,44 +1,95 @@
 namespace OJS.Services.Infrastructure.Cache.Implementations
 {
-    using FluentExtensions.Extensions;
-    using Microsoft.Extensions.Caching.Distributed;
-    using OJS.Common.Extensions.Strings;
-    using OJS.Services.Infrastructure.Constants;
     using System;
     using System.IO;
     using System.Threading.Tasks;
+    using FluentExtensions.Extensions;
+    using Microsoft.Extensions.Caching.Distributed;
+    using Microsoft.Extensions.Caching.Memory;
+    using Microsoft.Extensions.Options;
+    using OJS.Common.Extensions.Strings;
+    using OJS.Services.Common.Models.Configurations;
+    using OJS.Services.Infrastructure.Constants;
+    using OJS.Services.Infrastructure.Emails;
+    using StackExchange.Redis;
 
     public class CacheService : ICacheService
     {
+        private readonly int memoryCacheExpirationInSeconds = 10 * 60;
         private readonly IDistributedCache cache;
         private readonly IDatesService dates;
+        private readonly IMemoryCache memoryCache;
+        private readonly IEmailService emailService;
+        private readonly EmailServiceConfig emailConfig;
 
         public CacheService(
             IDistributedCache cache,
-            IDatesService dates)
+            IDatesService dates,
+            IMemoryCache memoryCache,
+            IEmailService emailService,
+            IOptions<EmailServiceConfig> emailConfig)
         {
             this.cache = cache;
             this.dates = dates;
+            this.memoryCache = memoryCache;
+            this.emailService = emailService;
+            this.emailConfig = emailConfig.Value;
         }
 
         public T Get<T>(string cacheId, Func<T> getItemCallback, DateTime absoluteExpiration)
         {
-            this.VerifyValueInCache(
-                    cacheId,
-                    () => Task.FromResult(getItemCallback),
-                    absoluteExpiration)
-                .Wait();
+            try
+            {
+                this.VerifyValueInCache(
+                        cacheId,
+                        () => Task.FromResult(getItemCallback),
+                        absoluteExpiration)
+                    .Wait();
 
-            return ParseValue<T>(this.cache.Get(cacheId));
+                return ParseValue<T>(this.cache.Get(cacheId));
+            }
+            catch (RedisConnectionException ex)
+            {
+                var exceptionTypeAsString = GetExceptionTypeAsString(ex);
+
+                if (this.ShouldSendExceptionEmail(
+                        exceptionTypeAsString,
+                        ex.Message))
+                {
+                    this.emailService.SendEmail(this.emailConfig.DevEmail, exceptionTypeAsString, ex.Message);
+                }
+
+                return getItemCallback();
+            }
         }
 
         public async Task<T> Get<T>(string cacheId, Func<Task<T>> getItemCallback, DateTime absoluteExpiration)
         {
-            await this.VerifyValueInCache(
-                cacheId,
-                getItemCallback,
-                absoluteExpiration);
-            return ParseValue<T>(await this.cache.GetAsync(cacheId));
+            try
+            {
+                await this.VerifyValueInCache(
+                    cacheId,
+                    getItemCallback,
+                    absoluteExpiration);
+
+                return ParseValue<T>(await this.cache.GetAsync(cacheId));
+            }
+            catch (RedisConnectionException ex)
+            {
+                var exceptionTypeAsString = GetExceptionTypeAsString(ex);
+
+                if (this.ShouldSendExceptionEmail(
+                        exceptionTypeAsString,
+                        ex.Message))
+                {
+                    await this.emailService.SendEmailAsync(
+                        this.emailConfig.DevEmail,
+                        exceptionTypeAsString,
+                        ex.Message);
+                }
+
+                return await getItemCallback();
+            }
         }
 
         public T Get<T>(string cacheId, Func<T> getItemCallback)
@@ -65,6 +116,8 @@ namespace OJS.Services.Infrastructure.Cache.Implementations
                 .ToJson()
                 .ToByteArray();
 
+        private static string GetExceptionTypeAsString(Exception exception) => exception.GetType().ToString();
+
         private DateTime GetAbsoluteExpirationByCacheSeconds(int cacheSeconds)
             => this.dates
                 .GetUtcNow()
@@ -86,6 +139,23 @@ namespace OJS.Services.Infrastructure.Cache.Implementations
                     cacheId,
                     ParseValue(result),
                     options);
+            }
+        }
+
+        private bool ShouldSendExceptionEmail(string exceptionName, string exceptionValue)
+        {
+            if (this.memoryCache.TryGetValue(exceptionName, out string cacheValue))
+            {
+                return false;
+            }
+            else
+            {
+                this.memoryCache.Set(
+                    exceptionName,
+                    exceptionValue,
+                    this.GetAbsoluteExpirationByCacheSeconds(this.memoryCacheExpirationInSeconds));
+
+                return true;
             }
         }
     }
