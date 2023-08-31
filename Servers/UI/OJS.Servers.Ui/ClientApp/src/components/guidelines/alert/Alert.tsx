1f9cbbf6785adf98cab973b9a29874581a0c4591
MTsatsarov <77450034+MTsatsarov@users.noreply.github.com>
2023-08-31 12:18:26 +0300

    Refactor alert component

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/guidelines/alert/Alert.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/guidelines/alert/Alert.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 import { Alert as MuiAlert, AlertTitle, Button, Snackbar } from '@mui/material';
 
+import './Alert.module.scss';
+
 interface IAlertProps {
-message:string;
-severity:AlertSeverity;
-onClose? :any;
-variant: AlertVariant;
-autoHideDuration?:number;
-vertical:AlertVerticalOrientation;
-horizontal:AlertHorizontalOrientation;
+    message:string;
+    severity:AlertSeverity;
+    onClose? :any;
+    variant: AlertVariant;
+    autoHideDuration?:number;
+    vertical:AlertVerticalOrientation;
+    horizontal:AlertHorizontalOrientation;
 }
 
  enum AlertVariant{
@@ -37,6 +39,10 @@ const Alert = (props:IAlertProps) => {
     const { message, severity, variant, onClose, autoHideDuration, vertical, horizontal } = props;
     const [ open, setOpen ] = useState<boolean>(true);
 
+    const alertClass = `${(severity === AlertSeverity.Success)}`
+        ? 'success-alert'
+        : '';
+    console.log(severity);
     const handleClose = () => {
         if (onClose) {
             onClose();
@@ -53,7 +59,7 @@ const Alert = (props:IAlertProps) => {
           onClick={handleClose}
         >
             <MuiAlert
-              sx={{ backgroundColor: `${(severity === AlertSeverity.Success)}` && 'rgb(245, 78, 84)' }}
+              className={`${alertClass}`}
               severity={severity}
               variant={variant}
               onClose={onClose}
