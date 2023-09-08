60d66942a9b93c9c75c7078688de4bd3a80fa70a
msmner <ivaylo.nikolov88@gmail.com>
2023-09-08 11:37:21 +0300

    fix

--- Services/Common/OJS.Services.Common/ISubmissionsCommonBusinessService.cs
+++ Services/Common/OJS.Services.Common/ISubmissionsCommonBusinessService.cs
@@ -3,8 +3,6 @@ namespace OJS.Services.Common;
 using System.Threading.Tasks;
 using OJS.Services.Common.Models.Submissions.ExecutionContext;
 using SoftUni.Services.Infrastructure;
-using OJS.Data.Models.Problems;
-using OJS.Data.Models.Submissions;
 
 public interface ISubmissionsCommonBusinessService : IService
 {
--- Services/Common/OJS.Services.Common/Implementations/SubmissionsCommonBusinessService.cs
+++ Services/Common/OJS.Services.Common/Implementations/SubmissionsCommonBusinessService.cs
@@ -5,28 +5,21 @@ using System.Threading.Tasks;
 using OJS.Services.Common.Data;
 using OJS.Services.Common.Models.Submissions.ExecutionContext;
 using Microsoft.Extensions.Logging;
-using OJS.Data.Models.Problems;
-using OJS.Data.Models.Submissions;
-using SoftUni.AutoMapper.Infrastructure.Extensions;
-using System.Linq;
 
 public class SubmissionsCommonBusinessService : ISubmissionsCommonBusinessService
 {
     private readonly ISubmissionPublisherService submissionPublisherService;
     private readonly ISubmissionsForProcessingCommonDataService submissionForProcessingData;
     private readonly ILogger<SubmissionsCommonBusinessService> logger;
-    private readonly IDataService<Submission> dataService;
 
     public SubmissionsCommonBusinessService(
         ISubmissionPublisherService submissionPublisherService,
         ISubmissionsForProcessingCommonDataService submissionForProcessingData,
-        ILogger<SubmissionsCommonBusinessService> logger,
-        IDataService<Submission> dataService)
+        ILogger<SubmissionsCommonBusinessService> logger)
     {
         this.submissionPublisherService = submissionPublisherService;
         this.submissionForProcessingData = submissionForProcessingData;
         this.logger = logger;
-        this.dataService = dataService;
     }
 
     public async Task PublishSubmissionForProcessing(SubmissionServiceModel submission)
