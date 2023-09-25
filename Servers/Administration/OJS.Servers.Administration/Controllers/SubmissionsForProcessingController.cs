f3b56c87cccd0b0ac1be885dc70a2b8ba798b76a
msmner <ivaylo.nikolov88@gmail.com>
2023-09-25 18:19:03 +0300

    add details as readonly

--- Servers/Administration/OJS.Servers.Administration/Controllers/SubmissionsForProcessingController.cs
+++ Servers/Administration/OJS.Servers.Administration/Controllers/SubmissionsForProcessingController.cs
@@ -4,9 +4,28 @@ using AutoCrudAdmin.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
 using OJS.Data.Models.Submissions;
+using System;
+using System.Linq.Expressions;
 
 public class SubmissionsForProcessing : BaseAutoCrudAdminController<SubmissionForProcessing>
 {
     protected override IEnumerable<GridAction> DefaultActions
-        => Enumerable.Empty<GridAction>();
+        => new[] { new GridAction { Action = nameof(this.Edit) } };
+
+    protected override IEnumerable<FormControlViewModel> GenerateFormControls(
+        SubmissionForProcessing entity,
+        EntityAction action,
+        IDictionary<string, string> entityDict,
+        IDictionary<string, Expression<Func<object, bool>>> complexOptionFilters,
+        Type autocompleteType)
+    {
+        var formControls = base.GenerateFormControls(entity, action, entityDict, complexOptionFilters, autocompleteType).ToList();
+
+        if (action == EntityAction.Edit)
+        {
+            formControls.ForEach(fc => fc.IsReadOnly = true);
+        }
+
+        return formControls;
+    }
 }
