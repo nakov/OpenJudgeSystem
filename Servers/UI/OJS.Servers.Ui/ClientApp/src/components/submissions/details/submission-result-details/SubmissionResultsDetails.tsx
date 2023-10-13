0988a1d44bda07e8a9ba9dc46f6cc957be30ec2e
msmner <ivaylo.nikolov88@gmail.com>
2023-10-13 12:44:22 +0300

    fix

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/submissions/details/submission-result-details/SubmissionResultsDetails.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/submissions/details/submission-result-details/SubmissionResultsDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
 import {
     ISubmissionResultsDetails,
@@ -79,6 +79,25 @@ const SubmissionResultsDetails = ({ testRuns }: ISubmissionResultsDetails) => {
             </h2>
         );
     };
+
+    const renderTestIds = useMemo(
+        (testRun: ITestCaseRun) => (
+            <div>
+                <span>
+                    Run
+                    #
+                    {testRun.id}
+                </span>
+                <br />
+                <span>
+                    Test
+                    #
+                    {testRun.testId}
+                </span>
+            </div>
+        ),
+        [],
+    );
 
     const renderShowInputButton = useCallback((id: number, isExpanded: boolean) => (
         <Button
