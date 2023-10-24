138f5ae2e4de4afbe3fff885cabd09dfbbc05a4d
Simona Simeonova <simona.simeonova@softuni.bg>
2023-10-24 14:15:09 +0300

    Removed redundant constructor

--- Services/UI/OJS.Services.Ui.Business/Implementations/HangfireBackgroundJobsBusinessService.cs
+++ Services/UI/OJS.Services.Ui.Business/Implementations/HangfireBackgroundJobsBusinessService.cs
@@ -4,10 +4,6 @@ using OJS.Services.Common;
 
 public class HangfireBackgroundJobsBusinessService : IHangfireBackgroundJobsBusinessService
 {
-    public HangfireBackgroundJobsBusinessService()
-    {
-    }
-
     // Method is executed by administration implementation
     public int EnqueuePendingSubmissions() => throw new System.NotImplementedException();
 
