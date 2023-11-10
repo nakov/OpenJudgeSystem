87c21af772813b757518a76b66ea05d9cd37e9ec
Mihail Tsatsarov <MihailTsatsarov92@gmail.com>
2023-11-10 15:18:46 +0200

    Progress

---
+++ Servers/UI/OJS.Servers.Ui/ClientApp/vite.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-unused-modules */
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import monacoEditorPlugin from 'vite-plugin-monaco-editor';
+
+export default defineConfig({
+    plugins: [
+        react(),
+        monacoEditorPlugin({ languages: [ 'python', 'javascript', 'csharp', 'java', 'cpp', 'go', 'php' ] }),
+    ],
+});
