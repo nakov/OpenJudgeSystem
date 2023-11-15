27cf75019a4c6f3af4d06b10bee9be6695e83440
SvetoslavUzunov <svetoslavuzunov99@gmail.com>
2023-11-15 07:02:43 +0200

    Included AutoMapper to ContestResultsModel.cs

--- Services/Administration/OJS.Services.Administration.Business/Implementations/ContestResultsAggregatorService.cs
+++ Services/Administration/OJS.Services.Administration.Business/Implementations/ContestResultsAggregatorService.cs
@@ -34,23 +34,28 @@ public class ContestResultsAggregatorService : IContestResultsAggregatorService
         var contestActivityEntity = this.activityService
             .GetContestActivity(contestResultsModel.Contest.Map<ContestForActivityServiceModel>());
 
-        var contestResults = new ContestResultsViewModel
-            {
-                Id = contestResultsModel.Contest.Id,
-                Name = contestResultsModel.Contest.Name,
-                IsCompete = contestResultsModel.Official,
-                ContestCanBeCompeted = contestActivityEntity.CanBeCompeted,
-                ContestCanBePracticed = contestActivityEntity.CanBePracticed,
-                UserHasContestRights = contestResultsModel.IsUserAdminOrLecturer,
-                ContestType = contestResultsModel.Contest.Type,
-                Problems = contestResultsModel.Contest.ProblemGroups
-                    .SelectMany(pg => pg.Problems)
-                    .AsQueryable()
-                    .Where(p => !p.IsDeleted)
-                    .OrderBy(p => p.OrderBy)
-                    .ThenBy(p => p.Name)
-                    .Select(ContestProblemListViewModel.FromProblem),
-            };
+        var contestResults = contestResultsModel.Map<ContestResultsViewModel>();
+
+        contestResults.ContestCanBeCompeted = contestActivityEntity.CanBeCompeted;
+        contestResults.ContestCanBePracticed = contestActivityEntity.CanBePracticed;
+
+        // var contestResults = new ContestResultsViewModel
+        //     {
+        //         Id = contestResultsModel.Contest.Id,
+        //         Name = contestResultsModel.Contest.Name,
+        //         IsCompete = contestResultsModel.Official,
+        //         ContestCanBeCompeted = contestActivityEntity.CanBeCompeted,
+        //         ContestCanBePracticed = contestActivityEntity.CanBePracticed,
+        //         UserHasContestRights = contestResultsModel.IsUserAdminOrLecturer,
+        //         ContestType = contestResultsModel.Contest.Type,
+        //         Problems = contestResultsModel.Contest.ProblemGroups
+        //             .SelectMany(pg => pg.Problems)
+        //             .AsQueryable()
+        //             .Where(p => !p.IsDeleted)
+        //             .OrderBy(p => p.OrderBy)
+        //             .ThenBy(p => p.Name)
+        //             .Select(ContestProblemListViewModel.FromProblem),
+        //     };
 
         var totalParticipantsCount = contestResultsModel.TotalResultsCount
                                      ?? this.participantsData
--- Services/UI/OJS.Services.Ui.Models/Contests/ContestResultsModel.cs
+++ Services/UI/OJS.Services.Ui.Models/Contests/ContestResultsModel.cs
@@ -1,8 +1,10 @@
 ﻿namespace OJS.Services.Ui.Models.Contests;
 
 using AutoMapper;
-using SoftUni.AutoMapper.Infrastructure.Models;
 using OJS.Data.Models.Contests;
+using OJS.Services.Common.Models.Contests.Results;
+using SoftUni.AutoMapper.Infrastructure.Models;
+using System.Linq;
 
 public class ContestResultsModel : IMapExplicitly
 {
@@ -21,5 +23,30 @@ public class ContestResultsModel : IMapExplicitly
     public int Page { get; set; } = 1;
 
     public int ItemsInPage { get; set; } = int.MaxValue;
-    public void RegisterMappings(IProfileExpression configuration) => throw new System.NotImplementedException();
+
+    public void RegisterMappings(IProfileExpression configuration) =>
+        configuration.CreateMap<ContestResultsViewModel, ContestResultsModel>()
+        .ForMember(
+            d => d.Official,
+            opt => opt.MapFrom(s => s.IsCompete))
+        .ForMember(
+            d => d.Contest.Name,
+            opt => opt.MapFrom(s => s.Name))
+        .ForMember(
+            d => d.IsUserAdminOrLecturer,
+            opt => opt.MapFrom(s => s.UserHasContestRights))
+        .ForMember(
+            d => d.Contest.Type,
+            opt => opt.MapFrom(s => s.ContestType))
+        .ForMember(
+            d => d.Contest.ProblemGroups
+                .SelectMany(pg => pg.Problems)
+                .AsQueryable()
+                .Where(p => !p.IsDeleted)
+                .OrderBy(p => p.OrderBy)
+                .ThenBy(p => p.Name)
+                .Select(ContestProblemListViewModel.FromProblem),
+            opt =>
+                opt.MapFrom(s => s.Problems))
+        .ReverseMap();
 }
