4ef40786364fecd32ee2b8dab1f870536aa99783
Georgi Georgiev <gogo4ds@gmail.com>
2023-11-17 17:04:46 +0200

    Fixed AutoCrudAdmin exception with Actvator.CreateInstance for DbContext

--- Data/OJS.Data/OjsDbContext.cs
+++ Data/OJS.Data/OjsDbContext.cs
@@ -19,9 +19,21 @@ namespace OJS.Data
     {
         private readonly IGlobalQueryFilterTypesCache? globalQueryFilterTypesCache;
 
+        // Used by AutoCrudAdmin with Activator.CreateInstance for getting Metadata about the context.
+        // In OnConfiguring in memory database is used when DbContext is created from
+        // the parameterless constructor, as options have to be configured, to not throw exception.
+        public OjsDbContext()
+        {
+        }
+
+        public OjsDbContext(DbContextOptions<OjsDbContext> options)
+            : base(options)
+        {
+        }
+
         public OjsDbContext(
             DbContextOptions<OjsDbContext> options,
-            IGlobalQueryFilterTypesCache? globalQueryFilterTypesCache)
+            IGlobalQueryFilterTypesCache globalQueryFilterTypesCache)
             : base(options, globalQueryFilterTypesCache)
             => this.globalQueryFilterTypesCache = globalQueryFilterTypesCache;
 
@@ -88,6 +100,21 @@ namespace OJS.Data
         public DbSet<Tag> Tags { get; set; } = null!;
 
         public DbSet<TagInProblem> TagProblems { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            // Builder is configured in Service Collection registration.
+            if (builder.IsConfigured)
+            {
+                return;
+            }
+
+            // NOTE: This is used by AutoCrudAdmin to just get metadata
+            // about the db context via reflection and the parameterless constructor,
+            // where no real connection has to be made, but to only instantiate the context.
+            // Using the same name always, to not fill the memory with unused databases.
+            builder.UseInMemoryDatabase($"{nameof(OjsDbContext)}InMemoryDatabase");
+        }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
