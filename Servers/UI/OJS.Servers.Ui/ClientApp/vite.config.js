083b4c29991af042ef7f0b3eaddfc26236be7e11
Mihail Tsatsarov <MihailTsatsarov92@gmail.com>
2023-11-17 13:03:36 +0200

    Fix svg import

--- Servers/UI/OJS.Servers.Ui/ClientApp/vite.config.js
+++ Servers/UI/OJS.Servers.Ui/ClientApp/vite.config.js
@@ -2,10 +2,13 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 // eslint-disable-next-line import/no-extraneous-dependencies
+import svgr from 'vite-plugin-svgr';
+/// <reference types="vite-plugin-svgr/client" />
 
 export default defineConfig({
     plugins: [
         react(),
+        svgr(),
     ],
     server: { port: 5002 },
 });
