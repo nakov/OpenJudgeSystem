a76a51e5d96e017d43abcf4d391fab008be8453e
Svetoslav Uzunov <53313278+SvetoslavUzunov@users.noreply.github.com>
2023-11-21 16:32:26 +0200

    Add TotalScore Snapshot (#916)
    Closes:
    https://github.com/SoftUni-Internal/exam-systems-issues/issues/821
    
    Added two new properties to Participant entity: 
    int TotalScoreSnapshot
    Datetime? TotalScoreSnapshotModifiedOn
    
    These properties will be updated/calculated everytime user submits a
    submission.
    
    Two new Background jobs added.
    
    Background job that will update participant total score once per day.
    Background job that will remove Participant scores if there are more
    than one for specific problem , for practice or compete. It will remain
    only the one with highest points.

---
+++ Services/Administration/OJS.Services.Administration.Business/Implementations/ParticipantsBusinessService.cs
@@ -0,0 +1,46 @@
+﻿namespace OJS.Services.Administration.Business.Implementations;
+
+using OJS.Services.Administration.Data;
+using OJS.Data.Models.Participants;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+public class ParticipantsBusinessService : IParticipantsBusinessService
+{
+    private readonly IParticipantsDataService participantsData;
+    private readonly IParticipantScoresDataService scoresDataService;
+
+    public ParticipantsBusinessService(
+        IParticipantsDataService participantsData,
+        IParticipantScoresDataService scoresDataService)
+    {
+        this.participantsData = participantsData;
+        this.scoresDataService = scoresDataService;
+    }
+
+    public async Task UpdateTotalScoreSnapshotOfParticipants()
+        => await this.participantsData.UpdateTotalScoreSnapshot();
+
+    public async Task RemoveParticipantMultipleScores()
+    {
+        var participantScores =
+            this.scoresDataService
+                .GetAll()
+                .GroupBy(ps => new { ps.IsOfficial, ps.ProblemId, ps.ParticipantId })
+                .Where(ps => ps.Count() > 1)
+                .ToList();
+
+        var participantScoresToRemove = new List<ParticipantScore>();
+        participantScores.ForEach(participantScoreGroup =>
+        {
+            participantScoresToRemove
+                .AddRange(participantScoreGroup
+                    .OrderByDescending(ps => ps.Points)
+                    .Skip(1)
+                    .ToList());
+        });
+
+        await this.scoresDataService.Delete(participantScoresToRemove);
+    }
+}
