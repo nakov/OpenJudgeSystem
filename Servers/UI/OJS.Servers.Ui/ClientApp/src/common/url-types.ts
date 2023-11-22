5db8b20453ad5496473c62ea567516897ef8d3b3
SvetoslavUzunov <svetoslavuzunov99@gmail.com>
2023-11-22 20:17:50 +0200

    fixed formatting errors

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/common/url-types.ts
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/common/url-types.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import { IFilter, ISort } from './contest-types';
-import { SubmissionStatus } from "../hooks/submissions/use-public-submissions";
+import { SubmissionStatus } from '../hooks/submissions/use-public-submissions';
 
 interface IContestDetailsUrlParams {
     id: number;
