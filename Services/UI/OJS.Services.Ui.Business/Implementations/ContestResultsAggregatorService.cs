d008ba2be88f47585cb343b40f40f1c4cdab2dfd
SvetoslavUzunov <svetoslavuzunov99@gmail.com>
2023-11-22 11:13:15 +0200

    Removed unnecessary commends

--- Services/UI/OJS.Services.Ui.Business/Implementations/ContestResultsAggregatorService.cs
+++ Services/UI/OJS.Services.Ui.Business/Implementations/ContestResultsAggregatorService.cs
@@ -28,66 +28,6 @@ public class ContestResultsAggregatorService : IContestResultsAggregatorService
         this.participantsData = participantsData;
     }
 
-    // public ContestResultsViewModel GetContestResults(
-    //         Contest contest,
-    //         bool official,
-    //         bool isFullResults,
-    //         bool isExportResults = false)
-    // {
-    //     var contestActivityEntity = this.activityService
-    //         .GetContestActivity(contest.Map<ContestForActivityServiceModel>());
-    //
-    //     var contestResults = new ContestResultsViewModel
-    //         {
-    //             Id = contest.Id,
-    //             Name = contest.Name,
-    //             IsCompete = official,
-    //             ContestCanBeCompeted = contestActivityEntity.CanBeCompeted,
-    //             ContestCanBePracticed = contestActivityEntity.CanBePracticed,
-    //             ContestType = contest.Type,
-    //             Problems = contest.ProblemGroups
-    //                 .SelectMany(pg => pg.Problems)
-    //                 .AsQueryable()
-    //                 .Where(p => !p.IsDeleted)
-    //                 .OrderBy(p => p.OrderBy)
-    //                 .ThenBy(p => p.Name)
-    //                 .Select(ContestProblemListViewModel.FromProblem),
-    //         };
-    //
-    //     var participants = this.participantsCommonData
-    //             .GetAllByContestAndIsOfficial(contest.Id, official);
-    //
-    //     var participantResults = participants
-    //             .Select(ParticipantResultViewModel.FromParticipantAsSimpleResultByContest(contest.Id))
-    //             .OrderByDescending(parRes => parRes.ProblemResults
-    //                 .Where(pr => pr.ShowResult)
-    //                 .Sum(pr => pr.BestSubmission.Points));
-    //
-    //     if (isFullResults)
-    //     {
-    //         participantResults = participants
-    //             .Select(ParticipantResultViewModel.FromParticipantAsFullResultByContest(contest.Id))
-    //             .OrderByDescending(parRes => parRes.ProblemResults
-    //                 .Sum(pr => pr.BestSubmission.Points));
-    //     }
-    //     else if (isExportResults)
-    //     {
-    //         participantResults = participants
-    //             .Select(ParticipantResultViewModel.FromParticipantAsExportResultByContest(contest.Id))
-    //             .OrderByDescending(parRes => parRes.ProblemResults
-    //                 .Where(pr => pr.ShowResult && !pr.IsExcludedFromHomework)
-    //                 .Sum(pr => pr.BestSubmission.Points));
-    //     }
-    //
-    //     contestResults.Results = participantResults
-    //             .ThenBy(parResult => parResult.ProblemResults
-    //                 .OrderByDescending(pr => pr.BestSubmission.Id)
-    //                 .Select(pr => pr.BestSubmission.Id)
-    //                 .FirstOrDefault());
-    //
-    //     return contestResults;
-    // }
-
     public ContestResultsViewModel GetContestResults(ContestResultsModel contestResultsModel)
     {
         var contestActivityEntity = this.activityService
