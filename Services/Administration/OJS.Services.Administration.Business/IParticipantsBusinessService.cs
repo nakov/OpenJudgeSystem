f28ba0cd8eba9713c0abbbc633a5c2b45c3e097c
SvetoslavUzunov <svetoslavuzunov99@gmail.com>
2023-11-23 13:14:57 +0200

    Adding comments and removing unnecessary things.

--- Services/Administration/OJS.Services.Administration.Business/IParticipantsBusinessService.cs
+++ Services/Administration/OJS.Services.Administration.Business/IParticipantsBusinessService.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 
 public interface IParticipantsBusinessService : IService
 {
+    // This method updates the total score snapshot for all participants
     Task UpdateTotalScoreSnapshotOfParticipants();
 
+    // This method removes multiple scores associated with a participant
+    // For each group, it keeps the score with the highest points and prepares to remove the rest.
     Task RemoveParticipantMultipleScores();
 }
--- Services/UI/OJS.Services.Ui.Data/Implementations/ParticipantsDataService.cs
+++ Services/UI/OJS.Services.Ui.Data/Implementations/ParticipantsDataService.cs
@@ -4,7 +4,6 @@ namespace OJS.Services.Ui.Data.Implementations
     using OJS.Common.Enumerations;
     using OJS.Data;
     using OJS.Data.Models.Participants;
-    using OJS.Data.Models.Problems;
     using OJS.Services.Common.Data.Implementations;
     using System;
     using System.Collections.Generic;
