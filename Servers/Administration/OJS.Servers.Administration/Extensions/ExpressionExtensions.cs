e3c2147b0ea846fa83b2bb0654a02ec88fcb4387
PetarNeshkov <petar.neshkov@softuni.bg>
2023-11-27 18:27:11 +0200

    Removed unused namespace

--- Servers/Administration/OJS.Servers.Administration/Extensions/ExpressionExtensions.cs
+++ Servers/Administration/OJS.Servers.Administration/Extensions/ExpressionExtensions.cs
@@ -3,8 +3,6 @@ namespace OJS.Servers.Administration.Extensions;
 using System.Collections.Generic;
 using System.Linq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 
 public static class ExpressionExtensions
