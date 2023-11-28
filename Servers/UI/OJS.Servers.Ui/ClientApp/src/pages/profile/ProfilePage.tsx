10855d702aedf1946b8089bb6cdb1abd81735f2a
PetarNeshkov <petar.neshkov@softuni.bg>
2023-11-28 10:55:36 +0200

    Refactored heading and removed firstName and lastName if empty

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/pages/profile/ProfilePage.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/pages/profile/ProfilePage.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 
 import Heading from '../../components/guidelines/headings/Heading';
 import SpinningLoader from '../../components/guidelines/spinning-loader/SpinningLoader';
 import ProfileAboutInfo from '../../components/profile/profile-about-info/ProfileAboutInfo';
 import { useUsers } from '../../hooks/use-users';
+import isNilOrEmpty from '../../utils/check-utils';
 import { flexCenterObjectStyles } from '../../utils/object-utils';
 import { makePrivate } from '../shared/make-private';
 import { setLayout } from '../shared/set-layout';
@@ -21,6 +22,25 @@ const ProfilePage = () => {
         })();
     }, [ getProfile ]);
 
+    const renderUsernameHeading = useCallback(
+        () => {
+            const { userName } = profile;
+
+            return isNilOrEmpty(userName)
+                ? (
+                    <Heading>Profile</Heading>
+                )
+                : (
+                    <Heading>
+                        {profile.userName}
+                        &apos;s
+                        profile
+                    </Heading>
+                );
+        },
+        [ profile ],
+    );
+
     if (isLoading) {
         return (
             <div style={{ ...flexCenterObjectStyles }}>
@@ -31,12 +51,12 @@ const ProfilePage = () => {
 
     return (
         <>
-            <Heading>Profile</Heading>
+            {renderUsernameHeading()}
             <ProfileAboutInfo value={profile} />
-            {/* Tabs will be hidden for alpha version, as it is not production ready yet */}
+            Tabs will be hidden for alpha version, as it is not production ready yet
             {/* <Tabs */}
-            {/*    labels={['Submissions', 'Contest Participations']} */}
-            {/*    contents={[<ProfileSubmissions/>, <ProfileContestParticipations/>]} */}
+            {/*  labels={[ 'Submissions', 'Contest Participations' ]} */}
+            {/*  contents={[ <ProfileSubmissions />, <ProfileContestParticipations /> ]} */}
             {/* /> */}
         </>
     );
