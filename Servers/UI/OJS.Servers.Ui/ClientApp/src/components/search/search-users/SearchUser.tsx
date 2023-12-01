5f3d7442a7a04f7699d4047b73157278a4814f69
PetarNeshkov <petar.neshkov@softuni.bg>
2023-12-01 16:11:59 +0200

    Implemented redirecting to user profile from search page

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/search/search-users/SearchUser.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/search/search-users/SearchUser.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { IUserSearchType } from '../../../common/search-types';
+import { useUsers } from '../../../hooks/use-users';
 import concatClassNames from '../../../utils/class-names';
+import { getUserProfileInfoUrlByUsername } from '../../../utils/urls';
+import { Button, ButtonSize, ButtonType } from '../../guidelines/buttons/Button';
 
 import styles from './SearchUser.module.scss';
 
@@ -10,17 +13,54 @@ interface ISearchUser {
 }
 
 const SearchUser = ({ user }: ISearchUser) => {
+    const { actions: { initiateRedirectionToUserProfile } } = useUsers();
+
+    const searchProblemCardHeader = 'search-header';
+    const searchUserCardHeaderClassName = concatClassNames(
+        styles.userCardHeader,
+        searchProblemCardHeader,
+        user.name.length >= 23
+            ? styles.usernameHoverable
+            : '',
+    );
     const searchUserText = 'search-user-text';
     const searchUserClassName = concatClassNames(styles.userText, searchUserText);
     const searchUserElement = 'search-user-element';
     const searchUserElementClassName = concatClassNames(styles.userElement, searchUserElement);
+
+    const handleUserRedirection = useCallback(
+        () => {
+            const getUserProfileInfoUrl = getUserProfileInfoUrlByUsername(user.name);
+
+            initiateRedirectionToUserProfile(user.name, getUserProfileInfoUrl);
+        },
+        [ initiateRedirectionToUserProfile, user.name ],
+    );
+
     return (
-        <div className={searchUserElementClassName}>
-            <span
-              className={searchUserClassName}
-            >
-                {user.name}
-            </span>
+        <div className={searchUserCardHeaderClassName}>
+            <div className={searchUserElementClassName}>
+                <div className={styles.tooltip}>
+                    <span className={styles.tooltipText}>
+                        <Button
+                          internalClassName={styles.redirectButton}
+                          type={ButtonType.secondary}
+                          text={user.name}
+                          onClick={handleUserRedirection}
+                          size={ButtonSize.small}
+                        />
+                    </span>
+                </div>
+                <span className={searchUserClassName}>
+                    <Button
+                      internalClassName={styles.redirectButton}
+                      type={ButtonType.secondary}
+                      text={user.name}
+                      onClick={handleUserRedirection}
+                      size={ButtonSize.small}
+                    />
+                </span>
+            </div>
         </div>
     );
 };
