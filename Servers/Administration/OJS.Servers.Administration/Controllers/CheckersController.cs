eeaf883533fbd67bed6a319b889530545f38457d
Georgi Georgiev <gogo4ds@gmail.com>
2023-12-05 14:13:31 +0200

    Removed unused import

--- Servers/Administration/OJS.Servers.Administration/Controllers/CheckersController.cs
+++ Servers/Administration/OJS.Servers.Administration/Controllers/CheckersController.cs
@@ -9,7 +9,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using OJS.Services.Administration.Models;
 
 public class CheckersController : BaseAutoCrudAdminController<Checker>
 {
