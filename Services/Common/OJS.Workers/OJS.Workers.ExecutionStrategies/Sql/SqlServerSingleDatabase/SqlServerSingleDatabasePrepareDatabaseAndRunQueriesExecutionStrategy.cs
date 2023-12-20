db5adb51ebcf1e6abf6c3a913ea391d942cd2dab
Georgi Georgiev <gogo4ds@gmail.com>
2023-12-20 18:32:20 +0200

    Added SQL strategies to source control

---
+++ Services/Common/OJS.Workers/OJS.Workers.ExecutionStrategies/Sql/SqlServerSingleDatabase/SqlServerSingleDatabasePrepareDatabaseAndRunQueriesExecutionStrategy.cs
@@ -0,0 +1,30 @@
+﻿namespace OJS.Workers.ExecutionStrategies.Sql.SqlServerSingleDatabase
+{
+    using OJS.Workers.Common;
+    using OJS.Workers.ExecutionStrategies.Models;
+
+    public class SqlServerSingleDatabasePrepareDatabaseAndRunQueriesExecutionStrategy : BaseSqlServerSingleDatabaseExecutionStrategy
+    {
+        public SqlServerSingleDatabasePrepareDatabaseAndRunQueriesExecutionStrategy(
+            string masterDbConnectionString,
+            string restrictedUserId,
+            string restrictedUserPassword,
+            string submissionProcessorIdentifier)
+            : base(masterDbConnectionString, restrictedUserId, restrictedUserPassword, submissionProcessorIdentifier)
+        {
+        }
+
+        protected override IExecutionResult<TestResult> ExecuteAgainstTestsInput(
+            IExecutionContext<TestsInputModel> executionContext,
+            IExecutionResult<TestResult> result)
+            => this.Execute(
+                executionContext,
+                result,
+                (connection, test) =>
+                {
+                    this.ExecuteNonQuery(connection, test.Input);
+                    var sqlTestResult = this.ExecuteReader(connection, executionContext.Code, executionContext.TimeLimit);
+                    ProcessSqlResult(sqlTestResult, executionContext, test, result);
+                });
+    }
+}
