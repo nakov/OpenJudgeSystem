6e357c97ee9f3f0f5d189123a10daaf6cc437bd0
Mihail Tsatsarov <MihailTsatsarov92@gmail.com>
2024-01-08 23:51:50 +0200

    Fix build error

--- Services/Common/OJS.Services.Common.Data/Pagination/FilteringService.cs
+++ Services/Common/OJS.Services.Common.Data/Pagination/FilteringService.cs
@@ -62,10 +62,6 @@ public abstract class FilteringService<TEntity>
         {
             expression = BuildIntExpression(filter.OperatorType, filter.Value, property);
         }
-        else if (filter.Property.PropertyType == typeof(DateTime))
-        {
-            expression
-        }
 
         if (expression == null)
         {
