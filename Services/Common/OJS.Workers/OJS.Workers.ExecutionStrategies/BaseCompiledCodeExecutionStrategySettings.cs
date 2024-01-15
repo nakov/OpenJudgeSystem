3ce222ab182f5c3fd0622efa32aa6cff2ec38b24
Georgi Georgiev <gogo4ds@gmail.com>
2024-01-15 17:07:03 +0200

    Deleted unused class

--- Services/Common/OJS.Workers/OJS.Workers.ExecutionStrategies/BaseCompiledCodeExecutionStrategySettings.cs
+++
@@ -1,6 +0,0 @@
-namespace OJS.Workers.ExecutionStrategies;
-
-public class BaseCompiledCodeExecutionStrategySettings : BaseCodeExecutionStrategySettings
-{
-    public string CompilerPath { get; set; } = string.Empty;
-}
