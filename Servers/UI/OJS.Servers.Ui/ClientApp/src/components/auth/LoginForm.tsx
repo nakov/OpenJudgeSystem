2a45ea2abf8f245bfa19744f3dde87956879e0cc
Georgi Georgiev <gogo4ds@gmail.com>
2024-02-22 11:38:16 +0200

    Used custom hooks and added payload type  for demo

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/auth/LoginForm.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/auth/LoginForm.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable max-len */
 /* eslint-disable no-useless-return */
 import React, { useCallback, useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import isEmpty from 'lodash/isEmpty';
 import isNil from 'lodash/isNil';
 
@@ -11,8 +10,9 @@ import {
     PasswordLengthErrorMessage,
     UsernameFormatErrorMessage, UsernameLengthErrorMessage,
 } from '../../common/constants';
-import { IAuthorizationReduxState, setInternalUser, setIsLoggedIn } from '../../redux/features/authorizationSlice';
+import { setInternalUser, setIsLoggedIn } from '../../redux/features/authorizationSlice';
 import { useGetUserinfoQuery, useLoginMutation } from '../../redux/services/authorizationService';
+import { useAppDispatch, useAppSelector } from '../../redux/store';
 import { flexCenterObjectStyles } from '../../utils/object-utils';
 import { LinkButton, LinkButtonType } from '../guidelines/buttons/Button';
 import Form from '../guidelines/forms/Form';
@@ -34,9 +34,8 @@ const LoginPage = () => {
 
     const [ login, { isLoading, isSuccess, error } ] = useLoginMutation();
     const { data, isSuccess: isGetInfoSuccessfull, refetch } = useGetUserinfoQuery(null);
-    const { isLoggedIn } =
-    useSelector((state: {authorization: IAuthorizationReduxState}) => state.authorization);
-    const dispatch = useDispatch();
+    const { isLoggedIn } = useAppSelector((state) => state.authorization);
+    const dispatch = useAppDispatch();
     const usernameFieldName = 'Username';
     const passwordFieldName = 'Password';
 
