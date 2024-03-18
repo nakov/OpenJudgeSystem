1792377eb89938ec0896576ec5b5e02bb169772d
Mihail Tsatsarov <77450034+MTsatsarov@users.noreply.github.com>
2024-03-18 16:15:13 +0200

    Implementing checkers administration. (#1053)
    Closes
    https://github.com/SoftUni-Internal/exam-systems-issues/issues/1199
    
    **Summary of the changes made**:
    1. Added BE logic for checkers.
    2.  Added FE logic for checkers.

--- Services/Administration/OJS.Services.Administration.Models/Checkers/CheckerAdministrationModel.cs
+++ Services/Administration/OJS.Services.Administration.Models/Checkers/CheckerAdministrationModel.cs
@@ -1,5 +1,32 @@
 ﻿namespace OJS.Services.Administration.Models.Checkers;
 
-public class CheckerAdministrationModel : BaseAdministrationModel<int>
+using AutoMapper;
+using OJS.Data.Models.Checkers;
+using SoftUni.AutoMapper.Infrastructure.Models;
+
+public class CheckerAdministrationModel : BaseAdministrationModel<int>, IMapExplicitly
 {
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? DllFile { get; set; }
+
+    public string? ClassName { get; set; }
+
+    public string? Parameter { get; set; }
+    public void RegisterMappings(IProfileExpression configuration)
+    {
+        configuration.CreateMap<Checker, CheckerAdministrationModel>();
+
+        configuration.CreateMap<CheckerAdministrationModel, Checker>()
+            .ForMember(c => c.IsDeleted, opt
+                => opt.Ignore())
+            .ForMember(c => c.DeletedOn, opt
+                => opt.Ignore())
+            .ForMember(c => c.CreatedOn, opt
+                => opt.Ignore())
+            .ForMember(c => c.ModifiedOn, opt
+                => opt.Ignore());
+    }
 }
---
+++ Services/Administration/OJS.Services.Administration.Models/Checkers/CheckerInListModel.cs
@@ -0,0 +1,19 @@
+﻿namespace OJS.Services.Administration.Models.Checkers;
+
+using OJS.Data.Models.Checkers;
+using SoftUni.AutoMapper.Infrastructure.Models;
+
+public class CheckerInListModel : IMapFrom<Checker>
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? DllFile { get; set; }
+
+    public string? ClassName { get; set; }
+
+    public string? Parameter { get; set; }
+
+    public bool IsDeleted { get; set; }
+}
