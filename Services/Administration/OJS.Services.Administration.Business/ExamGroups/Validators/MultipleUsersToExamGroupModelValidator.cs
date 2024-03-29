baa91200ecf86601b677b001becf011668997abb
Mihail Tsatsarov <MihailTsatsarov92@gmail.com>
2024-03-29 22:19:07 +0200

    Fixes

--- Services/Administration/OJS.Services.Administration.Business/ExamGroups/Validators/MultipleUsersToExamGroupModelValidator.cs
+++ Services/Administration/OJS.Services.Administration.Business/ExamGroups/Validators/MultipleUsersToExamGroupModelValidator.cs
@@ -1,9 +1,11 @@
 ﻿namespace OJS.Services.Administration.Business.ExamGroups.Validators;
 
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using OJS.Services.Administration.Data;
 using OJS.Services.Administration.Models.ExamGroups;
 using OJS.Services.Common.Validation;
+using System.Threading.Tasks;
 
 public class MultipleUsersToExamGroupModelValidator : BaseValidator<MultipleUsersToExamGroupModel>
 {
@@ -15,6 +17,18 @@ public class MultipleUsersToExamGroupModelValidator : BaseValidator<MultipleUser
         this.RuleLevelCascadeMode = CascadeMode.Stop;
         this.RuleFor(x => x.ExamGroupId)
             .NotEmpty()
-            .MustAsync(async (model, _) => await this.examGroupsDataService.ExistsById(model));
+            .MustAsync(async (model, _) => await this.BeValidAndWithContest(model))
+            .WithMessage("The problem group does not exists or does not have a contest connected to it.");
+    }
+
+    private async Task<bool> BeValidAndWithContest(int examGroupId)
+    {
+        var examGroup = await this.examGroupsDataService.GetByIdQuery(examGroupId).FirstOrDefaultAsync();
+        if (examGroup == null || examGroup.ContestId == null)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
--- Services/Administration/OJS.Services.Administration.Business/ExamGroups/Validators/UserToExamGroupValidator.cs
+++ Services/Administration/OJS.Services.Administration.Business/ExamGroups/Validators/UserToExamGroupValidator.cs
@@ -1,9 +1,11 @@
 ﻿namespace OJS.Services.Administration.Business.ExamGroups.Validators;
 
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using OJS.Services.Administration.Data;
 using OJS.Services.Administration.Models.ExamGroups;
 using OJS.Services.Common.Validation;
+using System.Threading.Tasks;
 
 public class UserToExamGroupValidator : BaseValidator<UserToExamGroupModel>
 {
@@ -18,8 +20,18 @@ public class UserToExamGroupValidator : BaseValidator<UserToExamGroupModel>
 
         this.RuleFor(x => x.ExamGroupId)
             .GreaterThan(0)
-            .MustAsync(async (model, _)
-                => await this.examGroupsDataService.ExistsById(model))
-            .WithMessage("Exam group does not exists.");
+            .MustAsync(async (model, _) => await this.BeValidAndWithContest(model))
+            .WithMessage("The problem group does not exists or does not have a contest connected to it.");
+    }
+
+    private async Task<bool> BeValidAndWithContest(int examGroupId)
+    {
+        var examGroup = await this.examGroupsDataService.GetByIdQuery(examGroupId).FirstOrDefaultAsync();
+        if (examGroup == null || examGroup.ContestId == null)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
