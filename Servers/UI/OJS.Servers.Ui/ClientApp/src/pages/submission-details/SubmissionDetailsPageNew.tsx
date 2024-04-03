e1f6da3ae459b0b007e1991894f4c847a6256d6d
BojidarTonev <bojidar.tonev.9@gmail.com>
2024-04-03 16:37:50 +0300

    implementations over submission details page

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/pages/submission-details/SubmissionDetailsPageNew.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/pages/submission-details/SubmissionDetailsPageNew.tsx
@@ -2,12 +2,17 @@ import { useCallback } from 'react';
 import { useParams } from 'react-router';
 import { useNavigate } from 'react-router-dom';
 
+import CodeEditor from '../../components/code-editor/CodeEditor';
 import ContestBreadcrumbs from '../../components/contests/contest-breadcrumbs/ContestBreadcrumbs';
+import Button from '../../components/guidelines/buttons/Button';
 import SpinningLoader from '../../components/guidelines/spinning-loader/SpinningLoader';
+import SubmissionTestRun from '../../components/submissions/submission-test-run/SubmissionTestRun';
 import SubmissionTestRuns from '../../components/submissions/submission-test-runs/SubmissionTestRuns';
+import { ITestRun } from '../../hooks/submissions/types';
 import useTheme from '../../hooks/use-theme';
 import { useGetSubmissionDetailsQuery } from '../../redux/services/submissionsService';
 import { useAppSelector } from '../../redux/store';
+import { defaultDateTimeFormat, formatDate } from '../../utils/dates';
 import { flexCenterObjectStyles } from '../../utils/object-utils';
 
 import styles from './SubmissionsDetailsPageNew.module.scss';
@@ -21,10 +26,9 @@ const SubmissionDetailsPageNew = () => {
     const textColorClassName = getColorClassName(themeColors.textColor);
 
     const { data, isLoading, error } = useGetSubmissionDetailsQuery({ id: Number(submissionId) });
-    console.log('data => ', data);
 
     const { name, id: contestId } = contestDetails || {};
-    const { id: solutionId, problem, user } = data || {};
+    const { id: solutionId, problem, user, content, submissionType, testRuns, createdOn } = data || {};
 
     const renderSolutionTitle = useCallback(() => (
         <div className={styles.solutionTitle}>
@@ -42,6 +46,43 @@ const SubmissionDetailsPageNew = () => {
         </div>
     ), [ solutionId, user?.userName, problem?.name ]);
 
+    const renderSolutionDetails = useCallback(() => {
+        const { allowBinaryFilesUpload } = submissionType || {};
+
+        return (
+            <div className={styles.submissionDetailsWrapper}>
+                <div>
+                    <div className={styles.detailsRow}>
+                        <span>
+                            Submitted on:
+                        </span>
+                        <span>{formatDate(createdOn, defaultDateTimeFormat)}</span>
+                    </div>
+                    <div className={styles.detailsRow}>
+                        <span>Allowed memory:</span>
+                        <span>nz</span>
+                    </div>
+                </div>
+                <div className={styles.strategyWrapper}>
+                    <span style={{
+                        border: `2px solid ${themeColors.baseColor100}`,
+                        color: `${themeColors.baseColor100}`,
+                    }}
+                    >
+                        {submissionType?.name}
+                    </span>
+                </div>
+                {allowBinaryFilesUpload && (
+                    <div className={styles.buttonWrapper}>
+                        <Button text="DOWNLOAD" onClick={() => {}} />
+                    </div>
+                )}
+            </div>
+        );
+    }, [ submissionType, createdOn ]);
+
+    const renderSolutionTestRuns = useCallback(() => testRuns.map((testRun: ITestRun) => <SubmissionTestRun testRun={testRun} />), [ testRuns ]);
+
     if (isLoading) {
         return (
             <div style={{ ...flexCenterObjectStyles }}>
@@ -58,9 +99,15 @@ const SubmissionDetailsPageNew = () => {
             <div className={styles.submissionDetailsBody}>
                 <div className={styles.submissionTitle}>{name}</div>
                 <div className={styles.bodyWrapper}>
-                    <SubmissionTestRuns />
-                    <div>
+                    <SubmissionTestRuns testRuns={testRuns} />
+                    <div className={styles.innerBodyWrapper}>
                         {renderSolutionTitle()}
+                        {renderSolutionTestRuns()}
+                        <div className={styles.codeContentWrapper}>
+                            <div>Source Code</div>
+                            <CodeEditor code={content} readOnly />
+                        </div>
+                        {renderSolutionDetails()}
                     </div>
                 </div>
             </div>
