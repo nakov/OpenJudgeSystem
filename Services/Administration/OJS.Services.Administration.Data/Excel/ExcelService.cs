1bbe73340ff59531b2cc342ed7b72e297bdb959d
Mihail Tsatsarov <77450034+MTsatsarov@users.noreply.github.com>
2024-04-05 10:18:31 +0300

    Fix issues related to excel. (#1084)
    Closes
    https://github.com/SoftUni-Internal/exam-systems-issues/issues/1212
    
    **Summary of the changes made**:
    1. NPOI library removed.
    2. Adjustments created so the method to export results should work with
    CLosedXML
    3.
    
    ---------
    
    Co-authored-by: Georgi Georgiev <gogo4ds@gmail.com>

--- Services/Administration/OJS.Services.Administration.Data/Excel/ExcelService.cs
+++ Services/Administration/OJS.Services.Administration.Data/Excel/ExcelService.cs
@@ -1,9 +1,7 @@
 ﻿namespace OJS.Services.Administration.Data.Excel;
 
 using ClosedXML.Excel;
-using NPOI.HSSF.UserModel;
 using OJS.Common;
-using OJS.Common.Extensions;
 using OJS.Services.Common.Models.Contests.Results;
 using OJS.Services.Common.Models.Files;
 using System;
@@ -45,77 +43,69 @@ public class ExcelService : IExcelService
             var dataTypeProperties = dataType.GetProperties();
 
             // Create new Excel workbook
-            var workbook = new HSSFWorkbook();
+            var workbook = new XLWorkbook();
 
             // Create new Excel sheet
-            var sheet = workbook.CreateSheet();
+            var sheet = workbook.Worksheets.Add("Results");
 
             // Create a header row
-            var headerRow = sheet.CreateRow(0);
-            int columnNumber = 0;
+            var columnNumber = 1;
             foreach (var property in dataTypeProperties)
             {
-                string cellName = property.Name;
-                object[] attributes = property.GetCustomAttributes(typeof(DisplayAttribute), true);
+                var cellName = property.Name;
+                var attributes = property.GetCustomAttributes(typeof(DisplayAttribute), true);
                 if (attributes.Any())
                 {
-                    var attribute = attributes[0] as DisplayAttribute;
-                    if (attribute != null)
+                    if (attributes[0] is DisplayAttribute attribute)
                     {
                         cellName = attribute.Name ?? property.Name;
                     }
                 }
 
-                headerRow.CreateCell(columnNumber++).SetCellValue(cellName);
+                sheet.Cell(1, columnNumber++).Value = cellName;
             }
 
-            // (Optional) freeze the header row so it is not scrolled
-            sheet.CreateFreezePane(0, 1, 0, 1);
-
-            int rowNumber = 1;
+            var rowNumber = 2;
 
             // Populate the sheet with values from the grid data
             foreach (object? item in items)
             {
                 // Create a new row
-                var row = sheet.CreateRow(rowNumber++);
+                var row = sheet.Row(rowNumber++);
 
-                int cellNumber = 0;
+                var cellNumber = 1;
                 foreach (var property in dataTypeProperties)
                 {
-                    object propertyValue = item?.GetType()?.GetProperty(property.Name)!.GetValue(item, null)!;
+                    var propertyValue = item?.GetType().GetProperty(property.Name)?.GetValue(item, null);
                     if (propertyValue == null)
                     {
-                        row.CreateCell(cellNumber).SetCellType(NPOI.SS.UserModel.CellType.Blank);
+                        sheet.Cell(row.RowNumber(), cellNumber).Value = string.Empty;
                     }
                     else
                     {
-                        var cell = row.CreateCell(cellNumber);
-                        double value;
                         var typeCode = Type.GetTypeCode(property.PropertyType);
                         if (typeCode == TypeCode.Single || typeCode == TypeCode.Char)
                         {
-                            cell.SetCellValue(propertyValue.ToString());
+                            sheet.Cell(row.RowNumber(), cellNumber).Value = propertyValue.ToString();
                         }
 
-                        if (double.TryParse(propertyValue.ToString(), out value))
+                        if (double.TryParse(propertyValue.ToString(), out var value))
                         {
-                            cell.SetCellValue(value);
-                            cell.SetCellType(NPOI.SS.UserModel.CellType.Numeric);
+                            sheet.Cell(row.RowNumber(), cellNumber).Value = value;
                         }
                         else if (typeCode == TypeCode.DateTime)
                         {
-                            cell.SetCellValue((DateTime)propertyValue);
+                            sheet.Cell(row.RowNumber(), cellNumber).Value = (DateTime)propertyValue;
                         }
                         else
                         {
-                            string propertyValueAsString = propertyValue.ToString()!;
+                            var propertyValueAsString = propertyValue.ToString()!;
                             if (propertyValue.ToString()!.Length > 10000)
                             {
                                 propertyValueAsString = "THIS CELL DOES NOT CONTAIN FULL INFORMATION: " + propertyValueAsString.Substring(0, 10000);
                             }
 
-                            cell.SetCellValue(propertyValueAsString);
+                            sheet.Cell(row.RowNumber(), cellNumber).Value = propertyValueAsString;
                         }
                     }
 
@@ -123,16 +113,17 @@ public class ExcelService : IExcelService
                 }
             }
 
-            sheet.AutoSizeColumns(columnNumber);
+            sheet.Columns().AdjustToContents();
 
             // Write the workbook to a memory stream
             var outputStream = new MemoryStream();
-            workbook.Write(outputStream);
+
+            workbook.SaveAs(outputStream);
 
             // Return the result to the end user
             return new FileResponseModel(
                 outputStream.ToArray(), // The binary data of the XLS file
-                string.Format("{0}.xls", this.GetType().Name),
+                $"{this.GetType().Name}.xls",
                 GlobalConstants.MimeTypes.ExcelSheet);
     }
 
