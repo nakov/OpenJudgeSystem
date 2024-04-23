02e74120565b3d8c8184573f05f2c75530f3b8e5
Simona Simeonova <simona.simeonova@softuni.bg>
2024-04-23 16:59:49 +0300

    Removed global query filter

--- Data/OJS.Data/OjsDbContext.cs
+++ Data/OJS.Data/OjsDbContext.cs
@@ -245,8 +245,7 @@ namespace OJS.Data
                 .HasQueryFilter(x => !x.Problem.IsDeleted);
 
             builder.Entity<Submission>()
-                .HasQueryFilter(x => !x.Problem.IsDeleted)
-                .HasQueryFilter(x => !x.Problem.ProblemGroup.Contest.IsDeleted);
+                .HasQueryFilter(x => !x.Problem.IsDeleted);
 
             builder.Entity<SubmissionTypeInProblem>()
                 .HasQueryFilter(x => !x.Problem.IsDeleted);
