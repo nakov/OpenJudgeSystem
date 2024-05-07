adda397abfadad0482fc6c9cb8f8a5caa13426f2
Simona Simeonova <simona.simeonova@softuni.bg>
2024-05-07 16:32:13 +0300

    Removed mapping for EndDateTimeForParticipantOrContest

--- Services/UI/OJS.Services.Ui.Models/Contests/ContestParticipationServiceModel.cs
+++ Services/UI/OJS.Services.Ui.Models/Contests/ContestParticipationServiceModel.cs
@@ -32,13 +32,6 @@ public class ContestParticipationServiceModel : IMapExplicitly
     public void RegisterMappings(IProfileExpression configuration)
         => configuration.CreateMap<Participant, ContestParticipationServiceModel>()
             .ForMember(d => d.Contest, opt => opt.MapFrom(s => s.Contest))
-            .ForMember(d => d.EndDateTimeForParticipantOrContest, opt => opt.MapFrom(s =>
-                s.ParticipationEndTime.HasValue
-                ? s.ParticipationEndTime
-                : s.Contest.EndTime.HasValue && s.Contest.EndTime >= DateTime.UtcNow
-                    ? s.Contest.EndTime
-                    : s.Contest.PracticeEndTime.HasValue
-                        ? s.Contest.PracticeEndTime
-                        : null))
+            .ForMember(d => d.EndDateTimeForParticipantOrContest, opt => opt.Ignore())
             .ForAllOtherMembers(opt => opt.Ignore());
 }
