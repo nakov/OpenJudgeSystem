0891c4a1fccb1672eacfc8298a7769e85a2fe5b7
Simona Simeonova <simona.simeonova@softuni.bg>
2024-05-14 16:14:40 +0300

    Fix admin urls in solution submit

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/pages/contests/contest-solution-submit/ContestSolutionSubmitPage.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/pages/contests/contest-solution-submit/ContestSolutionSubmitPage.tsx
@@ -328,10 +328,9 @@ const ContestSolutionSubmitPage = () => {
         ? contest.problems.reduce((accumulator, problem) => accumulator + problem.maximumPoints, 0)
         : 0, [ contest ]);
 
-    const goToSubmissionAdministration = () => navigate(`
-    /administration-new/problems?filter=id~equals~${
+    const goToSubmissionAdministration = () => navigate(`/administration-new/problems?filter=id~equals~${
         selectedContestDetailsProblem!.id
-}%26%26%3Bisdeleted~equals~false&sorting=id%3DDESC`);
+    }%26%26%3Bisdeleted~equals~false&sorting=id%3DDESC`);
 
     const goToTestsAdministration = () => navigate(`/administration-new/tests?filter=problemid~equals~${
         selectedContestDetailsProblem!.id
