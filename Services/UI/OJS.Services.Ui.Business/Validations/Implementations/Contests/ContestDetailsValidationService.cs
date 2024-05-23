f055551a6589fe22ff375c92e76c153a955ad3f2
Simona Simeonova <simona.simeonova@softuni.bg>
2024-05-23 11:34:11 +0300

    Removed usings

--- Services/UI/OJS.Services.Ui.Business/Validations/Implementations/Contests/ContestDetailsValidationService.cs
+++ Services/UI/OJS.Services.Ui.Business/Validations/Implementations/Contests/ContestDetailsValidationService.cs
@@ -1,8 +1,6 @@
 ﻿namespace OJS.Services.Ui.Business.Validations.Implementations.Contests;
 
 using OJS.Data.Models.Contests;
-using OJS.Services.Common.Models;
-using OJS.Services.Common.Models.Users;
 using OJS.Services.Infrastructure;
 using OJS.Services.Infrastructure.Models;
 
