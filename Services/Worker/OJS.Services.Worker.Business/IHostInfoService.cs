1d50a5c90a66b2c8f59a71af9ff382bc7f6168b4
Georgi Georgiev <gogo4ds@gmail.com>
2024-06-15 09:51:08 +0300

    Get host IP from env variable

--- Services/Worker/OJS.Services.Worker.Business/IHostInfoService.cs
+++ Services/Worker/OJS.Services.Worker.Business/IHostInfoService.cs
@@ -1,11 +1,8 @@
 namespace OJS.Services.Worker.Business;
 
 using OJS.Services.Infrastructure;
-using System.Net;
 
 public interface IHostInfoService : ISingletonService
 {
-    string GetHostName();
-
-    IPAddress? GetHostIpv4Address();
+    string? GetHostIp();
 }
