b9bd624fe2cbcd8c74169a6419cbee27b53d7b88
Denis Yordanov <122925849+deyordanov@users.noreply.github.com>
2024-06-22 13:19:44 +0300

    Optimized the 'Contest Details' page's load time. (#1244)
    Closes
    https://github.com/SoftUni-Internal/exam-systems-issues/issues/1373
    
    ---------
    
    Co-authored-by: Georgi Georgiev <gogo4ds@gmail.com>

---
+++ Services/Administration/OJS.Services.Administration.Business/IContestsCacheService.cs
@@ -0,0 +1,8 @@
+﻿namespace OJS.Services.Administration.Business;
+
+using OJS.Services.Infrastructure;
+
+public interface IContestsCacheService : IService
+{
+    void ClearContestsCacheByContestId(int contestId);
+}
