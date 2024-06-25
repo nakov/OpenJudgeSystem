7fb78d0ac7dd536e2197ef5b57b9582f3f3d1467
Georgi Georgiev <gogo4ds@gmail.com>
2024-06-25 10:56:45 +0300

    Check text in component

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/common/MultiLineTextDisplay.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/common/MultiLineTextDisplay.tsx
@@ -1,14 +1,22 @@
+import isEmpty from 'lodash/isEmpty';
+
 interface IMultiLineTextProps {
     text: string;
     className?: string;
 }
 
-const MultiLineTextDisplay = ({ text, className } : IMultiLineTextProps) => (
-    <div className={className}>
-        {text.split('\n').map((line: string, index: number) => (
-            <p key={index}>{line}</p>
-        ))}
-    </div>
-);
+const MultiLineTextDisplay = ({ text, className } : IMultiLineTextProps) => {
+    if (isEmpty(text)) {
+        return null;
+    }
+
+    return (
+        <div className={className}>
+            {text.split('\n').map((line: string, index: number) => (
+                <p key={index}>{line}</p>
+            ))}
+        </div>
+    );
+};
 
 export default MultiLineTextDisplay;
