f6d26776cd73d3034bb01394337bcc8de0613823
Georgi Georgiev <gogo4ds@gmail.com>
2024-07-02 13:50:42 +0300

    Fixed bug

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/pages/home/HomePage.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/pages/home/HomePage.tsx
@@ -1,9 +1,10 @@
 import { useEffect } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation, useSearchParams } from 'react-router-dom';
 
 import ContestBreadcrumbs from '../../components/contests/contest-breadcrumbs/ContestBreadcrumbs';
 import { ContestCategories } from '../../components/contests/contest-categories/ContestCategories';
 import {
+    clearContestCategoryBreadcrumbItems,
     setContestDetails,
 } from '../../redux/features/contestsSlice';
 import { useAppDispatch } from '../../redux/store';
@@ -15,13 +16,19 @@ import styles from './HomePage.module.scss';
 
 const HomePage = () => {
     const location = useLocation();
+    const [ searchParams ] = useSearchParams();
     const dispatch = useAppDispatch();
 
     useEffect(() => {
-        // Clear contest details when navigating to home page,
-        // so that breadcrumb is also cleared (as it depends on contest details)
-        dispatch(setContestDetails({ contest: null }));
-    });
+        if (!searchParams.get('category')) {
+            /**
+             * Clear contest details and breadcrumb items when no category is selected
+             * so categories tree is collapsed properly (as it depends on category in the store)
+             */
+            dispatch(setContestDetails({ contest: null }));
+            dispatch(clearContestCategoryBreadcrumbItems());
+        }
+    }, [ searchParams, dispatch ]);
 
     return (
         <div>
