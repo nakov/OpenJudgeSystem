f5b67ef3b83e52884d0134b5b4e748fef1c19d4b
Denis Yordanov <122925849+deyordanov@users.noreply.github.com>
2024-07-09 13:18:03 +0300

    When an invalid number of test cases is provided, a proper error message is now shown. (#1270)
    Closes
    https://github.com/SoftUni-Internal/exam-systems-issues/issues/1389

--- Services/Common/OJS.Workers/OJS.Workers.ExecutionStrategies/Java/JavaProjectTestsExecutionStrategy.cs
+++ Services/Common/OJS.Workers/OJS.Workers.ExecutionStrategies/Java/JavaProjectTestsExecutionStrategy.cs
@@ -21,6 +21,7 @@ namespace OJS.Workers.ExecutionStrategies.Java
     public class JavaProjectTestsExecutionStrategy<TSettings> : JavaUnitTestsExecutionStrategy<TSettings>
         where TSettings : JavaProjectTestsExecutionStrategySettings
     {
+        private const string InvalidNumberOfTestCasesPrefix = "Invalid number of test cases";
         private const string TestRanPrefix = "Test Ran. Successful:";
         private readonly string testResultRegexPattern = $@"(?:{TestRanPrefix})\s*(true|false)";
 
@@ -86,10 +87,15 @@ public class _$TestRunner {{
 
             System.out.println(testClasses[i].getSimpleName() + "" {TestRanPrefix} "" + result.wasSuccessful());
 
-            for (Failure failure : result.getFailures()) {{
-                String failureClass = failure.getDescription().getTestClass().getSimpleName();
-                String failureException = failure.getException().toString().replaceAll(""\r"", ""\\\\r"").replaceAll(""\n"",""\\\\n"");
-                System.out.printf(""%s %s%s"", failureClass, failureException, System.lineSeparator());
+            var failureCount = result.getFailures().size();
+            if (failureCount > 1) {{
+                System.out.printf(""{InvalidNumberOfTestCasesPrefix} "" + ""(%d) for %s. There should be a single test case per test.%n"", failureCount, testClasses[i].getSimpleName());
+            }} else {{
+                for (Failure failure : result.getFailures()) {{
+                    String failureClass = failure.getDescription().getTestClass().getSimpleName();
+                    String failureException = failure.getException().toString().replaceAll(""\r"", ""\\\\r"").replaceAll(""\n"",""\\\\n"");
+                    System.out.printf(""%s %s%s"", failureClass, failureException, System.lineSeparator());
+                }}
             }}
         }}
     }}
@@ -168,14 +174,20 @@ public class _$TestRunner {{
             SummaryGeneratingListener listener = listeners.get(i);
             var summary = listener.getSummary();
 
-            var hasFailures = summary.getTotalFailureCount() > 0;
+            var failureCount = summary.getTotalFailureCount();
+
+            var hasFailures = failureCount > 0;
             System.out.println(testClasses[i].getSimpleName() + "" {TestRanPrefix} "" + !hasFailures);
 
-            summary.getFailures().forEach(failure -> {{
-                String failureClass = failure.getTestIdentifier().getDisplayName();
-                String failureException = failure.getException().toString().replaceAll(""\r"", ""\\r"").replaceAll(""\n"",""\\n"");
-                System.out.printf(""%s %s%s"", failureClass, failureException, System.lineSeparator());
-            }});
+            if (failureCount > 1) {{
+                System.out.printf(""{InvalidNumberOfTestCasesPrefix} "" + ""(%d) for %s. There should be a single test case per test.%n"", failureCount, testClasses[i].getSimpleName());
+            }} else {{
+                summary.getFailures().forEach(failure -> {{
+                    String failureClass = failure.getTestIdentifier().getDisplayName();
+                    String failureException = failure.getException().toString().replaceAll(""\r"", ""\\r"").replaceAll(""\n"",""\\n"");
+                    System.out.printf(""%s %s%s"", failureClass, failureException, System.lineSeparator());
+                }});
+            }}
         }}
     }}
 }}";
@@ -415,6 +427,17 @@ public class _$TestRunner {{
                 if (!isTestSuccessful)
                 {
                     var errorLine = output.ReadLine();
+
+                    if (errorLine == null)
+                    {
+                        throw new InvalidOperationException("The test was unsuccessful, but no error message was found.");
+                    }
+
+                    if (errorLine.StartsWith(InvalidNumberOfTestCasesPrefix))
+                    {
+                        throw new InvalidOperationException(errorLine);
+                    }
+
                     var errorMessage = errorLine.Substring(firstSpaceIndex);
                     errorsByFiles.Add(fileName, errorMessage);
                 }
