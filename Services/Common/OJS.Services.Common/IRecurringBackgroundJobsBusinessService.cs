af94ed2875a5351cdf34d197bbf7d9b4217ab2c4
Georgi Georgiev <gogo4ds@gmail.com>
2024-07-17 09:35:08 +0300

    Hangfire retries jobs on the same queue it was failed on (#1284)
    Closes
    https://github.com/SoftUni-Internal/exam-systems-issues/issues/1402
    
    **Summary of the changes made**:
    1. {Describe your changes in the list}
    2.

--- Services/Common/OJS.Services.Common/IRecurringBackgroundJobsBusinessService.cs
+++ Services/Common/OJS.Services.Common/IRecurringBackgroundJobsBusinessService.cs
@@ -1,8 +1,10 @@
 ﻿namespace OJS.Services.Common
 {
+    using Hangfire;
     using OJS.Services.Common.Filters;
     using OJS.Services.Infrastructure;
     using System.Threading.Tasks;
+    using static OJS.Common.GlobalConstants.BackgroundJobs;
 
     public interface IRecurringBackgroundJobsBusinessService : IService
     {
@@ -10,15 +12,19 @@
         /// Enqueues all submissions that are pending (not added in the message queue, nor processing).
         /// </summary>
         [MessageBusExceptionFilter]
+        [Queue(AdministrationQueueName)]
         Task<object> EnqueuePendingSubmissions();
 
         /// <summary>
         /// Deletes all processed (and not processing) submissions from the SubmissionsForProcessing table.
         /// </summary>
+        [Queue(AdministrationQueueName)]
         Task<object> DeleteProcessedSubmissions();
 
+        [Queue(AdministrationQueueName)]
         Task<object> UpdateTotalScoreSnapshotOfParticipants();
 
+        [Queue(AdministrationQueueName)]
         Task<object> RemoveParticipantMultipleScores();
     }
 }
