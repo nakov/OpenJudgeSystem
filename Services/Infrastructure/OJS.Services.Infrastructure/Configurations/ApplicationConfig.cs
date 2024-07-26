f00d7bff6ddecabce9a5954e3c65563e5aa90aad
Georgi Georgiev <gogo4ds@gmail.com>
2024-07-26 16:29:37 +0300

    Removed unused prop

--- Services/Infrastructure/OJS.Services.Infrastructure/Configurations/ApplicationConfig.cs
+++ Services/Infrastructure/OJS.Services.Infrastructure/Configurations/ApplicationConfig.cs
@@ -14,7 +14,4 @@ public class ApplicationConfig : BaseConfig
 
     [Required]
     public string ApiKey { get; set; } = string.Empty;
-
-    [Required]
-    public ElasticsearchConfig Elasticsearch { get; set; } = new();
 }
