ac5da5bb0250121babb14d615b6d2ae1b1e885d6
Denis Yordanov <deyordanov777@gmail.com>
2024-08-09 09:59:30 +0300

    Fixed the logic behind sending emails when Redis is down.

---
+++ Services/Infrastructure/OJS.Services.Infrastructure/Models/ExceptionCacheEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace OJS.Services.Infrastructure.Models;
+
+using System;
+
+public class ExceptionCacheEntry
+{
+    public string Value { get; set; } = null!;
+
+    public DateTime LastEmailSentTime { get; set; }
+}
