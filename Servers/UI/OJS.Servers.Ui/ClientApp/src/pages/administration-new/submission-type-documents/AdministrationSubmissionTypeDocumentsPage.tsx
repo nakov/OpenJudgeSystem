26a9690b34422f19e19bf9a0f9a1fc5a5aca4b0c
Denis Yordanov <deyordanov777@gmail.com>
2024-08-21 14:24:09 +0300

    Created the page for listing all submission type documents.

---
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/pages/administration-new/submission-type-documents/AdministrationSubmissionTypeDocumentsPage.tsx
@@ -0,0 +1,65 @@
+﻿import { useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+import { IGetAllAdminParams } from '../../../common/types';
+import { NEW_ADMINISTRATION_PATH, SUBMISSION_TYPE_DOCUMENTS_PATH } from '../../../common/urls/administration-urls';
+import CreateButton from '../../../components/administration/common/create/CreateButton';
+import SpinningLoader from '../../../components/guidelines/spinning-loader/SpinningLoader';
+import {
+    useGetAllSubmissionTypesInSubmissionDocumentsQuery,
+} from '../../../redux/services/admin/submissionTypesInSubmissionDocumentsAdminService';
+import { applyDefaultFilterToQueryString } from '../administration-filters/AdministrationFilters';
+import AdministrationGridView from '../AdministrationGridView';
+
+import submissionTypeDocumentsFilterableColumns, {
+    returnNonFilterableColumns,
+} from './submissionTypeDocumentsGridColumns';
+
+const AdministrationSubmissionTypeDocumentsPage = () => {
+    const [ searchParams ] = useSearchParams();
+    const navigate = useNavigate();
+
+    const [ queryParams, setQueryParams ] = useState<IGetAllAdminParams>(applyDefaultFilterToQueryString('', '', searchParams));
+
+    const {
+        refetch: refetchSubmissionTypesInSubmissionDocuments,
+        data: submissionTypesInSubmissionDocumentsData,
+        isFetching: isDataFetching,
+        isLoading: isDataLoading,
+        error,
+    } = useGetAllSubmissionTypesInSubmissionDocumentsQuery(queryParams);
+
+    if (isDataFetching || isDataLoading) {
+        return <SpinningLoader />;
+    }
+
+    const onDelete = () => {
+        refetchSubmissionTypesInSubmissionDocuments();
+    };
+
+    const renderGridSettings = () => (
+        // The 'showModal' parameter is not needed, since we want to use the button for navigation
+        <CreateButton
+          showModal={false}
+          showModalFunc={() => navigate(`/${NEW_ADMINISTRATION_PATH}/${SUBMISSION_TYPE_DOCUMENTS_PATH}/0?isEditMode=false`)}
+          styles={{ width: '40px', height: '40px' }}
+        />
+    );
+
+    return (
+        <AdministrationGridView
+          filterableGridColumnDef={submissionTypeDocumentsFilterableColumns}
+          notFilterableGridColumnDef={returnNonFilterableColumns(onDelete)}
+          data={submissionTypesInSubmissionDocumentsData}
+          error={error}
+          queryParams={queryParams}
+          setQueryParams={setQueryParams}
+          renderActionButtons={renderGridSettings}
+          defaultSorter=""
+          defaultFilter=""
+          specificRowIdName={[ 'submissionTypeDocumentId', 'submissionTypeId' ]}
+        />
+    );
+};
+
+export default AdministrationSubmissionTypeDocumentsPage;
---
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/pages/administration-new/submission-type-documents/submissionTypeDocumentsGridColumns.tsx
@@ -0,0 +1,104 @@
+﻿/* eslint-disable @typescript-eslint/ban-types,max-len */
+import { GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
+
+import {
+    EDIT,
+    SUBMISSION_TYPE_DOCUMENT,
+    SUBMISSION_TYPE_DOCUMENT_ID,
+    SUBMISSION_TYPE_DOCUMENT_TITLE, SUBMISSION_TYPE_ID,
+    SUBMISSION_TYPE_NAME,
+} from '../../../common/labels';
+import {
+    DELETE_CONFIRMATION_MESSAGE,
+    SUBMISSION_TYPE_DOCUMENT_DELETE_CONFIRMATION_MESSAGE,
+} from '../../../common/messages';
+import {
+    NEW_ADMINISTRATION_PATH,
+    SUBMISSION_TYPE_DOCUMENTS_PATH, SUBMISSION_TYPE_DOCUMENTS_VIEW_PATH,
+} from '../../../common/urls/administration-urls';
+import DeleteButton from '../../../components/administration/common/delete/DeleteButton';
+import RedirectButton from '../../../components/administration/common/edit/RedirectButton';
+import ViewButton from '../../../components/administration/common/view/ViewButton';
+import {
+    useDeleteSubmissionTypeDocumentByIdAndSubmissionTypeIdMutation,
+} from '../../../redux/services/admin/submissionTypeDocumentsAdminService';
+
+const submissionTypeDocumentsFilterableColumns: GridColDef[] = [
+    {
+        field: 'submissionTypeDocumentId',
+        headerName: SUBMISSION_TYPE_DOCUMENT_ID,
+        flex: 1,
+        type: 'number',
+        filterable: false,
+        sortable: false,
+        align: 'center',
+        headerAlign: 'center',
+        valueFormatter: (params) => params.value.toString(),
+    },
+    {
+        field: 'submissionTypeDocumentTitle',
+        headerName: SUBMISSION_TYPE_DOCUMENT_TITLE,
+        flex: 1,
+        type: 'string',
+        filterable: false,
+        sortable: false,
+        align: 'center',
+        headerAlign: 'center',
+    },
+    {
+        field: 'submissionTypeId',
+        headerName: SUBMISSION_TYPE_ID,
+        flex: 1,
+        type: 'number',
+        filterable: false,
+        sortable: false,
+        align: 'center',
+        headerAlign: 'center',
+    },
+    {
+        field: 'submissionTypeName',
+        headerName: SUBMISSION_TYPE_NAME,
+        flex: 1,
+        type: 'string',
+        filterable: false,
+        sortable: false,
+        align: 'center',
+        headerAlign: 'center',
+    },
+];
+
+export const returnNonFilterableColumns = (onSuccessfulDelete: () => void) => [
+    {
+        field: 'actions',
+        headerName: 'Actions',
+        flex: 1,
+        headerAlign: 'center',
+        align: 'center',
+        filterable: false,
+        sortable: false,
+        renderCell: (params: GridRenderCellParams) => (
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                <ViewButton
+                  path={`/${NEW_ADMINISTRATION_PATH}/${SUBMISSION_TYPE_DOCUMENTS_VIEW_PATH}?submissionTypeIds=${params.row.submissionTypeId}`}
+                  text={`View '${params.row.submissionTypeDocumentTitle}'`}
+                />
+                <RedirectButton
+                  path={`/${NEW_ADMINISTRATION_PATH}/${SUBMISSION_TYPE_DOCUMENTS_PATH}/${Number(params.row.submissionTypeDocumentId)}?isEditMode=true`}
+                  location={`${EDIT} page`}
+                />
+                <DeleteButton
+                  id={{
+                      firstEntityId: params.row.submissionTypeId,
+                      secondEntityId: params.row.submissionTypeDocumentId,
+                  }}
+                  name={`${SUBMISSION_TYPE_DOCUMENT}`}
+                  text={`${DELETE_CONFIRMATION_MESSAGE} ${SUBMISSION_TYPE_DOCUMENT_DELETE_CONFIRMATION_MESSAGE}`}
+                  mutation={useDeleteSubmissionTypeDocumentByIdAndSubmissionTypeIdMutation}
+                  onSuccess={onSuccessfulDelete}
+                />
+            </div>
+        ),
+    },
+] as GridColDef[];
+
+export default submissionTypeDocumentsFilterableColumns;
