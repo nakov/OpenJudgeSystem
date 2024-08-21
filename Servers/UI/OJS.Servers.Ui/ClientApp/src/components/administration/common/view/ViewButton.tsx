fd2031489577b38b1f3f6cc37631464bc75d5fd4
Denis Yordanov <deyordanov777@gmail.com>
2024-08-21 14:28:51 +0300

    Added a reusable 'ViewButton'.

---
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/administration/common/view/ViewButton.tsx
@@ -0,0 +1,24 @@
+﻿import { Link } from 'react-router-dom';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import { IconButton, Tooltip } from '@mui/material';
+
+interface IViewButtonProps {
+    path: string;
+    text: string;
+}
+
+const ViewButton = (props: IViewButtonProps) => {
+    const { path, text } = props;
+
+    return (
+        <Tooltip title={text}>
+            <Link to={path}>
+                <IconButton>
+                    <VisibilityIcon color="primary" />
+                </IconButton>
+            </Link>
+        </Tooltip>
+    );
+};
+
+export default ViewButton;
