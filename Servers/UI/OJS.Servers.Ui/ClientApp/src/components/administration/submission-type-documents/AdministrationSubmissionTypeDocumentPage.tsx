95181ae61e1f4d518bd548083eb67d5b423fdf9e
Denis Yordanov <deyordanov777@gmail.com>
2024-08-21 14:25:29 +0300

    Create the page for creating / editing a submission type document.

---
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/administration/submission-type-documents/AdministrationSubmissionTypeDocumentPage.tsx
@@ -0,0 +1,26 @@
+﻿import { useLocation } from 'react-router-dom';
+
+import TabsInView from '../common/tabs/TabsInView';
+
+import SubmissionTypeDocumentForm from './submission-type-document-form/SubmissionTypeDocumentForm';
+
+const AdministrationSubmissionTypeDocumentPage = () => {
+    const location = useLocation();
+    const [ , , , submissionTypeDocumentId ] = location.pathname.split('/');
+
+    const params = new URLSearchParams(location.search);
+
+    const isEditMode = params.get('isEditMode') === 'true'
+        ? true
+        : false ?? false;
+
+    const renderSubmissionTypeDocumentForm = () => (
+        <SubmissionTypeDocumentForm id={Number(submissionTypeDocumentId)} isEditMode={isEditMode} />
+    );
+
+    return (
+        <TabsInView form={renderSubmissionTypeDocumentForm} />
+    );
+};
+
+export default AdministrationSubmissionTypeDocumentPage;
