6212f649dc3d1922c690c6bb0e7030a4a34dcf7b
Denis Yordanov <deyordanov777@gmail.com>
2024-08-21 14:19:51 +0300

    Created the 'SubmissionTypeDocument' and 'SubmissionTypeInSubmissionDocument' entities.

---
+++ Data/OJS.Data.Models/SubmissionTypeInSubmissionDocument.cs
@@ -0,0 +1,15 @@
+﻿namespace OJS.Data.Models;
+
+using OJS.Data.Models.Common;
+using OJS.Data.Models.Submissions;
+
+public class SubmissionTypeInSubmissionDocument : IEntity
+{
+    public int SubmissionTypeId { get; set; }
+
+    public SubmissionType SubmissionType { get; set; } = null!;
+
+    public int SubmissionTypeDocumentId { get; set; }
+
+    public virtual SubmissionTypeDocument SubmissionTypeDocument { get; set; } = null!;
+}
