3e8f7d2dda1a0583522d2617672b8beebe94c37a
Denis Yordanov <deyordanov777@gmail.com>
2024-08-27 17:27:25 +0300

    Added a custom migration to set all empty files to null.

---
+++ Data/OJS.Data/Migrations/20240827161142_SetNullForEmptyFilesInProblemResources.cs
@@ -0,0 +1,22 @@
+﻿#nullable disable
+
+namespace OJS.Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    /// <inheritdoc />
+    public partial class SetNullForEmptyFilesInProblemResources : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder) =>
+            migrationBuilder.Sql(@"
+                    UPDATE ProblemResources
+                    SET [File] = NULL, [FileExtension] = NULL
+                    WHERE [File] is not null and DATALENGTH([File]) = 0;");
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
