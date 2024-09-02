ac4c574dd62e4efd13efd6f925d8ff769a54f7e4
Georgi Georgiev <gogo4ds@gmail.com>
2024-09-02 14:45:37 +0300

    Don't show AdministrationLink for regular users

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/guidelines/buttons/AdministrationLink.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/guidelines/buttons/AdministrationLink.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 
 import { NEW_ADMINISTRATION_PATH } from '../../../common/urls/administration-urls';
+import { useAppSelector } from '../../../redux/store';
 
 import { ButtonSize, LinkButton, LinkButtonType } from './Button';
 
@@ -12,19 +13,27 @@ interface IAdministrationLinkProps {
     children?: ReactNode;
 }
 
-const AdministrationLink = ({ text, to, type, className, children }: IAdministrationLinkProps) => (
-    <LinkButton
-      text={text}
-      size={ButtonSize.small}
-      type={type ?? LinkButtonType.secondary}
-      to={`/${NEW_ADMINISTRATION_PATH}${to.startsWith('/')
-          ? ''
-          : '/'}${to}`}
-      isToExternal
-      className={className}
-    >
-        {children}
-    </LinkButton>
-);
+const AdministrationLink = ({ text, to, type, className, children }: IAdministrationLinkProps) => {
+    const { internalUser: user } = useAppSelector((state) => state.authorization);
+
+    if (!user.canAccessAdministration) {
+        return null;
+    }
+
+    return (
+        <LinkButton
+          text={text}
+          size={ButtonSize.small}
+          type={type ?? LinkButtonType.secondary}
+          to={`/${NEW_ADMINISTRATION_PATH}${to.startsWith('/')
+              ? ''
+              : '/'}${to}`}
+          isToExternal
+          className={className}
+        >
+            {children}
+        </LinkButton>
+    );
+};
 
 export default AdministrationLink;
