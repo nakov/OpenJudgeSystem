b657034df2615a7289017d06155658ed31a297d7
Georgi Georgiev <gogo4ds@gmail.com>
2024-09-02 16:04:14 +0300

    Removed unused model

--- Tools/DeployEmailGenerator/Models/GitHubConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace DeployEmailGenerator.Models;
-
-public class GitHubConfig
-{
-    public string ForDeployMilestoneNodeId { get; set; }
-    public string DoneMilestoneNodeId { get; set; }
-    public string Token { get; set; }
-}
