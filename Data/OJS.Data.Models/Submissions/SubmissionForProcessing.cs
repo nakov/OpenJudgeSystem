569875f90bc63410fa75ee8a6e4ee6c68eb16c3b
Georgi Georgiev <gogo4ds@gmail.com>
2024-09-11 13:04:47 +0300

    Added Enqueued status to SubmissionForProcessing.cs data model

--- Data/OJS.Data.Models/Submissions/SubmissionForProcessing.cs
+++ Data/OJS.Data.Models/Submissions/SubmissionForProcessing.cs
@@ -1,13 +1,22 @@
 namespace OJS.Data.Models.Submissions
 {
     using OJS.Data.Models.Common;
+    using System;
 
     public class SubmissionForProcessing : AuditInfoEntity<int>
     {
         public int SubmissionId { get; set; }
 
+        public bool Enqueued { get; set; }
+
         public bool Processing { get; set; }
 
         public bool Processed { get; set; }
+
+        public DateTimeOffset? EnqueuedAt { get; set; }
+
+        public DateTimeOffset? ProcessingStartedAt { get; set; }
+
+        public DateTimeOffset? ProcessedAt { get; set; }
     }
 }
---
+++ Data/OJS.Data/Migrations/20240911093856_AddEnqueuedStateToSubmissionForProcessing.cs
@@ -0,0 +1,60 @@
+﻿#nullable disable
+
+namespace OJS.Data.Migrations
+{
+    using System;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    /// <inheritdoc />
+    public partial class AddEnqueuedStateToSubmissionForProcessing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Enqueued",
+                table: "SubmissionsForProcessing",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "EnqueuedAt",
+                table: "SubmissionsForProcessing",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ProcessedAt",
+                table: "SubmissionsForProcessing",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ProcessingStartedAt",
+                table: "SubmissionsForProcessing",
+                type: "datetimeoffset",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Enqueued",
+                table: "SubmissionsForProcessing");
+
+            migrationBuilder.DropColumn(
+                name: "EnqueuedAt",
+                table: "SubmissionsForProcessing");
+
+            migrationBuilder.DropColumn(
+                name: "ProcessedAt",
+                table: "SubmissionsForProcessing");
+
+            migrationBuilder.DropColumn(
+                name: "ProcessingStartedAt",
+                table: "SubmissionsForProcessing");
+        }
+    }
+}
