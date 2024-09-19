300c786a5037efe9cad34b989aefec3b91565ef8
Denis Yordanov <deyordanov777@gmail.com>
2024-09-19 15:20:27 +0300

    The users' autocomplete can now be queried based on 'roleId' as well.

---
+++ Services/Administration/OJS.Services.Administration.Models/Contests/LecturerInContestActionsModel.cs
@@ -0,0 +1,22 @@
+﻿namespace OJS.Services.Administration.Models.Contests;
+
+using AutoMapper;
+using OJS.Data.Models;
+using OJS.Services.Infrastructure.Models.Mapping;
+
+public class LecturerInContestActionsModel : IMapExplicitly
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+    public void RegisterMappings(IProfileExpression configuration)
+        => configuration.CreateMap<LecturerInContest, LecturerInContestActionsModel>()
+            .ForMember(
+                dest => dest.Id,
+                opt => opt.MapFrom(
+                    s => s.ContestId))
+            .ForMember(
+                dest => dest.Name,
+                opt => opt.MapFrom(
+                    s => s.Contest.Name));
+}
