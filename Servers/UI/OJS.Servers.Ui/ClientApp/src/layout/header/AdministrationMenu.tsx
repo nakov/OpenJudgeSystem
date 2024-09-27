00d79744bba7ab9bb474eb79ac6284bced03c4aa
Simona Simeonova <simona.simeonova@softuni.bg>
2024-09-27 15:48:35 +0300

    FE improvements and fixed menu is shown for users not in role and lint fixes
    https://github.com/SoftUni-Internal/exam-systems-issues/issues/1471

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/layout/header/AdministrationMenu.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/layout/header/AdministrationMenu.tsx
@@ -18,6 +18,7 @@ import {
 import Button, { ButtonType } from '../../components/guidelines/buttons/Button';
 import useNavigation from '../../hooks/common/use-routing';
 import useTheme from '../../hooks/use-theme';
+import { useAppSelector } from '../../redux/store';
 import concatClassNames from '../../utils/class-names';
 
 import styles from './AdministrationMenu.module.scss';
@@ -25,6 +26,7 @@ import styles from './AdministrationMenu.module.scss';
 const AdministrationMenu = () => {
     const [ isMenuVisible, setMenuVisible ] = useState(false);
     const { navigateInNewWindow } = useNavigation();
+    const { internalUser: user } = useAppSelector((state) => state.authorization);
 
     const { themeColors, getColorClassName } = useTheme();
 
@@ -38,7 +40,7 @@ const AdministrationMenu = () => {
 
     const onClickNavigate = (administrationEntity: string) => navigateInNewWindow(`/${NEW_ADMINISTRATION_PATH}/${administrationEntity}`);
 
-    return (
+    return user.canAccessAdministration && (
         <div
           className={styles.adminMenuContainer}
           onMouseOver={handleMouseEnter}
@@ -68,27 +70,33 @@ const AdministrationMenu = () => {
                 >
                     <div className={styles.menuSection}>
                         <span onClick={() => onClickNavigate(CONTESTS_PATH)}>Contests</span>
-                        <span onClick={() => onClickNavigate(CONTEST_CATEGORIES_PATH)}>Categories</span>
-                        <span onClick={() => onClickNavigate(PARTICIPANTS_PATH)}>Participants</span>
                         <span onClick={() => onClickNavigate(EXAM_GROUPS_PATH)}>Exam Groups</span>
                         <span onClick={() => onClickNavigate(SUBMISSIONS_PATH)}>Submissions</span>
+                        {user.isAdmin && (
+                            <span onClick={() => onClickNavigate(CONTEST_CATEGORIES_PATH)}>Categories</span>)}
+                        {user.isAdmin && (
+                            <span onClick={() => onClickNavigate(PARTICIPANTS_PATH)}>Participants</span>)}
                     </div>
-                    <div className={styles.menuSection}>
-                        <span onClick={() => onClickNavigate(PROBLEMS_PATH)}>Problems</span>
-                        <span onClick={() => onClickNavigate(PROBLEM_GROUPS_PATH)}>Problem Groups</span>
-                        <span onClick={() => onClickNavigate(TESTS_PATH)}>Tests</span>
-                        <span onClick={() => onClickNavigate(SUBMISSION_TYPES_PATH)}>Submission Types</span>
-                    </div>
-                    <div className={styles.menuSection}>
-                        <span onClick={() => onClickNavigate(USERS_PATH)}>Users</span>
-                        <span onClick={() => onClickNavigate(ROLES_PATH)}>Roles</span>
-                        {/* TODO */}
-                        {/* <span onClick={() => onClickNavigate(TESTS_PATH)}>
-                        Lecturers in contests and categories</span> */}
-                    </div>
-                    <div className={styles.menuSection}>
-                        <span onClick={() => onClickNavigate('')}>All Administrations</span>
-                    </div>
+                    {user.isAdmin && (
+                        <>
+                            <div className={styles.menuSection}>
+                                <span onClick={() => onClickNavigate(PROBLEMS_PATH)}>Problems</span>
+                                <span onClick={() => onClickNavigate(PROBLEM_GROUPS_PATH)}>Problem Groups</span>
+                                <span onClick={() => onClickNavigate(TESTS_PATH)}>Tests</span>
+                                <span onClick={() => onClickNavigate(SUBMISSION_TYPES_PATH)}>Submission Types</span>
+                            </div>
+                            <div className={styles.menuSection}>
+                                <span onClick={() => onClickNavigate(USERS_PATH)}>Users</span>
+                                <span onClick={() => onClickNavigate(ROLES_PATH)}>Roles</span>
+                                {/* TODO */}
+                                {/* <span onClick={() => onClickNavigate(TESTS_PATH)}>
+                                Lecturers in contests and categories</span> */}
+                            </div>
+                            <div className={styles.menuSection}>
+                                <span onClick={() => onClickNavigate('')}>All Administrations</span>
+                            </div>
+                        </>
+                    )}
                 </div>
             )}
         </div>
