d186f77205b0e7e85a1ae2236ce18dd9e808dc20
Denis Yordanov <deyordanov777@gmail.com>
2024-09-27 13:02:53 +0300

    Fixed the white background on page reload when the dark theme is selected.

---
+++ Servers/UI/OJS.Servers.Ui/ClientApp/public/admin-theme-init.js
@@ -0,0 +1,19 @@
+﻿(function() {
+    try {
+        const mode = localStorage.getItem("administrationMode");
+        if (!mode) {
+            document.body.classList.add("body-dark-theme");
+            return;
+        }
+
+        if (mode === "dark") {
+            document.body.classList.add("body-dark-theme");
+            document.body.classList.remove("body-light-theme");
+        } else {
+            document.body.classList.add("body-light-theme");
+            document.body.classList.remove("body-dark-theme");
+        }
+    } catch (e) {
+        console.error("Error applying theme for administration from localStorage:", e);
+    }
+})();
---
+++ Servers/UI/OJS.Servers.Ui/ClientApp/public/client-theme-init.js
@@ -0,0 +1,28 @@
+﻿(function() {
+    try {
+        const persistRoot = localStorage.getItem("persist:root");
+        if (!persistRoot) {
+            document.body.classList.add("body-dark-theme");
+            return;
+        }
+
+        const persist = JSON.parse(persistRoot);
+
+        if (!persist.theme) {
+            document.body.classList.add("body-dark-theme");
+            return;
+        }
+
+        const theme = JSON.parse(persist.theme);
+
+        if (theme.mode === "dark") {
+            document.body.classList.add("body-dark-theme");
+            document.body.classList.remove("body-light-theme");
+        } else {
+            document.body.classList.add("body-light-theme");
+            document.body.classList.remove("body-dark-theme");
+        }
+    } catch (e) {
+        console.error("Error applying theme for client from localStorage:", e);
+    }
+})();
