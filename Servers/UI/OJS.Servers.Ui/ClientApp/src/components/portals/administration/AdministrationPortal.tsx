e677b0b456ef03eed0c4ea5b85582a7771c95906
Denis Yordanov <deyordanov777@gmail.com>
2024-10-01 16:21:09 +0300

    Removed the default profile icon and added icons for all user actions, added tooltips for the administration's sections names.

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/portals/administration/AdministrationPortal.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/portals/administration/AdministrationPortal.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
 
 import { ChangeEvent, useEffect, useRef, useState } from 'react';
-import { FaCheckDouble, FaLayerGroup, FaUserCircle, FaUsers } from 'react-icons/fa';
+import { FaCheckDouble, FaLayerGroup, FaUsers } from 'react-icons/fa';
 import { GiFiles } from 'react-icons/gi';
 import { IoMdCheckbox } from 'react-icons/io';
 import { IoSettingsSharp } from 'react-icons/io5';
@@ -24,24 +24,21 @@ import {
     CssBaseline,
     CSSObject,
     Divider,
-    FormControlLabel,
     IconButton,
     List,
     ListItem,
     ListItemButton,
     ListItemIcon,
     ListItemText,
-    Menu,
-    MenuItem,
     styled,
-    Switch,
     Theme,
-    Tooltip,
+    Tooltip, tooltipClasses, TooltipProps,
     Typography,
 } from '@mui/material';
 import MuiDrawer from '@mui/material/Drawer';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import UserActions from 'src/components/administration/common/user-actions/UserActions';
 
 import 'dayjs/locale/bg';
 
@@ -114,58 +111,10 @@ import AdministrationSubmissionTypeDocumentPage
     from '../../administration/submission-type-documents/AdministrationSubmissionTypeDocumentPage';
 import AdministrationTest from '../../administration/tests/AdministrationTest';
 import AdministrationUser from '../../administration/users/AdministrationUser';
-import { LinkButton, LinkButtonType } from '../../guidelines/buttons/Button';
 
 import styles from './AdministrationPortal.module.scss';
 
 const drawerWidth = 240;
-
-const MaterialUISwitch = styled(Switch)(({ theme }) => ({
-    width: 52,
-    height: 26,
-    padding: 7,
-    '& .MuiSwitch-switchBase': {
-        margin: 1,
-        padding: 0,
-        transform: 'translateX(6px)',
-        '&.Mui-checked': {
-            color: '#fff',
-            transform: 'translateX(22px)',
-            '& .MuiSwitch-thumb:before': { backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent('#fff')}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')` },
-            '& + .MuiSwitch-track': {
-                opacity: 1,
-                backgroundColor: theme.palette.mode === 'dark'
-                    ? '#8796A5'
-                    : '#aab4be',
-            },
-        },
-    },
-    '& .MuiSwitch-thumb': {
-        backgroundColor: theme.palette.mode === 'dark'
-            ? '#003892'
-            : '#001e3c',
-        width: 24,
-        height: 24,
-        '&::before': {
-            content: '\'\'',
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-            left: 0,
-            top: 0,
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-            backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent('#fff')}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
-        },
-    },
-    '& .MuiSwitch-track': {
-        opacity: 1,
-        backgroundColor: theme.palette.mode === 'dark'
-            ? '#8796A5'
-            : '#aab4be',
-        borderRadius: 20 / 2,
-    },
-}));
 
 const administrationItems = [
     {
@@ -308,6 +257,34 @@ const DrawerHeader = styled('div')(({ theme }) => ({
     ...theme.mixins.toolbar,
 }));
 
+const SectionTooltip = styled(({ className, ...props }: TooltipProps) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+        backgroundColor: theme.palette.mode === ThemeMode.Light
+            ? '#fff'
+            : '#444',
+        color: theme.palette.mode === ThemeMode.Light
+            ? '#000'
+            : '#fff',
+        boxShadow: theme.shadows[1],
+        fontSize: 14,
+        border: theme.palette.mode === ThemeMode.Light
+            ? '1px solid #ccc'
+            : '1px solid #666',
+    },
+    [`& .${tooltipClasses.arrow}`]: {
+        color: theme.palette.mode === ThemeMode.Light
+            ? '#fff'
+            : '#444',
+        '&::before': {
+            border: theme.palette.mode === ThemeMode.Light
+                ? '1px solid #ccc'
+                : '1px solid #666',
+        },
+    },
+}));
+
 const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(({ theme, open }) => ({
     width: drawerWidth,
     flexShrink: 0,
@@ -364,7 +341,12 @@ const AdministrationPortal = () => {
     }, []);
 
     const handleResize = () => {
-        setOpen(!(window.innerWidth < mobileBreak));
+        const isDesktop = window.innerWidth >= mobileBreak;
+        setOpen(isDesktop);
+
+        if (isDesktop) {
+            setShowMenu(false);
+        }
     };
 
     const capitalizeFirstLetter = (value: string) => value.charAt(0).toUpperCase() + value.slice(1);
@@ -394,6 +376,7 @@ const AdministrationPortal = () => {
             dispatch(toggleAdministrationThemeMode(ThemeMode.Dark));
         }
     };
+
     const adminRoutes = [
         {
             path: `${CONTESTS_PATH}`,
@@ -547,17 +530,6 @@ const AdministrationPortal = () => {
         },
     ];
 
-    const renderSectionicon = (name: string, icon: any) => {
-        if (!open) {
-            return (
-                <Tooltip title={name}>
-                    <div>{icon}</div>
-                </Tooltip>
-            );
-        }
-        return icon;
-    };
-
     return (
         <AdministrationThemeProvider mode={currentThemeMode}>
             <CssBaseline />
@@ -588,75 +560,64 @@ const AdministrationPortal = () => {
                                     <ChevronLeftIcon />
                                 </IconButton>
                             )}
-                        <FormControlLabel
-                          sx={{ position: 'absolute', top: 1, right: 1, marginRight: '0.5rem', marginTop: '0.5rem' }}
-                          control={(
-                              <MaterialUISwitch
-                                onChange={handleThemeChange}
-                                checked={currentThemeMode === ThemeMode.Light}
-                              />
-                            )}
-                          label=""
-                        />
                         <DrawerHeader className={styles.drawerHeader}>
-                            <IconButton ref={iconButtonRef} onClick={() => setShowMenu(!showMenu)}>
-                                <FaUserCircle className={styles.profileIcon} />
-                            </IconButton>
-                            {open && (
-                                <>
-                                    <Typography variant="subtitle1" className={styles.userName}>
-                                        {user.userName}
-                                    </Typography>
-                                    <Divider sx={{ color: 'red', width: '90%' }} />
-                                </>
-                            )}
-                            <Menu
-                              anchorEl={iconButtonRef.current}
-                              open={showMenu}
-                              onClose={() => setShowMenu(false)}
-                            >
-                                <MenuItem>
-                                    <LinkButton
-                                      to="/logout"
-                                      isToExternal
-                                      type={LinkButtonType.plain}
-                                      className={styles.adminHeaderLink}
-                                      text="Log out"
-                                    />
-                                </MenuItem>
-                            </Menu>
+                            <UserActions
+                              isDropdown={!open}
+                              handleThemeChange={handleThemeChange}
+                              currentThemeMode={currentThemeMode}
+                              showMenu={showMenu}
+                              setShowMenu={setShowMenu}
+                              iconButtonRef={iconButtonRef}
+                            />
+                            <DrawerHeader className={styles.username}>
+                                {open && (
+                                    <>
+                                        <Typography variant="subtitle1">
+                                            {user.userName}
+                                        </Typography>
+                                        <Divider sx={{ color: 'red', width: '90%' }} />
+                                    </>
+                                )}
+                            </DrawerHeader>
                         </DrawerHeader>
                         <List className={styles.list}>
                             <Divider />
                             {administrationItems.map((item) => (user.isAdmin || !item.visibleOnlyForAdmin) && (
-                                <Box key={item.path}>
-                                    <ListItem key={item.name} disablePadding>
-                                        <Link
-                                          to={item.path}
-                                          className={`${isSelected(item.path)
-                                              ? styles.activeAdminNavLink
-                                              : ''} ${
-                                              styles.adminNavLink
-                                          }`}
-                                        >
-                                            <ListItemButton
-                                              className={isSelected(item.path)
-                                                  ? styles.selectedSection
-                                                  : ''}
+                                <SectionTooltip
+                                  key={item.path}
+                                  title={item.name}
+                                  placement="right"
+                                  arrow
+                                >
+                                    <div>
+                                        <ListItem key={item.name} disablePadding>
+                                            <Link
+                                              to={item.path}
+                                              className={`${isSelected(item.path)
+                                                  ? styles.activeAdminNavLink
+                                                  : ''} ${
+                                                  styles.adminNavLink
+                                              }`}
                                             >
-                                                <ListItemIcon
+                                                <ListItemButton
                                                   className={isSelected(item.path)
-                                                      ? styles.listItemIcon
+                                                      ? styles.selectedSection
                                                       : ''}
                                                 >
-                                                    {renderSectionicon(item.name, item.icon)}
-                                                </ListItemIcon>
-                                                <ListItemText primary={item.name} />
-                                            </ListItemButton>
-                                        </Link>
-                                    </ListItem>
-                                    <Divider />
-                                </Box>
+                                                    <ListItemIcon
+                                                      className={isSelected(item.path)
+                                                          ? styles.listItemIcon
+                                                          : ''}
+                                                    >
+                                                        {item.icon}
+                                                    </ListItemIcon>
+                                                    <ListItemText primary={item.name} />
+                                                </ListItemButton>
+                                            </Link>
+                                        </ListItem>
+                                        <Divider />
+                                    </div>
+                                </SectionTooltip>
                             ))}
                         </List>
                     </Drawer>
@@ -691,4 +652,5 @@ const AdministrationPortal = () => {
         </AdministrationThemeProvider>
     );
 };
+
 export default AdministrationPortal;
