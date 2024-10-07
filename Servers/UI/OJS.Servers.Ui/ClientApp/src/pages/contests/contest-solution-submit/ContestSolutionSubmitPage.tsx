a49127662cf69c497d1ff3989b7f57473bff9b4b
Denis Yordanov <deyordanov777@gmail.com>
2024-10-07 08:53:58 +0300

    Added a message when a submission is sent successfully.

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/pages/contests/contest-solution-submit/ContestSolutionSubmitPage.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/pages/contests/contest-solution-submit/ContestSolutionSubmitPage.tsx
@@ -8,6 +8,9 @@ import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
 import { Tooltip } from '@mui/material';
 import Popover from '@mui/material/Popover';
 import moment from 'moment';
+import { SUBMISSION_SENT } from 'src/common/messages';
+import useSuccessMessageEffect from 'src/hooks/common/use-success-message-effect';
+import { renderSuccessfullAlert } from 'src/utils/render-utils';
 
 import { ContestParticipationType } from '../../../common/constants';
 import { IProblemResourceType, IProblemType, ISubmissionTypeType } from '../../../common/types';
@@ -58,6 +61,7 @@ const ContestSolutionSubmitPage = () => {
     const { themeColors, getColorClassName } = useTheme();
     const { contestId, participationType, slug } = useParams();
 
+    const [ successMessage, setSuccessMessage ] = useState<string | null>(null);
     const [ isSubmitButtonDisabled, setIsSubmitButtonDisabled ] = useState<boolean>(false);
     const [ remainingTime, setRemainingTime ] = useState<number>(0);
     const [ remainingTimeForCompete, setRemainingTimeForCompete ] = useState<string | null>();
@@ -89,12 +93,14 @@ const ContestSolutionSubmitPage = () => {
 
     const [ submitSolution, {
         error: submitSolutionError,
+        isSuccess: submitSolutionSuccess,
         isError: submitSolutionHasError,
         isLoading: submitSolutionIsLoading,
     } ] = useSubmitContestSolutionMutation();
 
     const [ submitSolutionFile, {
         error: submitSolutionFileError,
+        isSuccess: submitSolutionFileSuccess,
         isError: submitSolutionFileHasError,
         isLoading: submitSolutionFileIsLoading,
     } ] = useSubmitContestSolutionFileMutation();
@@ -162,6 +168,15 @@ const ContestSolutionSubmitPage = () => {
             isOfficial: isCompete,
         });
     };
+
+    useSuccessMessageEffect({
+        data: [
+            { message: SUBMISSION_SENT, shouldGet: submitSolutionSuccess },
+            { message: SUBMISSION_SENT, shouldGet: submitSolutionFileSuccess },
+        ],
+        setSuccessMessage,
+        clearFlags: [ submitSolutionIsLoading, submitSolutionFileIsLoading ],
+    });
 
     useEffect(() => {
         if (submissionsData?.items && problems && submissionsData.items.length > 0) {
@@ -674,6 +689,7 @@ const ContestSolutionSubmitPage = () => {
 
     return (
         <div className={`${styles.contestSolutionSubmitWrapper} ${textColorClassName}`}>
+            {renderSuccessfullAlert(successMessage)}
             <ContestBreadcrumbs />
             <div className={styles.nameWrapper}>
                 <Link
