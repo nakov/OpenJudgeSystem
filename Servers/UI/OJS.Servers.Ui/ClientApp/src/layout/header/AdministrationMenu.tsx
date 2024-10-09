0de3ae51f3f3b0f4bd8dbfc51ebf399e0c9e1891
Simona Simeonova <simona.simeonova@softuni.bg>
2024-10-09 08:53:09 +0300

    Removed unused import

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/layout/header/AdministrationMenu.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/layout/header/AdministrationMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { GoGear } from 'react-icons/go';
 import { IoIosArrowDown } from 'react-icons/io';
 import isNilOrEmpty from 'src/utils/check-utils';
