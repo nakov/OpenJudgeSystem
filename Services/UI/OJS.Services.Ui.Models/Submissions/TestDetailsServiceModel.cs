f6df47ce206d99efda82c27cc5a81e3bf3562a42
Denis Yordanov <deyordanov777@gmail.com>
2024-10-23 09:33:38 +0300

    Added a model only with the necessary test data.

---
+++ Services/UI/OJS.Services.Ui.Models/Submissions/TestDetailsServiceModel.cs
@@ -0,0 +1,15 @@
+﻿namespace OJS.Services.Ui.Models.Submissions;
+
+using OJS.Data.Models.Tests;
+using OJS.Services.Infrastructure.Models.Mapping;
+
+public class TestDetailsServiceModel : IMapFrom<Test>
+{
+    public int Id { get; set; }
+
+    public bool IsTrialTest { get; set; }
+
+    public bool IsOpenTest { get; set; }
+
+    public bool HideInput { get; set; }
+}
