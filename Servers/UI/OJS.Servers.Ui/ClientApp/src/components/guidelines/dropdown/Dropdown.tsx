6ed7f1563445cb87d5256ad083e6bf8863419cf9
Denis Yordanov <deyordanov777@gmail.com>
2024-10-29 14:55:41 +0200

    Made the 'Dropdown' component searchable and reusable.

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/guidelines/dropdown/Dropdown.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/guidelines/dropdown/Dropdown.tsx
@@ -1,21 +1,37 @@
-import { MenuItem, Select } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Autocomplete, Popper, TextField } from '@mui/material';
+import { IDropdownItem } from 'src/common/types';
 
 import useTheme from '../../../hooks/use-theme';
 
+// eslint-disable-next-line css-modules/no-unused-class
 import styles from './Dropdown.module.scss';
-
-interface IDropdownItem {
-    id: number;
-    name: string;
-}
 
 interface IDropdownProps {
     dropdownItems: Array<IDropdownItem>;
-    value: string;
+    value: IDropdownItem | null;
     handleDropdownItemClick?: (arg?: any) => any;
     placeholder?: string;
     isDisabled?: boolean;
+    noOptionsFoundText?: string;
 }
+
+const StyledPopper = (popperProps: any) => (
+    <Popper
+      {...popperProps}
+      placement="bottom-start"
+      modifiers={[
+          {
+              name: 'flip',
+              enabled: false,
+          },
+          {
+              name: 'offset',
+              options: { offset: [ 0, 8 ] },
+          },
+      ]}
+    />
+);
 
 const Dropdown = (props: IDropdownProps) => {
     const {
@@ -24,37 +40,66 @@ const Dropdown = (props: IDropdownProps) => {
         handleDropdownItemClick,
         isDisabled = false,
         placeholder = 'Select element',
+        noOptionsFoundText = 'No options found',
     } = props;
 
-    const { getColorClassName, themeColors } = useTheme();
+    const { isDarkMode } = useTheme();
+    const theme = isDarkMode
+        ? 'dark'
+        : 'light';
 
-    const textColorClassName = getColorClassName(themeColors.textColor);
+    const [ inputValue, setInputValue ] = useState('');
+
+    useEffect(() => {
+        if (value) {
+            setInputValue(value.name);
+        } else {
+            setInputValue('');
+        }
+    }, [ value ]);
+
+    const autocompleteClasses = {
+        root: styles[`${theme}Autocomplete`],
+        inputRoot: `${styles.inputRoot} ${styles[`${theme}InputRoot`]}`,
+        paper: styles[`${theme}Paper`],
+        option: `${styles.option} ${styles[`${theme}Option`]}`,
+        noOptions: `${styles.option} ${styles[`${theme}NoOptions`]}`,
+        listbox: styles[`${theme}Listbox`],
+        input: `${styles.input} ${styles[`${theme}Input`]}`,
+    };
+
+    const textFieldClasses = {
+        root: `${styles.textField} ${styles[`${theme}TextField`]}`,
+        input: `${styles.input} ${styles[`${theme}Input`]}`,
+    };
 
     return (
-        <Select
-          sx={{ '.MuiSvgIcon-root ': { fill: themeColors.textColor } }}
-          className={`${styles.dropdown} ${textColorClassName}`}
+        <Autocomplete
+          options={dropdownItems}
+          getOptionLabel={(option) => option.name}
           value={value}
-          autoWidth
-          displayEmpty
+          inputValue={inputValue}
+          onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue.trim() === ''
+                  ? ''
+                  : newInputValue);
+          }}
+          onChange={(event, newValue) => {
+              handleDropdownItemClick?.(newValue);
+          }}
+          noOptionsText={noOptionsFoundText}
           disabled={isDisabled}
-          MenuProps={{ MenuListProps: { disablePadding: true } }}
-        >
-            <MenuItem key="dropdown-default-item" value="" disabled>
-                {placeholder}
-            </MenuItem>
-            {dropdownItems.map((item: IDropdownItem) => (
-                <MenuItem
-                  key={`dropdown-item-${item.id}`}
-                  value={item.id}
-                  selected
-                  onClick={() => handleDropdownItemClick?.(item)}
-                  className={textColorClassName}
-                >
-                    {item.name}
-                </MenuItem>
-            ))}
-        </Select>
+          PopperComponent={StyledPopper}
+          classes={autocompleteClasses}
+          renderInput={(params) => (
+              <TextField
+                {...params}
+                placeholder={placeholder}
+                classes={textFieldClasses}
+              />
+          )}
+          ListboxProps={{ style: { maxHeight: '400px' } }}
+        />
     );
 };
 
