0bc3e9db6b85f70626ee7811e7c3d1d57a92314c
Georgi Georgiev <gogo4ds@gmail.com>
2024-11-13 01:16:55 +0200

    Removed unused method

--- Services/UI/OJS.Services.Ui.Business/Cache/ISubmissionTypesCacheService.cs
+++ Services/UI/OJS.Services.Ui.Business/Cache/ISubmissionTypesCacheService.cs
@@ -5,14 +5,9 @@ using OJS.Services.Infrastructure;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using OJS.Services.Infrastructure.Constants;
-using OJS.Services.Ui.Models.Cache;
 
 public interface ISubmissionTypesCacheService : IService
 {
     Task<IEnumerable<SubmissionTypeFilterServiceModel>> GetAllOrderedByLatestUsage(
-        int cacheSeconds = CacheConstants.OneDayInSeconds);
-
-    Task<SubmissionTypeCacheModel?> GetById(
-        int submissionTypeId,
         int cacheSeconds = CacheConstants.OneDayInSeconds);
 }
--- Services/UI/OJS.Services.Ui.Business/Cache/Implementations/SubmissionTypesCacheService.cs
+++ Services/UI/OJS.Services.Ui.Business/Cache/Implementations/SubmissionTypesCacheService.cs
@@ -5,36 +5,23 @@ using OJS.Services.Infrastructure.Constants;
 using OJS.Services.Ui.Models.SubmissionTypes;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using OJS.Services.Ui.Data;
-using OJS.Services.Ui.Models.Cache;
 
 public class SubmissionTypesCacheService : ISubmissionTypesCacheService
 {
     private readonly ICacheService cache;
     private readonly ISubmissionTypesBusinessService submissionTypesBusiness;
-    private readonly ISubmissionTypesDataService submissionTypesData;
 
     public SubmissionTypesCacheService(
         ICacheService cache,
-        ISubmissionTypesBusinessService submissionTypesBusiness,
-        ISubmissionTypesDataService submissionTypesData)
+        ISubmissionTypesBusinessService submissionTypesBusiness)
     {
         this.cache = cache;
         this.submissionTypesBusiness = submissionTypesBusiness;
-        this.submissionTypesData = submissionTypesData;
     }
 
     public Task<IEnumerable<SubmissionTypeFilterServiceModel>> GetAllOrderedByLatestUsage(int cacheSeconds)
         => this.cache.Get(
             CacheConstants.SubmissionTypesByUsage,
             this.submissionTypesBusiness.GetAllOrderedByLatestUsage,
-            cacheSeconds);
-
-    public async Task<SubmissionTypeCacheModel?> GetById(
-        int submissionTypeId,
-        int cacheSeconds)
-        => await this.cache.Get(
-            string.Format(CacheConstants.SubmissionTypeById, submissionTypeId),
-            async () => await this.submissionTypesData.OneByIdTo<SubmissionTypeCacheModel>(submissionTypeId),
             cacheSeconds);
 }
