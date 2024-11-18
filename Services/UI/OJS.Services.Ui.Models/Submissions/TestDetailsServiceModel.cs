41cfe9f660b5039abd47b23744a92b434ff6f99a
Georgi Georgiev <gogo4ds@gmail.com>
2024-11-18 11:59:46 +0200

    Optimize submission details by not retrieving test input for all tests

--- Services/UI/OJS.Services.Ui.Models/Submissions/TestDetailsServiceModel.cs
+++ Services/UI/OJS.Services.Ui.Models/Submissions/TestDetailsServiceModel.cs
@@ -1,9 +1,10 @@
 ﻿namespace OJS.Services.Ui.Models.Submissions;
 
+using AutoMapper;
 using OJS.Data.Models.Tests;
 using OJS.Services.Infrastructure.Models.Mapping;
 
-public class TestDetailsServiceModel : IMapFrom<Test>
+public class TestDetailsServiceModel : IMapExplicitly
 {
     public int Id { get; set; }
 
@@ -13,9 +14,13 @@ public class TestDetailsServiceModel : IMapFrom<Test>
 
     public bool HideInput { get; set; }
 
-    public byte[] InputData { get; set; } = [];
-
-    public string? InputDataAsString { get; set; }
+    public byte[]? InputData { get; set; }
 
     public double OrderBy { get; set; }
+
+    public void RegisterMappings(IProfileExpression configuration)
+        => configuration
+            .CreateMap<Test, TestDetailsServiceModel>()
+            .ForMember(m => m.InputData,
+                opt => opt.MapFrom(src => src.IsTrialTest && !src.HideInput ? src.InputData : null));
 }
