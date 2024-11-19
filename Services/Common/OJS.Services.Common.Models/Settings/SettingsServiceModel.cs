6a4f531270addd93b8de10beed0a73e33f30664a
Simona Simeonova <simona.simeonova@softuni.bg>
2024-11-19 10:18:47 +0200

    New row

--- Services/Common/OJS.Services.Common.Models/Settings/SettingsServiceModel.cs
+++ Services/Common/OJS.Services.Common.Models/Settings/SettingsServiceModel.cs
@@ -7,6 +7,7 @@ using OJS.Data.Models;
 public class SettingServiceModel : IMapTo<Setting>
 {
     public int Id { get; set; }
+
     public string Name { get; set; } = string.Empty;
 
     public string Value { get; set; } = string.Empty;
