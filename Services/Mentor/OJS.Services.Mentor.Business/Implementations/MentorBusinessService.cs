f0387ac4f759aa99a02530ed8b2bc916ca309c3e
Denis Yordanov <deyordanov777@gmail.com>
2024-11-21 20:46:45 +0200

    Added the retrieval of all problem resources for a given contest.

--- Services/Mentor/OJS.Services.Mentor.Business/Implementations/MentorBusinessService.cs
+++ Services/Mentor/OJS.Services.Mentor.Business/Implementations/MentorBusinessService.cs
@@ -15,6 +15,7 @@ using OJS.Services.Infrastructure.Exceptions;
 using OJS.Services.Infrastructure.Extensions;
 using OJS.Services.Mentor.Business;
 using OJS.Services.Mentor.Models;
+using OJS.Services.Ui.Data;
 using OpenAI;
 using OpenAI.Chat;
 using TiktokenSharp;
@@ -31,19 +32,22 @@ public class MentorBusinessService : IMentorBusinessService
     private readonly OpenAIClient openAiClient;
     private readonly IHttpClientFactory httpClientFactory;
     private readonly IDataService<Setting> settingData;
+    private readonly IContestsDataService contestsData;
 
     public MentorBusinessService(
         IDataService<UserMentor> userMentorData,
         IDataService<MentorPromptTemplate> mentorPromptTemplateData,
         IHttpClientFactory httpClientFactory,
         IDataService<Setting> settingData,
+        IContestsDataService contestsData,
         OpenAIClient openAiClient)
     {
         this.userMentorData = userMentorData;
         this.mentorPromptTemplateData = mentorPromptTemplateData;
-        this.openAiClient = openAiClient;
         this.httpClientFactory = httpClientFactory;
         this.settingData = settingData;
+        this.contestsData = contestsData;
+        this.openAiClient = openAiClient;
     }
 
     public async Task<ConversationResponseModel> StartConversation(ConversationRequestModel model)
@@ -100,7 +104,14 @@ public class MentorBusinessService : IMentorBusinessService
                 .GetQuery()
                 .FirstOrDefaultAsync();
 
-            var wordFiles = model.ProblemResources
+            var problemsResources = await this.contestsData.GetByIdQuery(model.ContestId)
+                .Include(c => c.ProblemGroups)
+                .ThenInclude(pg => pg.Problems)
+                .ThenInclude(p => p.Resources)
+                .SelectMany(c => c.ProblemGroups.SelectMany(pg => pg.Problems).SelectMany(p => p.Resources))
+                .ToListAsync();
+
+            var wordFiles = problemsResources
                 .Where(pr =>
                        pr is { File: not null, FileExtension: Docx } ||
                        pr.Link is not null && pr.Link.Split('.').Last().Equals(Docx, StringComparison.Ordinal))
