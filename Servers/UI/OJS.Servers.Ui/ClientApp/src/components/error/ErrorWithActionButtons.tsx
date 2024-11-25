37c47861825c5f153cbbab697e95bfb6029d90ea
Simona Simeonova <simona.simeonova@softuni.bg>
2024-11-25 15:54:46 +0200

    Reverted parsing of string to json error and added function to handle removal of quotes

--- Servers/UI/OJS.Servers.Ui/ClientApp/src/components/error/ErrorWithActionButtons.tsx
+++ Servers/UI/OJS.Servers.Ui/ClientApp/src/components/error/ErrorWithActionButtons.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { defaultErrorMessage } from 'src/utils/http-utils';
 
 import useTheme from '../../hooks/use-theme';
 import isNilOrEmpty from '../../utils/check-utils';
@@ -16,14 +17,25 @@ interface IErrorProps {
 const ErrorWithActionButtons = ({ message, backToUrl, backToText }: IErrorProps) => {
     const { getColorClassName, themeColors } = useTheme();
 
+    const getMessage = () => {
+        if (isNilOrEmpty(message) || !message) {
+            return defaultErrorMessage;
+        }
+
+        try {
+            // There are a few cases where the error message is returned as json
+            JSON.parse(message);
+
+            return JSON.stringify(message);
+        } catch (e) {
+            return message.replace('"', '');
+        }
+    };
+
     return (
         <div className={concatClassNames(getColorClassName(themeColors.textColor), styles.errorWrapper)}>
             <div className={styles.message}>
-                {
-                    isNilOrEmpty(message)
-                        ? 'Something went wrong, please try again!'
-                        : message
-                }
+                {getMessage()}
             </div>
             <div className={styles.buttonsWrapper}>
                 <LinkButton
