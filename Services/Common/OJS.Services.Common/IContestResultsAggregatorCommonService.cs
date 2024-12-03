8b4ac1e0ffe6fde640d328731527fe3bd50f1734
Georgi Georgiev <gogo4ds@gmail.com>
2024-12-03 11:05:42 +0200

    Optimize contest results validation and submissions by problem query (#1512)
    Closes
    https://github.com/SoftUni-Internal/exam-systems-issues/issues/1645
    Closes
    https://github.com/SoftUni-Internal/exam-systems-issues/issues/1643
    
    **Summary of the changes made**:
    1. Optimized validation logic
    2. Code style
    3. Optimize submissions by problem query

--- Services/Common/OJS.Services.Common/IContestResultsAggregatorCommonService.cs
+++ Services/Common/OJS.Services.Common/IContestResultsAggregatorCommonService.cs
@@ -1,9 +1,10 @@
 ﻿namespace OJS.Services.Common;
 
+using OJS.Services.Common.Models.Contests;
 using OJS.Services.Infrastructure;
 using OJS.Services.Common.Models.Contests.Results;
 
 public interface IContestResultsAggregatorCommonService : IService
 {
-    ContestResultsViewModel GetContestResults(ContestResultsModel contestResultsModel);
+    ContestResultsViewModel GetContestResults(ContestResultsModel contestResultsModel, IContestActivityServiceModel contestActivity);
 }
