c58805fcf59aa9f000caff0a7f70fd539e17ed78
Denis Yordanov <deyordanov777@gmail.com>
2025-01-17 09:17:21 +0200

    Removed the check for loading the additional files.

--- Services/UI/OJS.Services.Ui.Business/Implementations/SubmissionsBusinessService.cs
+++ Services/UI/OJS.Services.Ui.Business/Implementations/SubmissionsBusinessService.cs
@@ -406,16 +406,10 @@ public class SubmissionsBusinessService : ISubmissionsBusinessService
             return;
         }
 
-        if (submissionType.ExecutionStrategyType
-            is ExecutionStrategyType.RunSpaAndExecuteMochaTestsExecutionStrategy
-            or ExecutionStrategyType.RunSpaAndExecuteMochaTestsExecutionStrategySeparateTests
-            or ExecutionStrategyType.NodeJsV20RunSpaAndExecuteMochaTestsExecutionStrategySeparateTests)
-        {
-            problem.AdditionalFiles = await this.problemsDataService
-                .GetByIdQuery(model.ProblemId)
-                .Select(p => p.AdditionalFiles)
-                .FirstAsync();
-        }
+        problem.AdditionalFiles = await this.problemsDataService
+            .GetByIdQuery(model.ProblemId)
+            .Select(p => p.AdditionalFiles)
+            .FirstAsync();
 
         SubmissionForProcessing? submissionForProcessing = null;
         await this.transactionsProvider.ExecuteInTransaction(async () =>
