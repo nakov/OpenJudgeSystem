ffe6dfdbc70d59a9102c87576f874600fa67b9bc
Denis Yordanov <deyordanov777@gmail.com>
2025-02-13 16:38:15 +0200

    Improved the typescript strategy's code.

--- Services/Common/OJS.Workers/OJS.Workers.ExecutionStrategies/NodeJs/Typescript/TypeScriptPreprocessExecuteAndCheckExecutionStrategy.cs
+++ Services/Common/OJS.Workers/OJS.Workers.ExecutionStrategies/NodeJs/Typescript/TypeScriptPreprocessExecuteAndCheckExecutionStrategy.cs
@@ -1,29 +1,22 @@
 ﻿namespace OJS.Workers.ExecutionStrategies.NodeJs.Typescript;
 
 using System;
-using System.Collections.Generic;
 using System.IO;
 using Microsoft.Extensions.Logging;
 using OJS.Workers.Common;
+using OJS.Workers.Common.Exceptions;
 using OJS.Workers.Common.Extensions;
 using OJS.Workers.Common.Helpers;
-using OJS.Workers.ExecutionStrategies.Helpers;
 using OJS.Workers.ExecutionStrategies.Models;
 using OJS.Workers.Executors;
 using static OJS.Workers.ExecutionStrategies.NodeJs.NodeJsConstants;
 
-public class TypeScriptPreprocessExecuteAndCheckExecutionStrategy<TSettings> : BaseInterpretedCodeExecutionStrategy<TSettings>
+public class TypeScriptPreprocessExecuteAndCheckExecutionStrategy<TSettings> : NodeJsPreprocessExecuteAndCheckExecutionStrategy<TSettings>
     where TSettings : TypeScriptPreprocessExecuteAndCheckExecutionStrategySettings
 {
-    protected const string RequiredModules = "#requiredModule#";
-    protected const string PreevaluationPlaceholder = "#preevaluationCode#";
-    protected const string PostevaluationPlaceholder = "#postevaluationCode#";
-    protected const string EvaluationPlaceholder = "#evaluationCode#";
-
     private const string JsExtension = ".js";
     private const string TsExtension = ".ts";
     private const char TrimCharacter = ';';
-    private const string DefaultAdapterFunctionCode = "(input, code) => code(input);";
     private const string TranspiledSubmissionFile = "/transpiled-submission.ts";
     private const string StrictJavaScriptPrefix = "\"use strict\";";
 
@@ -34,156 +27,37 @@ public class TypeScriptPreprocessExecuteAndCheckExecutionStrategy<TSettings> : B
         ILogger<BaseExecutionStrategy<TSettings>> logger)
         : base(submission, processExecutorFactory, settingsProvider, logger)
     {
-        if (!File.Exists(this.Settings.NodeJsExecutablePath))
+        if (!File.Exists(this.Settings.TypeScriptExecutablePath))
         {
             throw new ArgumentException(
-                $"NodeJS not found in: {this.Settings.NodeJsExecutablePath}",
-                nameof(this.Settings.NodeJsExecutablePath));
-        }
-
-        if (!Directory.Exists(this.Settings.UnderscoreModulePath))
-        {
-            throw new ArgumentException(
-                $"Underscore not found in: {this.Settings.UnderscoreModulePath}",
-                nameof(this.Settings.UnderscoreModulePath));
-        }
-
-        if (this.Settings.BaseTimeUsed < 0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(this.Settings.BaseTimeUsed));
-        }
-
-        if (this.Settings.BaseMemoryUsed < 0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(this.Settings.BaseMemoryUsed));
+                $"TypeScript not found in: {this.Settings.TypeScriptExecutablePath}",
+                nameof(this.Settings.TypeScriptExecutablePath));
         }
     }
-
-    protected virtual string JsCodeRequiredModules => $@"
-var EOL = require('os').EOL,
-_ = require('{this.Settings.UnderscoreModulePath}')";
-
-    protected virtual string JsNodeDisableCode => @"
-// DataView = undefined;
-DTRACE_NET_SERVER_CONNECTION = undefined;
-// DTRACE_NET_STREAM_END = undefined;
-DTRACE_NET_SOCKET_READ = undefined;
-DTRACE_NET_SOCKET_WRITE = undefined;
-DTRACE_HTTP_SERVER_REQUEST = undefined;
-DTRACE_HTTP_SERVER_RESPONSE = undefined;
-DTRACE_HTTP_CLIENT_REQUEST = undefined;
-DTRACE_HTTP_CLIENT_RESPONSE = undefined;
-COUNTER_NET_SERVER_CONNECTION = undefined;
-COUNTER_NET_SERVER_CONNECTION_CLOSE = undefined;
-COUNTER_HTTP_SERVER_REQUEST = undefined;
-COUNTER_HTTP_SERVER_RESPONSE = undefined;
-COUNTER_HTTP_CLIENT_REQUEST = undefined;
-COUNTER_HTTP_CLIENT_RESPONSE = undefined;
-process.argv = undefined;
-process.versions = undefined;
-process.env = { NODE_DEBUG: false };
-process.addListener = undefined;
-process.EventEmitter = undefined;
-process.mainModule = undefined;
-process.config = undefined;
-//process.removeListener = undefined;
-// process.on = undefined;
-process.openStdin = undefined;
-process.chdir = undefined;
-process.cwd = undefined;
-process.exit = undefined;
-process.umask = undefined;
-// GLOBAL = undefined;
-// root = undefined;
-// global = {};
-setInterval = undefined;
-//setTimeout = undefined;
-//clearTimeout = undefined;
-clearInterval = undefined;
-// setImmediate = undefined;
-clearImmediate = undefined;
-module = undefined;
-require = undefined;
-msg = undefined;
-
-// delete DataView;
-delete DTRACE_NET_SERVER_CONNECTION;
-// delete DTRACE_NET_STREAM_END;
-delete DTRACE_NET_SOCKET_READ;
-delete DTRACE_NET_SOCKET_WRITE;
-delete DTRACE_HTTP_SERVER_REQUEST;
-delete DTRACE_HTTP_SERVER_RESPONSE;
-delete DTRACE_HTTP_CLIENT_REQUEST;
-delete DTRACE_HTTP_CLIENT_RESPONSE;
-delete COUNTER_NET_SERVER_CONNECTION;
-delete COUNTER_NET_SERVER_CONNECTION_CLOSE;
-delete COUNTER_HTTP_SERVER_REQUEST;
-delete COUNTER_HTTP_SERVER_RESPONSE;
-delete COUNTER_HTTP_CLIENT_REQUEST;
-delete COUNTER_HTTP_CLIENT_RESPONSE;
-delete process.argv;
-delete process.exit;
-delete process.versions;
-// delete GLOBAL;
-// delete root;
-delete setInterval;
-//delete setTimeout;
-//delete clearTimeout;
-delete clearInterval;
-// delete setImmediate;
-delete clearImmediate;
-delete module;
-delete require;
-delete msg;
-
-process.exit = function () {};";
-
-    protected virtual string JsCodeEvaluation => @"
-process.stdin.resume();
-process.stdin.on('data', function(buf) { content += buf.toString(); });
-process.stdin.on('end', function() {
-    content = content.replace(new RegExp(EOL + '$'), '');
-    let inputData = content.split(EOL);
-    let result = adapterFunction(inputData, code.run);
-    if (result !== undefined) {
-        console.log(result);
-    }
-});";
-
-    protected virtual string JsCodePostevaulationCode => string.Empty;
-
-    protected virtual string JsCodeTemplate =>
-        RequiredModules + @";" +
-        NodeDisablePlaceholder +
-        PreevaluationPlaceholder +
-        EvaluationPlaceholder +
-        PostevaluationPlaceholder;
 
     protected override async Task<IExecutionResult<TestResult>> ExecuteAgainstTestsInput(
         IExecutionContext<TestsInputModel> executionContext,
         IExecutionResult<TestResult> result)
     {
-        var tsCodeSavePath = FileHelpers.SaveStringToFile(executionContext.Code.Trim(TrimCharacter), FileHelpers.BuildPath(this.WorkingDirectory, TranspiledSubmissionFile));
-
         var executor = this.CreateExecutor();
 
-        var transpilationResult = await this.TranspileCode(
-            executionContext,
-            executor,
-            tsCodeSavePath);
-
-        if (!string.IsNullOrWhiteSpace(transpilationResult.ReceivedOutput))
+        string jsCodeSavePath;
+        try
+        {
+            jsCodeSavePath = await this.SaveAndTranspileTypeScriptCodeAndGetJavaScriptCodePath(executionContext, executor);
+        }
+        catch (InvalidProcessExecutionOutputException exception)
         {
             result.IsCompiledSuccessfully = false;
-            result.CompilerComment = transpilationResult.ReceivedOutput;
+            result.CompilerComment = exception.Message;
             return result;
         }
 
-        var jsFile = FileHelpers.ReadFile(tsCodeSavePath.Replace(TsExtension, JsExtension));
-
-        executionContext.Code = jsFile[StrictJavaScriptPrefix.Length..];
-
-        var jsCodeSavePath = this.SaveCodeToTempFile(executionContext);
+        if (string.IsNullOrWhiteSpace(jsCodeSavePath))
+        {
+            // The javascript code was not saved after transpilation.
+            throw new FileNotFoundException("The transpiled javascript file was not found.");
+        }
 
         var checker = executionContext.Input.GetChecker();
 
@@ -198,14 +72,30 @@ process.stdin.on('end', function() {
         IExecutionContext<SimpleInputModel> executionContext,
         IExecutionResult<OutputResult> result)
     {
-        var codeSavePath = this.SaveCodeToTempFile(executionContext);
-
         var executor = this.CreateExecutor();
+
+        string jsCodeSavePath;
+        try
+        {
+            jsCodeSavePath = await this.SaveAndTranspileTypeScriptCodeAndGetJavaScriptCodePath(executionContext, executor);
+        }
+        catch (InvalidProcessExecutionOutputException exception)
+        {
+            result.IsCompiledSuccessfully = false;
+            result.CompilerComment = exception.Message;
+            return result;
+        }
+
+        if (string.IsNullOrWhiteSpace(jsCodeSavePath))
+        {
+            // The javascript code was not saved after transpilation.
+            throw new FileNotFoundException("The transpiled javascript file was not found.");
+        }
 
         var processExecutionResult = await this.ExecuteCode(
             executionContext,
             executor,
-            codeSavePath,
+            jsCodeSavePath,
             executionContext.Input.Input);
 
         result.Results.Add(GetOutputResult(processExecutionResult));
@@ -213,79 +103,42 @@ process.stdin.on('end', function() {
         return result;
     }
 
-    protected virtual async Task<List<TestResult>> ProcessTests(
-        IExecutionContext<TestsInputModel> executionContext,
-        IExecutor executor,
-        IChecker checker,
-        string codeSavePath)
+    private async Task<string> SaveAndTranspileTypeScriptCodeAndGetJavaScriptCodePath<TInput>(
+        IExecutionContext<TInput> executionContext,
+        IExecutor executor)
     {
-        var testResults = new List<TestResult>();
+        var tsCodeSavePath = FileHelpers.SaveStringToFile(executionContext.Code.Trim(TrimCharacter), FileHelpers.BuildPath(this.WorkingDirectory, TranspiledSubmissionFile));
 
-        foreach (var test in executionContext.Input.Tests)
+        var transpilationResult = await this.TranspileCode(
+            executionContext,
+            executor,
+            tsCodeSavePath);
+
+        if (!string.IsNullOrWhiteSpace(transpilationResult.ReceivedOutput))
         {
-            var testInput = PrepareTestInput(test.Input);
-            var processExecutionResult = await this.ExecuteCode(
-                executionContext,
-                executor,
-                codeSavePath,
-                testInput);
-
-            var testResult = CheckAndGetTestResult(
-                test,
-                processExecutionResult,
-                checker,
-                processExecutionResult.ReceivedOutput);
-
-            testResults.Add(testResult);
+            // Any output retrieved from the transpilation process is a type error, it should be shown to the user.
+            throw new InvalidProcessExecutionOutputException(transpilationResult.ReceivedOutput);
         }
 
-        return testResults;
+        if (!string.IsNullOrWhiteSpace(transpilationResult.ErrorOutput))
+        {
+            // An error occurred during the transpilation, this is a process error and should not be shown to the user.
+            throw new ArgumentException(transpilationResult.ErrorOutput);
+        }
+
+        return this.ReadTranspiledCodeAndSaveAsCodeToExecute(executionContext, tsCodeSavePath);
     }
 
-    protected virtual string PreprocessJsSubmission<TInput>(string template, IExecutionContext<TInput> context)
-    {
-        var problemSkeleton = !string.IsNullOrEmpty((context.Input as SimpleInputModel)?.TaskSkeletonAsString)
-            ? (context.Input as SimpleInputModel)?.TaskSkeletonAsString
-            : !string.IsNullOrEmpty((context.Input as TestsInputModel)?.TaskSkeletonAsString)
-                ? (context.Input as TestsInputModel)?.TaskSkeletonAsString
-                : DefaultAdapterFunctionCode;
-
-        var code = context.Code.Trim(TrimCharacter);
-
-        var processedCode = template
-            .Replace(RequiredModules, this.JsCodeRequiredModules)
-            .Replace(PreevaluationPlaceholder, JsCodePreEvaluationCodeProvider.GetPreEvaluationCode(this.Type))
-            .Replace(EvaluationPlaceholder, this.JsCodeEvaluation)
-            .Replace(PostevaluationPlaceholder, this.JsCodePostevaulationCode)
-            .Replace(NodeDisablePlaceholder, this.JsNodeDisableCode)
-            .Replace(UserInputPlaceholder, code)
-            .Replace(AdapterFunctionPlaceholder, problemSkeleton);
-
-        return processedCode;
-    }
-
-    protected override string SaveCodeToTempFile<TInput>(IExecutionContext<TInput> executionContext)
-    {
-        // Preprocess the user submission
-        var codeToExecute = this.PreprocessJsSubmission(
-            this.JsCodeTemplate,
-            executionContext);
-
-        // Save the preprocessed submission which is ready for execution
-        return FileHelpers.SaveStringToTempFile(this.WorkingDirectory, codeToExecute);
-    }
-
-    private Task<ProcessExecutionResult> ExecuteCode<TInput>(
+    private string ReadTranspiledCodeAndSaveAsCodeToExecute<TInput>(
         IExecutionContext<TInput> executionContext,
-        IExecutor executor,
-        string codeSavePath,
-        string? inputData = null)
-        => executor.Execute(
-            this.Settings.NodeJsExecutablePath,
-            executionContext.TimeLimit,
-            executionContext.MemoryLimit,
-            inputData,
-            [LatestEcmaScriptFeaturesEnabledFlag, codeSavePath]);
+        string tsCodeSavePath)
+    {
+        var jsFile = FileHelpers.ReadFile(tsCodeSavePath.Replace(TsExtension, JsExtension));
+
+        executionContext.Code = jsFile[StrictJavaScriptPrefix.Length..];
+
+        return this.SaveCodeToTempFile(executionContext);
+    }
 
     private Task<ProcessExecutionResult> TranspileCode<TInput>(
         IExecutionContext<TInput> executionContext,
@@ -307,4 +160,8 @@ public record TypeScriptPreprocessExecuteAndCheckExecutionStrategySettings(
     string TypeScriptExecutablePath,
     string NodeJsExecutablePath,
     string UnderscoreModulePath)
-    : BaseInterpretedCodeExecutionStrategySettings(BaseTimeUsed, BaseMemoryUsed);
+    : NodeJsPreprocessExecuteAndCheckExecutionStrategySettings(
+        BaseTimeUsed,
+        BaseMemoryUsed,
+        NodeJsExecutablePath,
+        UnderscoreModulePath);
