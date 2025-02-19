b12a82018885164a01d9f1450a1175cbe6936314
Georgi Georgiev <gogo4ds@gmail.com>
2025-02-19 20:59:02 +0200

    Docs

--- Services/UI/OJS.Services.Ui.Business/Cache/ILecturersInContestsCacheService.cs
+++ Services/UI/OJS.Services.Ui.Business/Cache/ILecturersInContestsCacheService.cs
@@ -6,5 +6,13 @@ using System.Threading.Tasks;
 
 public interface ILecturersInContestsCacheService : IService
 {
+    /// <summary>
+    /// Check if user is admin or lecturer in contest, or in category for the contest.
+    /// Uses in-memory cache for all the lecturers in contests and categories, which is cleared on a short interval.
+    /// </summary>
+    /// <param name="contestId">The contest to check for</param>
+    /// <param name="categoryId">The category of the contest</param>
+    /// <param name="user">The user to check</param>
+    /// <returns>True if user is admin or lecturer in contest, or in category for the contest.</returns>
     Task<bool> IsUserAdminOrLecturerInContest(int? contestId, int? categoryId, UserInfoModel? user);
 }
